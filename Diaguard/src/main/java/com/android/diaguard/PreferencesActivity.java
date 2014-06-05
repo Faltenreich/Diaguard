590a44e77407ef1980e6823f6410b70efb39ea93
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2014-06-05 15:42:01 +0200

    Preferences without Headers

--- Diaguard/src/main/java/com/android/diaguard/PreferencesActivity.java
+++ Diaguard/src/main/java/com/android/diaguard/PreferencesActivity.java
@@ -1,10 +1,9 @@
 package com.android.diaguard;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-
-import java.util.List;
 
 /**
  * Created by Filip on 26.10.13.
@@ -12,14 +11,38 @@ import java.util.List;
 public class PreferencesActivity extends PreferenceActivity {
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle(getString(R.string.settings));
+
+        if(Build.VERSION.SDK_INT < 11)
+            addPreferencesFromResource(R.xml.preferences);
+        else
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new PreferenceFragmentMain())
+                    .commit();
+    }
+
+    /*
+    @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.headers, target);
     }
+    */
 
     @Override
     protected  boolean isValidFragment(String fragmentName) {
         // Needed to prevent RuntimeException on API >19
         return true;
+    }
+
+    public static class PreferenceFragmentMain extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 
     public static class PreferenceFragmentApplication extends PreferenceFragment {

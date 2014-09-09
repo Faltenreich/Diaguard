1b2c296a7e6e6ea35818108c35c07c4230e5c48a
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2014-09-09 19:25:25 +0200

    Fixed crash for LogFragment

--- Diaguard/src/main/java/com/faltenreich/diaguard/fragments/LogFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/fragments/LogFragment.java
@@ -1,48 +1,28 @@
 package com.faltenreich.diaguard.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.faltenreich.diaguard.EntryDetailActivity;
 import com.faltenreich.diaguard.R;
 
-public class LogFragment extends Fragment implements EntryListFragment.CallbackList {
+public class LogFragment extends Fragment {
 
-    public static final int REQUEST_EVENT_CREATED = 1;
-    private boolean isLargeScreen = false;
+    private static View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_log, container, false);
+        // To multiple instantiation of the same fragment
+        // Needed for fragments in fragments
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        view = inflater.inflate(R.layout.fragment_log, container, false);
         setHasOptionsMenu(true);
-
         return view;
-    }
-
-    /**
-     * Callback from MessageListFragment to respond to a selected ListItem
-     */
-    @Override
-    public void onItemSelected(long id) {
-        if (isLargeScreen) {
-            // Tablet
-            Bundle arguments = new Bundle();
-            arguments.putLong(EntryDetailFragment.ENTRY_ID, id);
-            EntryDetailFragment fragment = new EntryDetailFragment();
-            fragment.setArguments(arguments);
-            getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.entry_detail, fragment)
-                    .commit();
-        }
-        else {
-            // Phone
-            Intent intent = new Intent(getActivity(), EntryDetailActivity.class);
-            intent.putExtra(EntryDetailFragment.ENTRY_ID, id);
-            startActivity(intent);
-        }
     }
 }

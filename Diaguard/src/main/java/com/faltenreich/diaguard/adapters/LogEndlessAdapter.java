61377c4e2ec6f2373720b7b1122c36028eed0cec
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2015-01-21 02:36:57 +0100

    Integrated EndlessAdapter manually to prevent android.permission.READ_PHONE_STATE

--- Diaguard/src/main/java/com/faltenreich/diaguard/adapters/LogEndlessAdapter.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/adapters/LogEndlessAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 
-import com.commonsware.cwac.endless.EndlessAdapter;
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.database.DatabaseDataSource;
 import com.faltenreich.diaguard.database.DatabaseHelper;

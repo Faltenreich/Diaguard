a3354470d43601673442f0e550728e54349f630d
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2015-01-23 20:24:09 +0100

    Legal and privacy

---
+++ Diaguard/src/main/java/com/faltenreich/diaguard/preferences/AboutPreference.java
@@ -0,0 +1,31 @@
+package com.faltenreich.diaguard.preferences;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.faltenreich.diaguard.R;
+
+/**
+ * Created by Filip on 04.11.13.
+ */
+public class AboutPreference extends DialogPreference {
+
+    public AboutPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setDialogLayoutResource(R.layout.preference_about);
+        setNegativeButtonText(null);
+    }
+
+    @Override
+    public void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        if(Build.VERSION.SDK_INT <= 10) {
+            view.setBackgroundColor(Color.WHITE);
+        }
+    }
+}

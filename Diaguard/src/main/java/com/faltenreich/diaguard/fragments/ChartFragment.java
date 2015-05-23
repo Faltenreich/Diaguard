63c7e9914baac4a70e076348c64229113ff89e4c
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2015-05-23 19:10:24 +0200

    Small changes for moveViewportToCurrentDateTime-algorithm

--- Diaguard/src/main/java/com/faltenreich/diaguard/fragments/ChartFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/fragments/ChartFragment.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,12 +24,14 @@ import com.github.mikephil.charting.charts.BarLineChartBase;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.Highlight;
 
@@ -41,18 +44,21 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ChartFragment extends Fragment implements OnChartValueSelectedListener {
+public class ChartFragment extends Fragment implements View.OnClickListener, OnChartValueSelectedListener, OnChartGestureListener {
 
     private static final int TEXT_SIZE = 4;
     private static final int LINE_WIDTH = 1;
     private static final int SCATTER_SHAPE_SIZE = 5;
+    private static final int MOVING_SPEED = 2;
 
     private PreferenceHelper preferenceHelper;
     private DatabaseDataSource dataSource;
 
     private LineChart viewport;
     private ScatterChart chart;
-    Button buttonDate;
+    private Button buttonDate;
+    private View buttonPrevious;
+    private View buttonNext;
 
     private DateTime currentDateTime;
 
@@ -76,14 +82,14 @@ public class ChartFragment extends Fragment implements OnChartValueSelectedListe
     @Override
     public void onResume() {
         super.onResume();
-        update();
+        initializeChart();
     }
 
     private void initialize() {
         preferenceHelper = new PreferenceHelper(getActivity());
         dataSource = new DatabaseDataSource(getActivity());
 
-        currentDateTime = DateTime.now();
+        currentDateTime = DateTime.now().withHourOfDay(0).withMinuteOfHour(0);
 
         getComponents();
         initializeGUI();
@@ -93,29 +99,14 @@ public class ChartFragment extends Fragment implements OnChartValueSelectedListe
         viewport = (LineChart) getView().findViewById(R.id.viewport);
         chart = (ScatterChart) getView().findViewById(R.id.chart);
         buttonDate = (Button) getView().findViewById(R.id.button_date);
+        buttonPrevious = getView().findViewById(R.id.button_previous);
+        buttonNext = getView().findViewById(R.id.button_next);
     }
 
     private void initializeGUI() {
-        buttonDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePicker();
-            }
-        });
-
-        getView().findViewById(R.id.button_previous).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                previousDay();
-            }
-        });
-
-        getView().findViewById(R.id.button_next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nextDay();
-            }
-        });
+        buttonDate.setOnClickListener(this);
+        buttonPrevious.setOnClickListener(this);
+        buttonNext.setOnClickListener(this);
 
         setChartDefaultStyle(viewport);
         viewport.getAxisLeft().setEnabled(false);
@@ -123,6 +114,7 @@ public class ChartFragment extends Fragment implements OnChartValueSelectedListe
 
         setChartDefaultStyle(chart);
         chart.setOnChartValueSelectedListener(this);
+        chart.setOnChartGestureListener(this);
     }
 
     private void setChartDefaultStyle(BarLineChartBase chartBase) {
@@ -148,85 +140,104 @@ public class ChartFragment extends Fragment implements OnChartValueSelectedListe
         chartBase.getAxisRight().setEnabled(false);
     }
 
-    private void update() {
-        dataSource.open();
-
-        // TODO: Only for large devices
-        // updateViewport();
-        updateChartData();
-        updateChart();
-
-        dataSource.close();
+    private DateTime getCurrentDateTime() {
+        return this.currentDateTime;
     }
 
-    private void updateViewport() {
-        ArrayList<String> xLabels = new ArrayList<>();
-        ArrayList<com.github.mikephil.charting.data.Entry> entries = new ArrayList<>();
-
-        // Iterate through every day of the current selected month
-        for(DateTime day = currentDateTime.dayOfMonth().withMinimumValue();
-            day.isBefore(currentDateTime.dayOfMonth().withMaximumValue());
-            day = day.plusDays(1)) {
-
-            float averageOfDay = dataSource.getBloodSugarAverageOfDay(day);
-            if(averageOfDay > 0) {
-                averageOfDay = preferenceHelper.formatDefaultToCustomUnit(Measurement.Category.BloodSugar, averageOfDay);
-                entries.add(new com.github.mikephil.charting.data.Entry(averageOfDay, day.getDayOfMonth()));
-            }
-
-            xLabels.add(day.getDayOfMonth() + ".");
-        }
-
-        LineDataSet lineDataSet = new LineDataSet(entries, "Viewport");
-        lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        lineDataSet.setDrawCircles(false);
-        lineDataSet.setColor(getResources().getColor(android.R.color.darker_gray));
-        lineDataSet.setLineWidth(Helper.getDPI(getActivity(), LINE_WIDTH));
-        lineDataSet.setDrawCubic(true);
-        lineDataSet.setDrawValues(false);
-
-        ArrayList<LineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(lineDataSet);
-
-        viewport.setData(new LineData(xLabels, dataSets));
-        viewport.invalidate();
+    // Add a few hours to show target day in UI not until user has scrolled halfway to it
+    private DateTime getCurrentDateTimeForUI() {
+        return this.currentDateTime.plusHours(12);
     }
 
-    private void updateChart() {
-        DateTimeFormatter format = preferenceHelper.getDateFormat();
-        String weekDay = getResources().getStringArray(R.array.weekdays)[currentDateTime.dayOfWeek().get()-1];
-        String dateButtonText = weekDay.substring(0,2) + "., " + format.print(currentDateTime);
-        buttonDate.setText(dateButtonText);
+    private void updateDateTime() {
+        final int xMinimumVisibleValue = chart.getLowestVisibleXIndex();
+        final int dayOfMonth = xMinimumVisibleValue / 60 / 24;
+        final int hourOfDay = (xMinimumVisibleValue - (dayOfMonth * 60 * 24)) / 60;
+        final int minuteOfHour = xMinimumVisibleValue - (dayOfMonth * 60 * 24) - (hourOfDay * 60);
+        currentDateTime = currentDateTime
+                .withDayOfMonth(dayOfMonth + 1)
+                .withHourOfDay(hourOfDay)
+                .withMinuteOfHour(minuteOfHour);
+        updateLabels();
+    }
 
+    public void previousDay() {
+        currentDateTime = currentDateTime.minusDays(1);
+        updateLabels();
         moveViewportToCurrentDateTime();
+    }
+
+    public void nextDay() {
+        currentDateTime = currentDateTime.plusDays(1);
+        updateLabels();
+        moveViewportToCurrentDateTime();
+    }
+
+    public void showDatePicker() {
+        DialogFragment fragment = new DatePickerFragment() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                currentDateTime = currentDateTime.withYear(year).withMonthOfYear(month+1).withDayOfMonth(day);
+                updateLabels();
+                moveViewportToCurrentDateTime();
+            }
+        };
+        Bundle bundle = new Bundle(1);
+        bundle.putSerializable(DatePickerFragment.DATE, currentDateTime);
+        fragment.setArguments(bundle);
+        fragment.show(getActivity().getSupportFragmentManager(), "DatePicker");
+    }
+
+    //region Chart
+
+    private void initializeChart() {
+        dataSource.open();
+        updateChartData();
+        dataSource.close();
+
+        updateLabels();
+    }
+
+    private void updateLabels() {
+        DateTimeFormatter format = preferenceHelper.getDateFormat();
+        String weekDay = getResources().getStringArray(R.array.weekdays)[getCurrentDateTimeForUI().dayOfWeek().get()-1];
+        String dateButtonText = weekDay.substring(0,2) + "., " + format.print(getCurrentDateTimeForUI());
+        buttonDate.setText(dateButtonText);
     }
 
     private void moveViewportToCurrentDateTime(){
         final Handler handler = new Handler();
         final Runnable runnable = new Runnable() {
             public void run() {
-                int xPosition = chart.getLowestVisibleXIndex();
-                final int xPositionTarget = ((currentDateTime.getDayOfMonth() - 1) * 24 * 60);
-                final int distanceInMinutes = xPositionTarget - xPosition;
-                boolean hasReachedTarget = false;
-                while (!hasReachedTarget) {
-                    final int currentPosition = xPosition + (distanceInMinutes / 60);
-                    xPosition = currentPosition;
+                int lowestVisibleXIndex = chart.getLowestVisibleXIndex();
+                // Target is start of day
+                final int xPositionTarget = ((getCurrentDateTimeForUI().getDayOfMonth() - 1) * 24 * 60);
+                final int distanceInMinutes = xPositionTarget - lowestVisibleXIndex;
+
+                // Do until target is reached
+                int currentXPosition = lowestVisibleXIndex;
+                while(distanceInMinutes > 0 ?
+                        currentXPosition < xPositionTarget :
+                        currentXPosition > xPositionTarget) {
+
+                    currentXPosition += (distanceInMinutes / 60) * MOVING_SPEED;
+
+                    // Wait some time
                     try {
                         Thread.sleep(10);
                     }
                     catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+
+                    // Move viewport
+                    final int currentFinalXPosition = currentXPosition;
                     handler.post(new Runnable() {
                         public void run() {
-                            chart.moveViewToX(currentPosition);
+                            chart.moveViewToX(currentFinalXPosition);
                             chart.invalidate();
                         }
                     });
-                    hasReachedTarget = distanceInMinutes > 0 ?
-                            currentPosition > xPositionTarget :
-                            currentPosition < xPositionTarget;
                 }
             }
         };
@@ -299,53 +310,104 @@ public class ChartFragment extends Fragment implements OnChartValueSelectedListe
         chart.invalidate();
     }
 
+    //endregion
+
+    //region Viewport
+
+    // TODO: Viewport for large devices
+    private void updateViewport() {
+        ArrayList<String> xLabels = new ArrayList<>();
+        ArrayList<com.github.mikephil.charting.data.Entry> entries = new ArrayList<>();
+
+        // Iterate through every day of the current selected month
+        for(DateTime day = currentDateTime.dayOfMonth().withMinimumValue();
+            day.isBefore(currentDateTime.dayOfMonth().withMaximumValue());
+            day = day.plusDays(1)) {
+
+            float averageOfDay = dataSource.getBloodSugarAverageOfDay(day);
+            if(averageOfDay > 0) {
+                averageOfDay = preferenceHelper.formatDefaultToCustomUnit(Measurement.Category.BloodSugar, averageOfDay);
+                entries.add(new com.github.mikephil.charting.data.Entry(averageOfDay, day.getDayOfMonth()));
+            }
+
+            xLabels.add(day.getDayOfMonth() + ".");
+        }
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "Viewport");
+        lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        lineDataSet.setDrawCircles(false);
+        lineDataSet.setColor(getResources().getColor(android.R.color.darker_gray));
+        lineDataSet.setLineWidth(Helper.getDPI(getActivity(), LINE_WIDTH));
+        lineDataSet.setDrawCubic(true);
+        lineDataSet.setDrawValues(false);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet);
+
+        viewport.setData(new LineData(xLabels, dataSets));
+        viewport.invalidate();
+    }
+
+    //endregion
+
     //region Listeners
 
-    public void previousDay() {
-        currentDateTime = currentDateTime.minusDays(1);
-        updateChart();
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_date:
+                showDatePicker();
+                break;
+            case R.id.button_previous:
+                previousDay();
+                break;
+            case R.id.button_next:
+                nextDay();
+                break;
+            default:
+                break;
+        }
     }
 
-    public void nextDay() {
-        currentDateTime = currentDateTime.plusDays(1);
-        updateChart();
+    // OnChartGestureListener
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
     }
 
-    public void showDatePicker() {
-        DialogFragment fragment = new DatePickerFragment() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                currentDateTime = currentDateTime.withYear(year).withMonthOfYear(month+1).withDayOfMonth(day);
-                updateChart();
-            }
-        };
-        Bundle bundle = new Bundle(1);
-        bundle.putSerializable(DatePickerFragment.DATE, currentDateTime);
-        fragment.setArguments(bundle);
-        fragment.show(getActivity().getSupportFragmentManager(), "DatePicker");
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
     }
 
-    /**
-     * Called when a value has been selected inside the chart.
-     *
-     * @param e The selected Entry.
-     * @param dataSetIndex The index in the datasets array of the data object
-     * the Entrys DataSet is in.
-     * @param h the corresponding highlight object that contains information
-     * about the highlighted position
-     */
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+        // TODO: Necessary?
+        // updateDateTime();
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+        updateDateTime();
+    }
+
+    // OnChartValueSelectedListener
+
     @Override
     public void onValueSelected(com.github.mikephil.charting.data.Entry e, int dataSetIndex, Highlight h) {
-        ChartMarkerView mv = new ChartMarkerView (getActivity());
-        chart.setMarkerView(mv);
+        MarkerView markerView = new ChartMarkerView (getActivity());
+        chart.setMarkerView(markerView);
     }
 
-    /**
-     * Called when nothing has been selected or an "un-select" has been made.
-     */
     @Override
     public void onNothingSelected() {
-
     }
 
     //endregion

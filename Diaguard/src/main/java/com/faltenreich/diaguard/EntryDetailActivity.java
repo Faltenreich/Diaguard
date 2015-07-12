9e89ea292c572a4151298f4a915cd4f57018580a
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2015-07-12 15:58:41 +0200

    FAB above Toolbar DateTimeFormat for EntryDetailActivity

--- Diaguard/src/main/java/com/faltenreich/diaguard/EntryDetailActivity.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/EntryDetailActivity.java
@@ -2,17 +2,20 @@ package com.faltenreich.diaguard;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 
+import com.faltenreich.diaguard.database.DatabaseFacade;
 import com.faltenreich.diaguard.database.Entry;
 import com.faltenreich.diaguard.fragments.EntryDetailFragment;
-import com.faltenreich.diaguard.helpers.Helper;
+
+import org.joda.time.format.DateTimeFormat;
+
+import java.sql.SQLException;
 
 
 public class EntryDetailActivity extends BaseActivity {
 
-    private Entry entry;
-    private Toolbar toolbar;
+    private long entryId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,39 +23,29 @@ public class EntryDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_entry_detail);
 
         Bundle arguments = new Bundle();
-        long id = getIntent().getLongExtra(EntryDetailFragment.EXTRA_ENTRY, 0);
-        arguments.putLong(EntryDetailFragment.EXTRA_ENTRY, id);
+        entryId = getIntent().getLongExtra(EntryDetailFragment.EXTRA_ENTRY, 0);
+        arguments.putLong(EntryDetailFragment.EXTRA_ENTRY, entryId);
         EntryDetailFragment fragment = new EntryDetailFragment();
         fragment.setArguments(arguments);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.entry_detail, fragment)
                 .commit();
-
-        //entry = (Entry) dataSource.get(DatabaseHelper.ENTRY, id);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (entry != null && toolbar != null){
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
-            setSupportActionBar(toolbar);
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        // Update entry
-        //entry = (Entry) dataSource.get(DatabaseHelper.ENTRY, this.entry.getId());
-
-        // Display date and time of entry in ActionBar
-        if (entry != null && toolbar != null){
-            toolbar.setTitle(Helper.getDateFormat().print(entry.getDate()) + " " +
-                    Helper.getTimeFormat().print(entry.getDate()));
+        try {
+            // Display date and time of entry in ActionBar
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            Entry entry = DatabaseFacade.getInstance().getDao(Entry.class).queryForId(entryId);
+            if (entry != null && toolbar != null){
+                String weekDay = entry.getDate().dayOfWeek().getAsShortText();
+                String dateTime = entry.getDate().toString(DateTimeFormat.shortDateTime());
+                toolbar.setTitle(String.format("%s, %s", weekDay, dateTime));
+            }
+        } catch (SQLException exception) {
+            Log.e("EntryDetailFragment", exception.getMessage());
         }
     }
 }

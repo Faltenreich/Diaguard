874487be02c66387ff358b30af75cfc0e3caf73c
Faltenreich <philipp.fahlteich@gmail.com>
2015-12-28 18:07:39 +0100

    Replacing custom license logic with LicensesDialog library Updated licenses

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/LicensePreference.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/LicensePreference.java
@@ -4,28 +4,33 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.DialogPreference;
+import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.faltenreich.diaguard.R;
+
+import de.psdev.licensesdialog.LicensesDialog;
 
 /**
  * Created by Filip on 04.11.13.
  */
-public class LicensePreference extends DialogPreference {
+public class LicensePreference extends Preference {
 
     public LicensePreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setDialogLayoutResource(R.layout.preference_license);
-        setNegativeButtonText(null);
     }
 
     @Override
-    public void onBindDialogView(View view) {
-        super.onBindDialogView(view);
+    protected void onClick() {
+        super.onClick();
 
-        if(Build.VERSION.SDK_INT <= 10) {
-            view.setBackgroundColor(Color.WHITE);
-        }
+        new LicensesDialog.Builder(getContext())
+                .setNotices(R.raw.licenses)
+                .setTitle(R.string.licenses)
+                .setIncludeOwnLicense(true)
+                .build()
+                .show();
     }
 }

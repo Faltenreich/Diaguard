e354c11579cb815bc870884e23b47bac9beb2304
Faltenreich <philipp.fahlteich@gmail.com>
2016-01-01 17:53:18 +0100

    Load category images of timeline table with Picasso

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/viewholder/TableCategoryHolder.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/viewholder/TableCategoryHolder.java
@@ -1,8 +1,5 @@
 package com.faltenreich.diaguard.ui.view.viewholder;
 
-import android.content.Intent;
-import android.support.v4.content.ContextCompat;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +7,12 @@ import android.widget.TextView;
 
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.adapter.list.ListItemCategoryValues;
-import com.faltenreich.diaguard.adapter.list.ListItemEmpty;
 import com.faltenreich.diaguard.data.PreferenceHelper;
 import com.faltenreich.diaguard.data.entity.Measurement;
-import com.faltenreich.diaguard.ui.activity.EntryActivity;
-import com.faltenreich.diaguard.ui.activity.PreferenceActivity;
 import com.faltenreich.diaguard.ui.view.TintImageView;
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * Created by Faltenreich on 17.10.2015.
@@ -40,7 +34,8 @@ public class TableCategoryHolder extends BaseViewHolder<ListItemCategoryValues> 
         ListItemCategoryValues listItem = getListItem();
         Measurement.Category category = listItem.getCategory();
         int categoryImageResourceId = PreferenceHelper.getInstance().getCategoryImageResourceId(category);
-        imageView.setImageDrawable(ContextCompat.getDrawable(getContext(), categoryImageResourceId));
+        
+        Picasso.with(getContext()).load(categoryImageResourceId).into(imageView);
 
         for (float value : listItem.getValues()) {
             TextView textView = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.list_item_table_category_value, content, false);

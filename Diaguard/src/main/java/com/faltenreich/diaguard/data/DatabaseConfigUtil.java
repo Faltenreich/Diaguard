418697914ca4fc5350a6a63c717e51a79fa713ec
Faltenreich <philipp.fahlteich@gmail.com>
2016-08-01 13:17:47 +0200

    Removed comments

--- Diaguard/src/main/java/com/faltenreich/diaguard/data/DatabaseConfigUtil.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/data/DatabaseConfigUtil.java
@@ -6,53 +6,22 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
-/**
- * OrmliteDatabaseConfigUtil is a separate program from the actual android app,
- * that is used to generate the data structure configuration before runtime.
- * It uses the models provided via a list of class objects,
- * and also the annotations (e.g. @DatabaseField) on the models to generate the configuration accordingly.
- */
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
 
-    /**
-     * Given that this is a separate program from the android app, we have to use
-     * a static main java method to create the configuration file.
-     * @param args
-     * @throws IOException
-     * @throws SQLException
-     */
+    private static final String currDirectory = "user.dir";
+    private static final String configPath = "/Diaguard/src/main/res/raw/ormlite_config.txt";
+    private static final String projectRoot = System.getProperty(currDirectory);
+    private static final String fullConfigPath = projectRoot + configPath;
+
     public static void main(String[] args) throws IOException, SQLException {
-
-        String currDirectory = "user.dir";
-
-        String configPath = "/Diaguard/src/main/res/raw/ormlite_config.txt";
-
-        /**
-         * Gets the project root directory
-         */
-        String projectRoot = System.getProperty(currDirectory);
-
-        /**
-         * Full configuration path includes the project root path, and the location
-         * of the ormlite_config.txt file appended to it
-         */
-        String fullConfigPath = projectRoot + configPath;
 
         File configFile = new File(fullConfigPath);
 
-        /**
-         * In the a scenario where we run this program serveral times, it will recreate the
-         * configuration file each time with the updated configurations.
-         */
         if(configFile.exists()) {
             configFile.delete();
             configFile = new File(fullConfigPath);
         }
 
-        /**
-         * writeConfigFile is a util method used to write the necessary configurations
-         * to the ormlite_config.txt file.
-         */
         writeConfigFile(configFile, DatabaseHelper.tables);
     }
 }

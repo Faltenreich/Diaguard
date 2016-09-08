653a9a0b95632c328dceaf559f68adceb1252f48
Faltenreich <philipp.fahlteich@gmail.com>
2016-09-08 20:24:03 +0200

    Fixed crumbled values in factor preference list

--- Diaguard/src/main/java/com/faltenreich/diaguard/adapter/TimeAdapter.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/adapter/TimeAdapter.java
@@ -1,12 +1,16 @@
 package com.faltenreich.diaguard.adapter;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.adapter.list.ListItemTimePreference;
 import com.faltenreich.diaguard.ui.view.viewholder.TimeViewHolder;
+import com.faltenreich.diaguard.util.NumberUtils;
 
 /**
  * Created by Faltenreich on 04.09.2016.
@@ -23,7 +27,24 @@ public class TimeAdapter extends BaseAdapter<ListItemTimePreference, TimeViewHol
     }
 
     @Override
-    public void onBindViewHolder(TimeViewHolder holder, int position) {
+    public void onBindViewHolder(final TimeViewHolder holder, int position) {
         holder.bindData(getItem(position));
+        holder.value.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                ListItemTimePreference preference = getItem(holder.getAdapterPosition());
+                try {
+                    preference.setValue(NumberUtils.parseNumber(editable.toString()));
+                } catch (NumberFormatException exception) {
+                    preference.setValue(-1);
+                }
+            }
+        });
     }
 }

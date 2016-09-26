b94d4cb1ee7ff3808f8573aaeab2fae22ec44638
Faltenreich <philipp.fahlteich@gmail.com>
2016-09-26 22:30:21 +0200

    Enabled speech input

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/FoodSearchActivity.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/FoodSearchActivity.java
@@ -1,6 +1,15 @@
 package com.faltenreich.diaguard.ui.activity;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.text.TextUtils;
+
 import com.faltenreich.diaguard.R;
+import com.lapism.searchview.SearchView;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by Filip on 15.11.13.
@@ -8,7 +17,23 @@ import com.faltenreich.diaguard.R;
 
 public class FoodSearchActivity extends BaseActivity {
 
+    @BindView(R.id.search_view) SearchView searchView;
+
     public FoodSearchActivity() {
         super(R.layout.activity_food_search);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SearchView.SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (results != null && results.size() > 0) {
+                String voiceInput = results.get(0);
+                if (!TextUtils.isEmpty(voiceInput)) {
+                    searchView.setQuery(voiceInput);
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }

2fdc674a07e5bc1fffb19614d6c6ec9eb94098ab
Faltenreich <philipp.fahlteich@gmail.com>
2016-09-27 19:33:44 +0200

    Views for food detail

---
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/FoodActivity.java
@@ -0,0 +1,14 @@
+package com.faltenreich.diaguard.ui.activity;
+
+import com.faltenreich.diaguard.R;
+
+/**
+ * Created by Filip on 15.11.13.
+ */
+
+public class FoodActivity extends BaseActivity {
+
+    public FoodActivity() {
+        super(R.layout.activity_food);
+    }
+}

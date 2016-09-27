544d42dd4a9cf2829f7e24b1f4f849efd5dd8dac
Faltenreich <philipp.fahlteich@gmail.com>
2016-09-27 22:15:11 +0200

    Set intent extra for FoodFragment

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/FoodFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/FoodFragment.java
@@ -1,6 +1,18 @@
 package com.faltenreich.diaguard.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.faltenreich.diaguard.R;
+import com.faltenreich.diaguard.data.dao.FoodDao;
+import com.faltenreich.diaguard.data.entity.Food;
+import com.faltenreich.diaguard.ui.activity.FoodActivity;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
 
 /**
  * Created by Faltenreich on 27.09.2016.
@@ -10,7 +22,41 @@ public class FoodFragment extends BaseFragment {
 
     public static final String EXTRA_FOOD_ID = "EXTRA_FOOD_ID";
 
+    @BindView(R.id.food_image) ImageView image;
+    @BindView(R.id.food_name) TextView name;
+
+    private Food food;
+
     public FoodFragment() {
         super(R.layout.fragment_food);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkIntents();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+    }
+
+    private void checkIntents() {
+        if (getActivity() instanceof FoodActivity && getActivity().getIntent() != null && getActivity().getIntent().getExtras() != null) {
+            Bundle extras = getActivity().getIntent().getExtras();
+            if (extras.getLong(EXTRA_FOOD_ID) >= 0) {
+                long foodId = extras.getLong(EXTRA_FOOD_ID);
+                this.food = FoodDao.getInstance().get(foodId);
+            }
+        }
+    }
+
+    private void init() {
+        if (food != null) {
+            name.setText(food.getName());
+            Picasso.with(getContext()).load(food.getImageUrl()).into(image);
+        }
     }
 }

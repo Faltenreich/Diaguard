1274e2bf2a4ad2587b7b2f8f0c3f0fbcf379e7c2
Faltenreich <philipp.fahlteich@gmail.com>
2016-10-11 21:05:33 +0200

    FAB for FoodFragment

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/FoodFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/FoodFragment.java
@@ -6,9 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -22,6 +19,7 @@ import com.faltenreich.diaguard.ui.activity.FoodActivity;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by Faltenreich on 27.09.2016.
@@ -47,7 +45,6 @@ public class FoodFragment extends BaseFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
         checkIntents();
     }
 
@@ -55,24 +52,6 @@ public class FoodFragment extends BaseFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         init();
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.food, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_entry: {
-                createEntry();
-                return true;
-            }
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     private void checkIntents() {
@@ -99,7 +78,9 @@ public class FoodFragment extends BaseFragment {
         }
     }
 
-    private void createEntry() {
+    @SuppressWarnings("unused")
+    @OnClick(R.id.food_fab)
+    public void createEntry() {
         Intent intent = new Intent(getActivity(), EntryActivity.class);
         intent.putExtra(EntryActivity.EXTRA_FOOD, food.getId());
         startActivity(intent);

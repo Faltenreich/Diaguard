c3627e7165822d326dcad9bdef82bfe1c96b73e5
Faltenreich <philipp.fahlteich@gmail.com>
2016-11-10 23:52:13 +0100

    Listening for food query events

---
+++ Diaguard/src/main/java/com/faltenreich/diaguard/event/data/FoodQueryStartedEvent.java
@@ -0,0 +1,10 @@
+package com.faltenreich.diaguard.event.data;
+
+import com.faltenreich.diaguard.event.BaseEvent;
+
+/**
+ * Created by Faltenreich on 10.11.2016.
+ */
+
+public class FoodQueryStartedEvent extends BaseEvent {
+}

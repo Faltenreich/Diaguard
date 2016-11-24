7c27b8f3530a987af43d88569d39d692a75b4a17
Faltenreich <philipp.fahlteich@gmail.com>
2016-11-24 21:24:39 +0100

    Revert "Revert "Fixed updating summary of country setting""
    This reverts commit 9c31cafca95922fc068b352bc994f638623078b4.

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/CountryPreference.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/CountryPreference.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 
 import com.faltenreich.diaguard.data.PreferenceHelper;
+import com.faltenreich.diaguard.ui.activity.PreferenceActivity;
+import com.faltenreich.diaguard.ui.fragment.PreferenceFragment;
 import com.mukesh.countrypicker.fragments.CountryPicker;
 import com.mukesh.countrypicker.interfaces.CountryPickerListener;
 
@@ -42,6 +44,14 @@ public class CountryPreference extends Preference implements Preference.OnPrefer
 
     private void setCountry(String countryCode) {
         PreferenceHelper.getInstance().setCountry(countryCode);
-        setSummary(countryCode);
+
+        if (getContext() instanceof PreferenceActivity) {
+            PreferenceActivity activity = (PreferenceActivity) getContext();
+            
+            if (activity.getFragment() != null && activity.getFragment() instanceof PreferenceFragment) {
+                PreferenceFragment preferenceFragment = (PreferenceFragment)activity.getFragment();
+                preferenceFragment.onSharedPreferenceChanged(getSharedPreferences(), getKey());
+            }
+        }
     }
 }

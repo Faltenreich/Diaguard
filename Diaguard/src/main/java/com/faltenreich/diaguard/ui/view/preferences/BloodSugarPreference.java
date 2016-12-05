b3848d3b05b3ce155221763981a65d36673efed5
Faltenreich <philipp.fahlteich@gmail.com>
2016-12-05 22:36:06 +0100

    Decreased minimum blood sugar value to 1 mg/dL

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/BloodSugarPreference.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/BloodSugarPreference.java
@@ -73,8 +73,7 @@ public class BloodSugarPreference extends EditTextPreference {
             @Override
             public void onClick(View v)
             {
-                if(Validator.validateEditTextEvent(context, editTextValue, Measurement.Category.BLOODSUGAR, true))
-                {
+                if (Validator.validateEditTextEvent(context, editTextValue, Measurement.Category.BLOODSUGAR, true)) {
                     alertDialog.dismiss();
                     onDialogClosed(true);
                 }

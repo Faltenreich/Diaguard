c5eb93c755bccf70b3b049aa36ec35cc41f4964a
Faltenreich <philipp.fahlteich@gmail.com>
2017-11-06 19:05:19 +0100

    Reduced lag in timeline while paging quickly

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/ChartDayFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/ChartDayFragment.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.faltenreich.diaguard.R;
-import com.faltenreich.diaguard.ui.view.chart.CategoryTable;
+import com.faltenreich.diaguard.adapter.CategoryRecyclerAdapter;
+import com.faltenreich.diaguard.adapter.SimpleDividerItemDecoration;
+import com.faltenreich.diaguard.adapter.list.ListItemCategoryValues;
+import com.faltenreich.diaguard.data.PreferenceHelper;
+import com.faltenreich.diaguard.data.entity.Measurement;
 import com.faltenreich.diaguard.ui.view.chart.DayChart;
+import com.faltenreich.diaguard.util.thread.BaseAsyncTask;
+import com.faltenreich.diaguard.util.thread.TimelineTableTask;
 
 import org.joda.time.DateTime;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,10 +37,14 @@ public class ChartDayFragment extends Fragment {
     public static final String EXTRA_DATE_TIME = "EXTRA_DATE_TIME";
 
     @BindView(R.id.day_chart) DayChart dayChart;
-    @BindView(R.id.category_table) CategoryTable categoryTable;
+    @BindView(R.id.category_table) RecyclerView categoryTable;
 
     private DateTime day;
     private RecyclerView.OnScrollListener onScrollListener;
+    private CategoryRecyclerAdapter adapter;
+    private Measurement.Category[] categories;
+    private List<ListItemCategoryValues> tableValues;
+    private boolean isVisible;
 
     public static ChartDayFragment createInstance(DateTime dateTime) {
         ChartDayFragment fragment = new ChartDayFragment();
@@ -60,9 +74,48 @@ public class ChartDayFragment extends Fragment {
         init();
     }
 
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        this.isVisible = isVisibleToUser;
+    }
+
     private void init() {
+        Measurement.Category[] activeCategories = PreferenceHelper.getInstance().getActiveCategories();
+        categories = Arrays.copyOfRange(activeCategories, 1, activeCategories.length);
+        categoryTable.addItemDecoration(new SimpleDividerItemDecoration(getContext()));
+        categoryTable.setLayoutManager(new LinearLayoutManager(getContext()));
+        categoryTable.setHasFixedSize(true);
+        adapter = new CategoryRecyclerAdapter(getContext());
+        categoryTable.setAdapter(adapter);
         categoryTable.addOnScrollListener(onScrollListener);
         setDay(day);
+    }
+
+    private void invalidate() {
+        if (dayChart != null) {
+            dayChart.setDay(day);
+        }
+        if (categoryTable != null) {
+            new TimelineTableTask(getContext(), day, categories, new BaseAsyncTask.OnAsyncProgressListener<List<ListItemCategoryValues>>() {
+                @Override
+                public void onPostExecute(List<ListItemCategoryValues> values) {
+                    tableValues = values;
+                    if (isVisible) {
+                        update();
+                    }
+                }
+            }).execute();
+        }
+    }
+
+    public void update() {
+        if (tableValues != null) {
+            // Other notify methods lead to rendering issues on view paging
+            adapter.clear();
+            adapter.addItems(tableValues);
+            adapter.notifyDataSetChanged();
+        }
     }
 
     public DateTime getDay() {
@@ -71,12 +124,7 @@ public class ChartDayFragment extends Fragment {
 
     public void setDay(DateTime day) {
         this.day = day;
-        if (dayChart != null) {
-            dayChart.setDay(day);
-        }
-        if (categoryTable != null) {
-            categoryTable.setDay(day);
-        }
+        invalidate();
     }
 
     public void setOnScrollListener(RecyclerView.OnScrollListener onScrollListener) {
@@ -84,6 +132,6 @@ public class ChartDayFragment extends Fragment {
     }
 
     public void scrollTo(int yOffset) {
-        categoryTable.scrollTo(yOffset);
+        categoryTable.scrollBy(0, yOffset - categoryTable.computeVerticalScrollOffset());
     }
 }

fce806ffeb5473773f1f66a1f3e577ac3506ee4c
Faltenreich <philipp.fahlteich@gmail.com>
2017-11-15 21:30:49 +0100

    Improved log initialization logic

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/LogFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/LogFragment.java
@@ -81,7 +81,6 @@ public class LogFragment extends DateFragment implements LogRecyclerAdapter.OnAd
             }
         });
 
-        progressBar.setVisibility(View.VISIBLE);
         goToDay(getDay());
     }
 

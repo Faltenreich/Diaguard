ce072147f7843f6803034cb67343bd4d41f6ea01
Faltenreich <philipp.fahlteich@gmail.com>
2017-12-31 15:30:22 +0100

    Show Toast with category on clicking timeline table image

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/viewholder/CategoryImageViewHolder.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/viewholder/CategoryImageViewHolder.java
@@ -2,10 +2,12 @@ package com.faltenreich.diaguard.ui.view.viewholder;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.adapter.list.ListItemCategoryImage;
 import com.faltenreich.diaguard.data.PreferenceHelper;
+import com.faltenreich.diaguard.util.ViewUtils;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
@@ -13,12 +15,13 @@ import butterknife.BindView;
 /**
  * Created by Faltenreich on 17.10.2015.
  */
-public class CategoryImageViewHolder extends BaseViewHolder<ListItemCategoryImage> {
+public class CategoryImageViewHolder extends BaseViewHolder<ListItemCategoryImage> implements View.OnClickListener {
 
     @BindView(R.id.category_image) ImageView imageView;
 
     public CategoryImageViewHolder(View view) {
         super(view);
+        view.setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +30,10 @@ public class CategoryImageViewHolder extends BaseViewHolder<ListItemCategoryImag
         if (categoryImageResourceId > 0) {
             Picasso.with(getContext()).load(categoryImageResourceId).into(imageView);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        ViewUtils.showToast(getContext(), getListItem().getCategory().toLocalizedString(), Toast.LENGTH_SHORT);
     }
 }

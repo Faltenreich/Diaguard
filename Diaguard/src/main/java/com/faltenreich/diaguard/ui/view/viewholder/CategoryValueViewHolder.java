b9c9e8eec4d8d0855fd6e864b441de28974cffe6
Faltenreich <philipp.fahlteich@gmail.com>
2017-12-31 15:46:32 +0100

    Show Toast with uniton clicking timeline table value

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/viewholder/CategoryValueViewHolder.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/viewholder/CategoryValueViewHolder.java
@@ -1,26 +1,31 @@
 package com.faltenreich.diaguard.ui.view.viewholder;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.adapter.list.ListItemCategoryValue;
 import com.faltenreich.diaguard.data.PreferenceHelper;
 import com.faltenreich.diaguard.data.entity.Measurement;
 import com.faltenreich.diaguard.util.Helper;
+import com.faltenreich.diaguard.util.ViewUtils;
 
 import butterknife.BindView;
 
 /**
  * Created by Faltenreich on 17.10.2015.
  */
-public class CategoryValueViewHolder extends BaseViewHolder<ListItemCategoryValue> {
+public class CategoryValueViewHolder extends BaseViewHolder<ListItemCategoryValue> implements View.OnClickListener {
 
+    @BindView(R.id.category_value_container) ViewGroup container;
     @BindView(R.id.category_value_1) TextView textViewOne;
     @BindView(R.id.category_value_2) TextView textViewTwo;
 
     public CategoryValueViewHolder(View view) {
         super(view);
+        container.setOnClickListener(this);
     }
 
     @Override
@@ -43,5 +48,13 @@ public class CategoryValueViewHolder extends BaseViewHolder<ListItemCategoryValu
             textViewTwo.setVisibility(View.GONE);
             textViewTwo.setText(null);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        // TODO: Use Measurement.toString() instead
+        Measurement.Category category = getListItem().getCategory();
+        String unitAcronym = PreferenceHelper.getInstance().getUnitName(category);
+        ViewUtils.showToast(getContext(), unitAcronym, Toast.LENGTH_SHORT);
     }
 }

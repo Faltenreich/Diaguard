934c819beffceadc5549ba62c68cab4bbfe482b5
Faltenreich <philipp.fahlteich@gmail.com>
2018-08-22 20:21:44 +0200

    Set key for new tags Disable submit button temporarily to prevent duplicate entries

--- Diaguard/src/main/java/com/faltenreich/diaguard/data/dao/TagDao.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/data/dao/TagDao.java
@@ -1,6 +1,7 @@
 package com.faltenreich.diaguard.data.dao;
 
 import com.faltenreich.diaguard.data.entity.Tag;
+import com.faltenreich.diaguard.util.StringUtils;
 
 public class TagDao extends BaseDao<Tag> {
 
@@ -15,5 +16,13 @@ public class TagDao extends BaseDao<Tag> {
 
     private TagDao() {
         super(Tag.class);
+    }
+
+    @Override
+    public Tag createOrUpdate(Tag tag) {
+        if (StringUtils.isBlank(tag.getKey())) {
+            tag.setKey(tag.getName());
+        }
+        return super.createOrUpdate(tag);
     }
 }

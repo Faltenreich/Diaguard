33a7c6c1d289af33db181f148f5251d999cabfff
Faltenreich <philipp.fahlteich@gmail.com>
2018-08-25 23:33:36 +0200

    Refactored backup preferences

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/BackupPreference.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/preferences/BackupPreference.java
@@ -1,24 +1,22 @@
 package com.faltenreich.diaguard.ui.view.preferences;
 
 import android.content.Context;
-import android.preference.Preference;
 import android.util.AttributeSet;
 
-import com.faltenreich.diaguard.event.Events;
 import com.faltenreich.diaguard.event.PermissionRequestEvent;
 import com.faltenreich.diaguard.util.permission.Permission;
 import com.faltenreich.diaguard.util.permission.PermissionUseCase;
 
-public class BackupPreference extends Preference implements Preference.OnPreferenceClickListener {
+abstract class BackupPreference extends ClickablePreference<PermissionRequestEvent> {
 
     public BackupPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setOnPreferenceClickListener(this);
     }
 
+    abstract PermissionUseCase getUseCase();
+
     @Override
-    public boolean onPreferenceClick(Preference preference) {
-        Events.post(new PermissionRequestEvent(Permission.WRITE_EXTERNAL_STORAGE, PermissionUseCase.BACKUP_WRITE));
-        return true;
+    PermissionRequestEvent getEvent() {
+        return new PermissionRequestEvent(Permission.WRITE_EXTERNAL_STORAGE, getUseCase());
     }
 }

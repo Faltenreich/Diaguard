7d1ce8b9e25e3c39d25effe30ab150cf7ec8dc7e
Faltenreich <philipp.fahlteich@gmail.com>
2018-09-24 23:50:28 +0200

    Fixed animation for opening activity with reveal on Pre-Lollipop Updated Gradle

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/EntrySearchActivity.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/EntrySearchActivity.java
@@ -2,6 +2,7 @@ package com.faltenreich.diaguard.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.view.View;
 
@@ -14,14 +15,7 @@ import com.faltenreich.diaguard.util.ViewUtils;
 public class EntrySearchActivity extends BaseSearchActivity {
 
     private static Intent getIntent(Context context, @Nullable View source) {
-        Intent intent = new Intent(context, EntrySearchActivity.class);
-        if (source != null) {
-            Vector2D position = ViewUtils.getPositionOnScreen(source);
-            intent.putExtra(BaseActivity.ARGUMENT_REVEAL_X, position.x + (source.getWidth() / 2));
-            intent.putExtra(BaseActivity.ARGUMENT_REVEAL_Y, position.y + (source.getHeight() / 2));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        }
-        return intent;
+        return BaseActivity.getIntent(EntrySearchActivity.class, context, source);
     }
 
     public static void show(Context context, @Nullable View source) {

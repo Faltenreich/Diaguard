7a0007028f8ccb3fa37426bfcefc88e9ced7eaed
Faltenreich <philipp.fahlteich@gmail.com>
2018-09-30 19:17:41 +0200

    Refactoring

--- Diaguard/src/main/java/com/faltenreich/diaguard/adapter/SafeLinearLayoutManager.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/adapter/SafeLinearLayoutManager.java
@@ -3,26 +3,14 @@ package com.faltenreich.diaguard.adapter;
 import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
 import android.util.Log;
 
-/**
- * Created by Faltenreich on 03.12.2015.
- */
 public class SafeLinearLayoutManager extends LinearLayoutManager {
 
     private static final String TAG = SafeLinearLayoutManager.class.getSimpleName();
 
     public SafeLinearLayoutManager(Context context) {
         super(context);
-    }
-
-    public SafeLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
-        super(context, orientation, reverseLayout);
-    }
-
-    public SafeLinearLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override

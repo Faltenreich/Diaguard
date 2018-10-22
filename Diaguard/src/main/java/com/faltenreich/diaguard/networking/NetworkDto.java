e7b52e139e42d4adba042189e672353111917a32
Faltenreich <philipp.fahlteich@gmail.com>
2018-10-22 20:05:36 +0200

    Refactoring

--- Diaguard/src/main/java/com/faltenreich/diaguard/networking/NetworkDto.java
+++
@@ -1,8 +0,0 @@
-package com.faltenreich.diaguard.networking;
-
-/**
- * Created by Faltenreich on 23.09.2016.
- */
-
-public class NetworkDto {
-}
--- Diaguard/src/main/java/com/faltenreich/diaguard/networking/openfoodfacts/dto/SearchResponseDto.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/networking/openfoodfacts/dto/SearchResponseDto.java
@@ -1,16 +1,10 @@
 package com.faltenreich.diaguard.networking.openfoodfacts.dto;
 
-import com.faltenreich.diaguard.networking.NetworkDto;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-/**
- * Created by Faltenreich on 25.09.2016.
- */
-
-public class SearchResponseDto extends NetworkDto {
-
+public class SearchResponseDto {
     @SerializedName("products")
     public List<ProductDto> products;
 }

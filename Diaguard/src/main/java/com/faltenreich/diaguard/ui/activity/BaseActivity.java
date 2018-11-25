fe166df36b2d987163f90f0b7cd54f1a9410e8e5
Faltenreich <philipp.fahlteich@gmail.com>
2018-11-25 17:41:39 +0100

    Prepared dark mode

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/BaseActivity.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/activity/BaseActivity.java
@@ -82,6 +82,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_Dark);
         setContentView(layoutResourceId);
         ButterKnife.bind(this);
 

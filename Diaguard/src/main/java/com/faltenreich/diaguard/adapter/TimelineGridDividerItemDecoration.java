3543bb5845aa698db3564264694bca0a21be47b8
Faltenreich <philipp.fahlteich@gmail.com>
2019-02-05 20:16:59 +0100

    Adapted background colors

--- Diaguard/src/main/java/com/faltenreich/diaguard/adapter/TimelineGridDividerItemDecoration.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/adapter/TimelineGridDividerItemDecoration.java
@@ -15,7 +15,7 @@ public class TimelineGridDividerItemDecoration extends RecyclerView.ItemDecorati
     private Drawable divider;
 
     public TimelineGridDividerItemDecoration(Context context) {
-        divider = ContextCompat.getDrawable(context, R.drawable.line_divider);
+        divider = ContextCompat.getDrawable(context, R.drawable.separator);
     }
 
     @Override

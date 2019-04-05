6f7731002c08e6bc8afaba95229acea057c66b52
Faltenreich <philipp.fahlteich@gmail.com>
2019-04-05 18:01:25 +0200

    Tint background of category list item on drag

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/list/viewholder/CategoryViewHolder.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/list/viewholder/CategoryViewHolder.java
@@ -1,18 +1,25 @@
 package com.faltenreich.diaguard.ui.list.viewholder;
 
+import android.graphics.Color;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.data.PreferenceHelper;
 import com.faltenreich.diaguard.data.entity.Measurement;
+import com.faltenreich.diaguard.ui.list.helper.Selectable;
+import com.faltenreich.diaguard.util.ResourceUtils;
 import com.faltenreich.diaguard.util.ViewUtils;
+import com.faltenreich.diaguard.util.theme.ThemeUtils;
 import com.google.android.material.internal.ContextUtils;
 
+import androidx.core.graphics.ColorUtils;
 import butterknife.BindView;
 
-public class CategoryViewHolder extends BaseViewHolder<Measurement.Category> {
+public class CategoryViewHolder extends BaseViewHolder<Measurement.Category> implements Selectable {
 
+    @BindView(R.id.background) ViewGroup background;
     @BindView(R.id.checkBox) CheckBox checkBox;
     @BindView(R.id.dragView) View dragView;
 
@@ -28,5 +35,10 @@ public class CategoryViewHolder extends BaseViewHolder<Measurement.Category> {
         checkBox.setChecked(PreferenceHelper.getInstance().isCategoryActive(category));
         checkBox.setOnCheckedChangeListener((view, isChecked) -> PreferenceHelper.getInstance().setIsCategoryActive(category, isChecked));
         dragView.setOnClickListener(view -> ViewUtils.showToast(getContext(), R.string.drag_drop_hint));
+    }
+
+    @Override
+    public void setSelected(boolean isSelected) {
+        background.setBackgroundColor(isSelected ? ResourceUtils.getBackgroundSecondary(getContext()) : ResourceUtils.getBackgroundPrimary(getContext()));
     }
 }

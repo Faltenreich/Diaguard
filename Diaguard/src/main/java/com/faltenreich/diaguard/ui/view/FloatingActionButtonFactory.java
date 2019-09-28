45f4ef41c14e300211d1158ad72cc5862ddcd194
Faltenreich <philipp.fahlteich@gmail.com>
2019-09-28 12:57:36 +0200

    Catch categories for fab with invalid image resource identifiers

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/FloatingActionButtonFactory.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/view/FloatingActionButtonFactory.java
@@ -1,23 +1,29 @@
 package com.faltenreich.diaguard.ui.view;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.util.Log;
 
 import androidx.annotation.ColorInt;
-import androidx.annotation.ColorRes;
 import androidx.annotation.DrawableRes;
-import androidx.core.content.ContextCompat;
 
 import com.faltenreich.diaguard.util.Helper;
 import com.github.clans.fab.FloatingActionButton;
 
 public class FloatingActionButtonFactory {
 
+    private static final String TAG = FloatingActionButtonFactory.class.getSimpleName();
+
     public static FloatingActionButton createFloatingActionButton(Context context, String text, @DrawableRes int imageResourceId, @ColorInt int color) {
         FloatingActionButton fab = new FloatingActionButton(context);
         fab.setButtonSize(FloatingActionButton.SIZE_MINI);
         fab.setLabelText(text);
-        fab.setImageResource(imageResourceId);
+        try {
+            fab.setImageResource(imageResourceId);
+        } catch (Resources.NotFoundException exception) {
+            Log.e(TAG, exception.getMessage() != null ? exception.getMessage() : "Resources.NotFoundException: " + text);
+        }
         fab.setColorNormal(color);
         float brighteningPercentage = color == Color.WHITE ? .9f : 1.2f;
         int colorHighlight = Helper.colorBrighten(color, brighteningPercentage);

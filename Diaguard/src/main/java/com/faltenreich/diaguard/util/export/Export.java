4b07dc45a251b0f7c7b61d99bfa1d92411bf7199
Faltenreich <philipp.fahlteich@gmail.com>
2019-09-28 12:40:52 +0200

    Updated targetSdkVersion to Android 10

--- Diaguard/src/main/java/com/faltenreich/diaguard/util/export/Export.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/util/export/Export.java
@@ -13,24 +13,13 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-/**
- * Created by Faltenreich on 21.10.2015.
- */
 public class Export {
 
     public static final String BACKUP_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    private static final String FILE_BACKUP_1_1_PREFIX = "backup";
-    private static final String FILE_BACKUP_1_1_DATE_FORMAT = "yyyyMMddHHmmss";
-    private static final String FILE_BACKUP_1_1_REGEX = "backup[0-9]{14}.csv";
-
     private static final String FILE_BACKUP_1_3_PREFIX = "diaguard_backup_";
     private static final String FILE_BACKUP_1_3_DATE_FORMAT = "yyyyMMddHHmmss";
-    private static final String FILE_BACKUP_1_3_REGEX = "diaguard_backup_[0-9]{14}.csv";
 
     public enum FileType {
         CSV,
@@ -48,12 +37,12 @@ public class Export {
         }
     }
 
-    public static final String PDF_MIME_TYPE = "application/pdf";
+    static final String PDF_MIME_TYPE = "application/pdf";
 
-    public static final String CSV_MIME_TYPE = "text/csv";
+    static final String CSV_MIME_TYPE = "text/csv";
     public static final String CSV_IMPORT_MIME_TYPE = "text/*"; // Workaround: text/csv does not work for all apps
     public static final char CSV_DELIMITER = ';';
-    public static final String CSV_KEY_META = "meta";
+    static final String CSV_KEY_META = "meta";
 
     public static void exportPdf(Context context, DateTime dateStart, DateTime dateEnd, Measurement.Category[] categories, FileListener listener) {
         dateStart = dateStart != null ? dateStart.withTimeAtStartOfDay() : null;
@@ -90,7 +79,7 @@ public class Export {
         csvImport.execute();
     }
 
-    public static File getExportFile(FileType fileType) {
+    static File getExportFile(FileType fileType) {
         String fileName = String.format("%s%s%s_%s.%s",
                 FileUtils.getPublicDirectory(),
                 File.separator,
@@ -100,7 +89,7 @@ public class Export {
         return new File(fileName);
     }
 
-    public static File getBackupFile(FileType fileType) {
+    static File getBackupFile(FileType fileType) {
         String fileName = String.format("%s%s%s%s.%s",
                 FileUtils.getPublicDirectory(),
                 File.separator,
@@ -108,51 +97,5 @@ public class Export {
                 DateTimeFormat.forPattern(FILE_BACKUP_1_3_DATE_FORMAT).print(DateTime.now()),
                 fileType.getExtension());
         return new File(fileName);
-    }
-
-    public static DateTime getBackupDate(File file) {
-        String fileName = file.getName();
-        if (fileName.matches(String.format(".*%s", FILE_BACKUP_1_1_REGEX))) {
-            String dateString = getBackupDateString(fileName, FILE_BACKUP_1_1_PREFIX);
-            return DateTimeFormat.forPattern(FILE_BACKUP_1_1_DATE_FORMAT).parseDateTime(dateString);
-        } else if (fileName.matches(String.format(".*%s", FILE_BACKUP_1_3_REGEX))) {
-            String dateString = getBackupDateString(fileName, FILE_BACKUP_1_3_PREFIX);
-            return DateTimeFormat.forPattern(FILE_BACKUP_1_3_DATE_FORMAT).parseDateTime(dateString);
-        } else {
-            return null;
-        }
-    }
-
-    private static String getBackupDateString(String fileName, String prefix) {
-        int start = fileName.lastIndexOf(prefix) + prefix.length();
-        int end = fileName.lastIndexOf(FileUtils.POINT);
-        return fileName.substring(start, end);
-    }
-
-    public static List<File> getBackupFiles() {
-        List<File> files = new ArrayList<>();
-
-        File[] privateFiles = FileUtils.getPrivateDirectory().listFiles();
-        File[] publicFiles = FileUtils.getPublicDirectory().listFiles();
-
-        if (privateFiles != null && privateFiles.length > 0) {
-            files.addAll(new ArrayList<>(Arrays.asList(privateFiles)));
-        }
-        if (publicFiles != null && publicFiles.length > 0) {
-            files.addAll(new ArrayList<>(Arrays.asList(publicFiles)));
-        }
-
-        List<File> csvFiles = new ArrayList<>();
-        for (File file : files) {
-            if (isBackupFile(file)) {
-                csvFiles.add(0, file);
-            }
-        }
-        return csvFiles;
-    }
-
-    private static boolean isBackupFile(File file) {
-        return file.getName().matches(String.format(".*%s", FILE_BACKUP_1_1_REGEX)) ||
-                file.getName().matches(String.format(".*%s", FILE_BACKUP_1_3_REGEX));
     }
 }

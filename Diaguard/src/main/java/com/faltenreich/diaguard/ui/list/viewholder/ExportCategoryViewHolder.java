15a6a9904de1d616335b4c240c4f725792b1e205
Faltenreich <philipp.fahlteich@gmail.com>
2019-09-30 18:41:33 +0200

    Set view holder content Added divider decoration to category check box list

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/list/viewholder/ExportCategoryViewHolder.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/list/viewholder/ExportCategoryViewHolder.java
@@ -1,10 +1,21 @@
 package com.faltenreich.diaguard.ui.list.viewholder;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
+import com.faltenreich.diaguard.R;
+import com.faltenreich.diaguard.data.PreferenceHelper;
+import com.faltenreich.diaguard.data.entity.Measurement;
 import com.faltenreich.diaguard.ui.list.item.ListItemExportCategory;
 
+import butterknife.BindView;
+
 public class ExportCategoryViewHolder extends BaseViewHolder<ListItemExportCategory> {
+
+    @BindView(R.id.category_image) ImageView categoryImageView;
+    @BindView(R.id.category_checkbox) CheckBox categoryCheckBox;
+    @BindView(R.id.extra_checkbox) CheckBox extraCheckBox;
 
     public ExportCategoryViewHolder(View view) {
         super(view);
@@ -12,7 +23,11 @@ public class ExportCategoryViewHolder extends BaseViewHolder<ListItemExportCateg
 
     @Override
     protected void bindData() {
-
-
+        ListItemExportCategory item = getListItem();
+        Measurement.Category category = item.getCategory();
+        categoryImageView.setImageResource(PreferenceHelper.getInstance().getCategoryImageResourceId(category));
+        categoryCheckBox.setText(category.toLocalizedString(getContext()));
+        categoryCheckBox.setChecked(item.isCategorySelected());
+        extraCheckBox.setChecked(item.isExtraSelected());
     }
 }

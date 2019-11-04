97994ab1e45c80acb9227cbe6355efcbd11183cc
Faltenreich <philipp.fahlteich@gmail.com>
2019-11-04 21:38:38 +0100

    Improved multiline pdf cell by calculating width via font instead of character count

--- Diaguard/src/main/java/com/faltenreich/diaguard/export/pdf/view/MultilineCellPdfView.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/export/pdf/view/MultilineCellPdfView.java
@@ -1,5 +1,6 @@
 package com.faltenreich.diaguard.export.pdf.view;
 
+import com.faltenreich.diaguard.util.StringUtils;
 import com.pdfjet.Cell;
 import com.pdfjet.Font;
 import com.pdfjet.Page;
@@ -10,50 +11,51 @@ import java.io.IOException;
 
 public class MultilineCellPdfView extends Cell {
 
-    private final int characterCount;
+    private int characterCount;
 
-    public MultilineCellPdfView(Font font, String content, int characterCount) {
-        super(font, content);
-        this.characterCount = characterCount;
+    public MultilineCellPdfView(Font font, String text, float width) {
+        super(font);
+        setWidth(width);
+        setText(text);
+        characterCount = font.getFitChars(text, width);
     }
 
     @Override
     public String getText() {
-        return WordUtils.wrap(super.getText(), this.characterCount);
+        return WordUtils.wrap(super.getText(), characterCount);
     }
 
     @Override
     public float getHeight() {
-        float height = this.font.getBodyHeight();
-
+        float height = font.getBodyHeight();
         String text = getText();
-        if (text != null) {
-            String[] wrappedTexts = text.split(System.getProperty("line.separator"));
 
+        if (text != null) {
+            String[] wrappedTexts = text.split(StringUtils.newLine());
             if (wrappedTexts.length > 1) {
-                return (height * wrappedTexts.length) + this.top_padding + this.bottom_padding;
+                return (height * wrappedTexts.length) + top_padding + bottom_padding;
             }
         }
-        return height + this.top_padding + this.bottom_padding;
+        return height + top_padding + bottom_padding;
     }
 
     @Override
     protected void paint(Page page, float x, float y, float w, float h) throws Exception {
-        page.setPenColor(this.getPenColor());
-        page.setPenWidth(this.lineWidth);
+        page.setPenColor(getPenColor());
+        page.setPenWidth(lineWidth);
         drawText(page, x, y);
     }
 
     private void drawText(Page page, float x, float y) throws IOException {
         String wrappedText = WordUtils.wrap(super.getText(), this.characterCount);
-        String[] lines = wrappedText.split(System.getProperty("line.separator"));
+        String[] lines = wrappedText.split(StringUtils.newLine());
 
-        float x_text = x + this.left_padding;
-        float y_text = y + this.font.getAscent() + this.top_padding;
+        float x_text = x + left_padding;
+        float y_text = y + font.getAscent() + top_padding;
 
         for (String line : lines) {
-            page.drawString(this.font, line, x_text, y_text);
-            y_text += this.font.getBodyHeight();
+            page.drawString(font, line, x_text, y_text);
+            y_text += font.getBodyHeight();
         }
     }
 }

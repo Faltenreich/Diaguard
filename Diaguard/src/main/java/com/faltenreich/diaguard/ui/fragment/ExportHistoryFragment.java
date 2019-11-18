0826c3f631bdbee89e502240a7df66211cb3be17
Faltenreich <philipp.fahlteich@gmail.com>
2019-11-18 20:01:23 +0100

    Prepared ui of export history

---
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/ExportHistoryFragment.java
@@ -0,0 +1,10 @@
+package com.faltenreich.diaguard.ui.fragment;
+
+import com.faltenreich.diaguard.R;
+
+public class ExportHistoryFragment extends BaseFragment {
+
+    public ExportHistoryFragment() {
+        super(R.layout.fragment_export_history, R.string.export_history);
+    }
+}

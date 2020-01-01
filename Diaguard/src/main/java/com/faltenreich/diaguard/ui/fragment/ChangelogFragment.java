718d4eee90b1117f1781b8e853d7c4e1d4719663
Faltenreich <philipp.fahlteich@gmail.com>
2020-01-01 21:00:45 +0100

    Added changelog

--- Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/ChangelogFragment.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/ui/fragment/ChangelogFragment.java
@@ -8,16 +8,24 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.faltenreich.diaguard.R;
+import com.faltenreich.diaguard.ui.activity.MainActivity;
 
 public class ChangelogFragment extends DialogFragment {
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getContext())
+        return new AlertDialog.Builder(requireContext())
             .setTitle(R.string.changelog)
             .setMessage(R.string.changelog_desc)
-            .setPositiveButton(R.string.ok, (dlg, which) -> { })
+            .setNegativeButton(R.string.got_it, (dlg, which) -> { })
+            .setPositiveButton(R.string.show_me, (dlg, which) -> {
+                dismiss();
+                if (getActivity() instanceof MainActivity) {
+                    MainActivity activity = (MainActivity) getActivity();
+                    activity.showFragment(new ExportFragment(), null, true);
+                }
+            })
             .create();
     }
 }

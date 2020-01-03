55592b13509abd667656132bda60c88190ed15cd
Faltenreich <philipp.fahlteich@gmail.com>
2020-01-03 13:23:21 +0100

    Extracted logic for creating pojos and cells for notes into factories

---
+++ Diaguard/src/main/java/com/faltenreich/diaguard/export/pdf/view/CellFactory.java
@@ -0,0 +1,53 @@
+package com.faltenreich.diaguard.export.pdf.view;
+
+import com.faltenreich.diaguard.export.pdf.meta.PdfExportCache;
+import com.faltenreich.diaguard.export.pdf.meta.PdfNote;
+import com.faltenreich.diaguard.util.Helper;
+import com.pdfjet.Border;
+import com.pdfjet.Cell;
+import com.pdfjet.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CellFactory {
+
+    public static List<List<Cell>> createRowsForNotes(PdfExportCache cache, List<PdfNote> pdfNotes, float labelWidth) {
+        List<List<Cell>> rows = new ArrayList<>();
+        for (PdfNote note : pdfNotes) {
+            boolean isFirst = pdfNotes.indexOf(note) == 0;
+            boolean isLast = pdfNotes.indexOf(note) == pdfNotes.size() - 1;
+
+            ArrayList<Cell> noteCells = new ArrayList<>();
+
+            Cell timeCell = new CellBuilder(new Cell(cache.getFontNormal()))
+                .setWidth(labelWidth)
+                .setText(Helper.getTimeFormat().print(note.getDateTime()))
+                .setForegroundColor(Color.gray)
+                .build();
+            if (isFirst) {
+                timeCell.setBorder(Border.TOP, true);
+            }
+            if (isLast) {
+                timeCell.setBorder(Border.BOTTOM, true);
+            }
+            noteCells.add(timeCell);
+
+            Cell noteCell = new CellBuilder(new MultilineCell(cache.getFontNormal()))
+                .setWidth(cache.getPage().getWidth() -labelWidth)
+                .setText(note.getNote())
+                .setForegroundColor(Color.gray)
+                .build();
+            if (isFirst) {
+                noteCell.setBorder(Border.TOP, true);
+            }
+            if (isLast) {
+                noteCell.setBorder(Border.BOTTOM, true);
+            }
+            noteCells.add(noteCell);
+
+            rows.add(noteCells);
+        }
+        return rows;
+    }
+}

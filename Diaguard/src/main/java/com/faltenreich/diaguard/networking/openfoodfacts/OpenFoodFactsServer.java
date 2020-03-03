8675cb75892e4f03db716fb5fc05a05c115af882
Faltenreich <philipp.fahlteich@gmail.com>
2020-03-03 20:03:54 +0100

    Fixed url to OpenFoodFacts for language code: en Fixed search request to OpenFoodFacts

--- Diaguard/src/main/java/com/faltenreich/diaguard/networking/openfoodfacts/OpenFoodFactsServer.java
+++ Diaguard/src/main/java/com/faltenreich/diaguard/networking/openfoodfacts/OpenFoodFactsServer.java
@@ -5,14 +5,18 @@ import com.faltenreich.diaguard.util.Helper;
 
 public class OpenFoodFactsServer extends NetworkServer<OpenFoodFactsApi> {
 
-    private static final String HOST_FORMAT = "https://world-%s.openfoodfacts.org/";
-
     OpenFoodFactsServer() {
         super(OpenFoodFactsApi.class);
     }
 
     @Override
     public String getBaseUrl() {
-        return String.format(HOST_FORMAT, Helper.getLanguageCode());
+        String languageCode = Helper.getLanguageCode();
+        // Special case: "en" must be stripped for api to work
+        if (languageCode.equals("en")) {
+            return "https://world.openfoodfacts.org/";
+        } else {
+            return String.format("https://world-%s.openfoodfacts.org/", languageCode);
+        }
     }
 }

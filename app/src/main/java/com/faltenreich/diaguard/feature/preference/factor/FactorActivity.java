adfc71e93b57254730d80c03d31ace554a9d1ae3
Faltenreich <philipp.fahlteich@gmail.com>
2020-05-22 14:01:14 +0200

    Added stubs for general factor preference

---
+++ app/src/main/java/com/faltenreich/diaguard/feature/preference/factor/FactorActivity.java
@@ -0,0 +1,11 @@
+package com.faltenreich.diaguard.feature.preference.factor;
+
+import com.faltenreich.diaguard.R;
+import com.faltenreich.diaguard.shared.view.activity.BaseActivity;
+
+public class FactorActivity extends BaseActivity {
+
+    public FactorActivity() {
+        super(R.layout.activity_factor);
+    }
+}
---
+++ app/src/main/java/com/faltenreich/diaguard/feature/preference/factor/FactorFragment.java
@@ -0,0 +1,11 @@
+package com.faltenreich.diaguard.feature.preference.factor;
+
+import com.faltenreich.diaguard.R;
+import com.faltenreich.diaguard.shared.view.fragment.BaseFragment;
+
+public class FactorFragment extends BaseFragment {
+
+    public FactorFragment() {
+        super(R.layout.fragment_factor, R.string.factors);
+    }
+}

55f7270b25e5178e564c3676b69310f36faa39ae
Faltenreich <philipp.fahlteich@gmail.com>
2020-09-06 16:36:34 +0200

    Updated fastlane:screengrab

--- app/src/androidTest/java/com/faltenreich/diaguard/screenshot/ScreenshotGrabber.java
+++ app/src/androidTest/java/com/faltenreich/diaguard/screenshot/ScreenshotGrabber.java
@@ -1,15 +1,10 @@
 package com.faltenreich.diaguard.screenshot;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.feature.entry.edit.EntryEditActivity;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -26,18 +21,12 @@ public class ScreenshotGrabber {
 
     @Before
     public void setup() {
-        ActivityScenario.launch(EntryEditActivity.class);
+        Screengrab.setDefaultScreenshotStrategy(Screengrab.getDefaultScreenshotStrategy());
     }
 
     @Test
     public void testTakeScreenshot() {
-        Screengrab.screenshot("before_button_click");
-        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click());
-        Screengrab.screenshot("after_button_click");
-    }
-
-    @Test
-    public void example() {
-        Assert.assertTrue(true);
+        ActivityScenario.launch(EntryEditActivity.class);
+        Screengrab.screenshot("overview");
     }
 }

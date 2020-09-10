f2c37384c680fb8e295cceab537660c917d405aa
Faltenreich <philipp.fahlteich@gmail.com>
2020-09-10 16:38:39 +0200

    Changed screengrab configuration

--- app/src/androidTest/java/com/faltenreich/diaguard/screenshot/ScreenshotGrabber.java
+++ app/src/androidTest/java/com/faltenreich/diaguard/screenshot/ScreenshotGrabber.java
@@ -3,9 +3,8 @@ package com.faltenreich.diaguard.screenshot;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.faltenreich.diaguard.feature.entry.edit.EntryEditActivity;
+import com.faltenreich.diaguard.feature.navigation.MainActivity;
 
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,14 +18,9 @@ public class ScreenshotGrabber {
     @ClassRule
     public static final LocaleTestRule localeTestRule = new LocaleTestRule();
 
-    @Before
-    public void setup() {
-        // Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
-    }
-
     @Test
     public void testTakeScreenshot() {
-        ActivityScenario.launch(EntryEditActivity.class);
-        Screengrab.screenshot("1");
+        ActivityScenario.launch(MainActivity.class);
+        Screengrab.screenshot("test");
     }
 }

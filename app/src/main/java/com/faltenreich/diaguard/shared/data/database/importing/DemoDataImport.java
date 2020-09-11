2b8acdd031338ac1c215928a95fe2c371fba1b92
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2020-09-11 21:28:24 +0200

    Prepared demo data

--- app/src/main/java/com/faltenreich/diaguard/shared/data/database/importing/DemoDataImport.java
+++ app/src/main/java/com/faltenreich/diaguard/shared/data/database/importing/DemoDataImport.java
@@ -1,10 +1,21 @@
 package com.faltenreich.diaguard.shared.data.database.importing;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+
+import com.faltenreich.diaguard.feature.export.job.Export;
+
+import java.lang.ref.WeakReference;
 
 class DemoDataImport implements Importing {
 
-    private static final String TAG = DemoDataImport.class.getSimpleName();
+    private Context context;
+
+    DemoDataImport(Context context) {
+        this.context = context;
+    }
 
     @Override
     public boolean requiresImport() {
@@ -13,14 +24,24 @@ class DemoDataImport implements Importing {
 
     @Override
     public void importData() {
-        new ImportDemoDataTask().execute();
+        new ImportDemoDataTask(context).execute();
     }
 
     private static class ImportDemoDataTask extends AsyncTask<Void, Void, Void> {
 
+        private WeakReference<Context> contextReference;
+
+        ImportDemoDataTask(Context context) {
+            this.contextReference = new WeakReference<>(context);
+        }
+
         @Override
         protected Void doInBackground(Void... params) {
-            // TODO
+            Context context = contextReference.get();
+            AssetManager assetManager = context.getAssets();
+            // TODO: How to get uri from asset?
+            Uri uri = null;
+            Export.importCsv(context, uri, null);
             return null;
         }
     }

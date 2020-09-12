41920356c5d06e0e5f90933afb5f03a0cea90d03
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2020-09-12 14:32:18 +0200

    Executing import in main thread to prevent race conditions

--- app/src/main/java/com/faltenreich/diaguard/shared/data/database/importing/Import.java
+++ app/src/main/java/com/faltenreich/diaguard/shared/data/database/importing/Import.java
@@ -1,11 +1,9 @@
 package com.faltenreich.diaguard.shared.data.database.importing;
 
 import android.content.Context;
-import android.os.AsyncTask;
 
 import com.faltenreich.diaguard.shared.Helper;
 
-import java.lang.ref.WeakReference;
 import java.util.Locale;
 
 public class Import implements Importing {
@@ -25,32 +23,9 @@ public class Import implements Importing {
 
     @Override
     public void importData() {
-        new ImportTask(context, locale).execute();
-    }
-
-    private static class ImportTask extends AsyncTask<Void, Void, Void> {
-
-        private WeakReference<Context> contextReference;
-        private Locale locale;
-
-        private ImportTask(Context context, Locale locale) {
-            this.contextReference = new WeakReference<>(context);
-            this.locale = locale;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Context context = contextReference.get();
-            new TagImport(context, locale).importDataIfNeeded();
-            new FoodImport(context, locale).importDataIfNeeded();
-            new TestDataImport().importDataIfNeeded();
-            new DemoDataImport(context).importDataIfNeeded();
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-        }
+        new TagImport(context, locale).importDataIfNeeded();
+        new FoodImport(context, locale).importDataIfNeeded();
+        new TestDataImport().importDataIfNeeded();
+        new DemoDataImport(context).importDataIfNeeded();
     }
 }

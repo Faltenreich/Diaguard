5412bdbee5b07026c30edf41ed7e4868dd223d98
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2020-09-12 17:59:53 +0200

    Localized demo backup by using international language

--- app/src/main/java/com/faltenreich/diaguard/shared/data/database/importing/DemoDataImport.java
+++ app/src/main/java/com/faltenreich/diaguard/shared/data/database/importing/DemoDataImport.java
@@ -7,18 +7,17 @@ import android.util.Log;
 import com.faltenreich.diaguard.BuildConfig;
 import com.faltenreich.diaguard.feature.export.job.csv.CsvImport;
 import com.faltenreich.diaguard.feature.export.job.date.DemoDateStrategy;
-import com.faltenreich.diaguard.shared.Helper;
 
 import org.joda.time.DateTime;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Locale;
 
 class DemoDataImport implements Importing {
 
     private static final String TAG = DemoDataImport.class.getSimpleName();
-    private static final DateTime maxDate = new DateTime().withDate(2020, 1, 31);
+    private static final String DEMO_BACKUP_FILE_NAME = "backup.csv";
+    private static final DateTime MAXIMUM_DATE_IN_BACKUP = new DateTime().withDate(2020, 1, 31);
 
     private Context context;
 
@@ -33,15 +32,12 @@ class DemoDataImport implements Importing {
 
     @Override
     public void importData() {
-        AssetManager assetManager = context.getAssets();
         try {
-            Locale locale = Helper.getLocale(context);
-            String localeIdentifier = locale.getLanguage();
-            String fileName = String.format("backup/%s.csv", localeIdentifier);
-            InputStream inputStream = assetManager.open(fileName);
+            AssetManager assetManager = context.getAssets();
+            InputStream inputStream = assetManager.open(DEMO_BACKUP_FILE_NAME);
 
             CsvImport csvImport = new CsvImport(inputStream);
-            csvImport.setDateStrategy(new DemoDateStrategy(maxDate));
+            csvImport.setDateStrategy(new DemoDateStrategy(MAXIMUM_DATE_IN_BACKUP));
             csvImport.execute();
 
         } catch (IOException exception) {

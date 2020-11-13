e304d308ebc15d6f965a9783497166e8b8ff8b99
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2020-11-13 21:33:52 +0100

    Renamed layout for food detail

--- app/src/main/java/com/faltenreich/diaguard/feature/food/detail/FoodDetailActivity.java
+++ app/src/main/java/com/faltenreich/diaguard/feature/food/detail/FoodDetailActivity.java
@@ -3,17 +3,17 @@ package com.faltenreich.diaguard.feature.food.detail;
 import android.view.LayoutInflater;
 
 import com.faltenreich.diaguard.R;
-import com.faltenreich.diaguard.databinding.ActivityFoodBinding;
+import com.faltenreich.diaguard.databinding.ActivityFoodDetailBinding;
 import com.faltenreich.diaguard.shared.view.activity.BaseActivity;
 
-public class FoodDetailActivity extends BaseActivity<ActivityFoodBinding> {
+public class FoodDetailActivity extends BaseActivity<ActivityFoodDetailBinding> {
 
     public FoodDetailActivity() {
-        super(R.layout.activity_food);
+        super(R.layout.activity_food_detail);
     }
 
     @Override
-    protected ActivityFoodBinding createBinding(LayoutInflater layoutInflater) {
-        return ActivityFoodBinding.inflate(layoutInflater);
+    protected ActivityFoodDetailBinding createBinding(LayoutInflater layoutInflater) {
+        return ActivityFoodDetailBinding.inflate(layoutInflater);
     }
 }

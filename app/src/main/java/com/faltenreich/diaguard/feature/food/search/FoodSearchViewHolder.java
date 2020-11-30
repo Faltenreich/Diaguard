71c778d10f43edb35bdd0cc7c7f209022ce0a42b
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2020-11-30 20:18:02 +0100

    Converted food search view holder to view binding

--- app/src/main/java/com/faltenreich/diaguard/feature/food/search/FoodSearchViewHolder.java
+++ app/src/main/java/com/faltenreich/diaguard/feature/food/search/FoodSearchViewHolder.java
@@ -2,8 +2,6 @@ package com.faltenreich.diaguard.feature.food.search;
 
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.faltenreich.diaguard.R;
 import com.faltenreich.diaguard.databinding.ListItemFoodSearchBinding;
@@ -12,17 +10,10 @@ import com.faltenreich.diaguard.shared.event.Events;
 import com.faltenreich.diaguard.shared.event.ui.FoodSelectedEvent;
 import com.faltenreich.diaguard.shared.view.recyclerview.viewholder.BaseViewHolder;
 
-import butterknife.BindView;
-
 /**
  * Created by Faltenreich on 11.09.2016.
  */
 class FoodSearchViewHolder extends BaseViewHolder<ListItemFoodSearchBinding, FoodSearchListItem> {
-
-    @BindView(R.id.food_name) TextView name;
-    @BindView(R.id.food_brand) TextView brand;
-    @BindView(R.id.food_carbohydrates) TextView carbohydrates;
-    @BindView(R.id.food_recent) ImageView recentIndicator;
 
     FoodSearchViewHolder(ViewGroup parent) {
         super(parent, R.layout.list_item_food_search);
@@ -37,11 +28,11 @@ class FoodSearchViewHolder extends BaseViewHolder<ListItemFoodSearchBinding, Foo
     @Override
     protected void onBind(FoodSearchListItem item) {
         Food food = item.getFood();
-        name.setText(food.getName());
-        brand.setText(food.getBrand());
-        brand.setVisibility(food.getBrand() != null && food.getBrand().length() > 0 ? View.VISIBLE : View.GONE);
-        carbohydrates.setText(food.getValueForUi());
-        recentIndicator.setVisibility(item.getFoodEaten() != null ? View.VISIBLE : View.GONE);
+        getBinding().nameLabel.setText(food.getName());
+        getBinding().brandLabel.setText(food.getBrand());
+        getBinding().brandLabel.setVisibility(food.getBrand() != null && food.getBrand().length() > 0 ? View.VISIBLE : View.GONE);
+        getBinding().valueLabel.setText(food.getValueForUi());
+        getBinding().recentIndicator.setVisibility(item.getFoodEaten() != null ? View.VISIBLE : View.GONE);
     }
 
     private void selectFood() {

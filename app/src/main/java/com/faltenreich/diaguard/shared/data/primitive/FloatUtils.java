8db2b97e00548e5a94cbf3b469adaa59febdcfbd
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2022-02-27 16:52:20 +0100

    Adding todo

--- app/src/main/java/com/faltenreich/diaguard/shared/data/primitive/FloatUtils.java
+++ app/src/main/java/com/faltenreich/diaguard/shared/data/primitive/FloatUtils.java
@@ -8,8 +8,21 @@ import java.text.DecimalFormatSymbols;
 
 public class FloatUtils {
 
+    private static final int DECIMAL_FRACTION_DIGITS = 2;
+
     public static boolean isValid(Float number) {
         return !number.isNaN() && !number.isInfinite();
+    }
+
+    // TODO: Make scale dynamic via SharedPreferences
+    public static String parseFloat(float number, int scale) {
+        DecimalFormat format = new DecimalFormat("0", DecimalFormatSymbols.getInstance());
+        format.setMaximumFractionDigits(scale);
+        return format.format(number);
+    }
+
+    public static String parseFloat(float number) {
+        return parseFloat(number, DECIMAL_FRACTION_DIGITS);
     }
 
     public static float parseNumber(@NonNull String number) {
@@ -22,19 +35,11 @@ public class FloatUtils {
 
     public static Float parseNullableNumber(@Nullable String number) {
         try {
-            return number != null ? Float.parseFloat(number.trim().replaceAll(",", ".")) : null;
+            return number != null
+                ? Float.parseFloat(number.trim().replaceAll(",", "."))
+                : null;
         } catch (NumberFormatException exception) {
             return null;
         }
-    }
-
-    public static String parseFloat(float number, int scale) {
-        DecimalFormat format = new DecimalFormat("0", DecimalFormatSymbols.getInstance());
-        format.setMaximumFractionDigits(scale);
-        return format.format(number);
-    }
-
-    public static String parseFloat(float number) {
-        return parseFloat(number, 2);
     }
 }

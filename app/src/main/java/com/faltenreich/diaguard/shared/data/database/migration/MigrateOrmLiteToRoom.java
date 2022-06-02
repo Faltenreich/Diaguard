2ec32a139a1d01579d73ba68ed98b5739766e14f
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2022-06-02 18:47:34 +0200

    Migrating tags

--- app/src/main/java/com/faltenreich/diaguard/shared/data/database/migration/MigrateOrmLiteToRoom.java
+++ app/src/main/java/com/faltenreich/diaguard/shared/data/database/migration/MigrateOrmLiteToRoom.java
@@ -6,6 +6,14 @@ import android.util.Log;
 
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.faltenreich.diaguard.shared.data.database.entity.BaseEntity;
+import com.faltenreich.diaguard.shared.data.database.entity.Tag;
+
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MigrateOrmLiteToRoom {
 
     private static final String TAG = MigrateOrmLiteToRoom.class.getSimpleName();
@@ -17,11 +25,30 @@ public class MigrateOrmLiteToRoom {
     public void migrate(SupportSQLiteDatabase roomDatabase, SQLiteDatabase ormLiteDatabase) {
         Log.d(TAG, "Migrating database from OrmLite to Room");
         Cursor cursor = ormLiteDatabase.rawQuery("SELECT * FROM Tag", null);
+        List<Tag> tags = new ArrayList<>();
         while (cursor.moveToNext()) {
-            String value = cursor.getString(0);
+            try {
+                Tag tag = new Tag();
+                tag.setId(cursor.getLong(getColumnIndex(cursor, BaseEntity.Column.ID)));
+                tag.setCreatedAt(new DateTime((cursor.getLong(getColumnIndex(cursor, BaseEntity.Column.CREATED_AT)))));
+                tag.setUpdatedAt(new DateTime((cursor.getLong(getColumnIndex(cursor, BaseEntity.Column.UPDATED_AT)))));
+                tag.setName(cursor.getString(getColumnIndex(cursor, Tag.Column.NAME)));
+                tags.add(tag);
+            } catch (IndexOutOfBoundsException exception) {
+                Log.e(TAG, "Failed to import Tag: " + cursor);
+            }
         }
+        // TODO: Insert tags
         cursor.close();
         // TODO: Maybe delete ormLiteDatabase
         Log.d(TAG, "Migration succeeded");
+    }
+
+    private int getColumnIndex(Cursor cursor, String column) throws IndexOutOfBoundsException {
+        int index = cursor.getColumnIndex(column);
+        if (index == -1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return index;
     }
 }

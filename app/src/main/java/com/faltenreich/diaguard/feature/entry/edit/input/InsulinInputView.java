27473ec9447a38eee6fdc7efa901955cac77694e
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2022-12-31 15:26:19 +0100

    Fixing imeOptions for horizontal layouts in entry form

--- app/src/main/java/com/faltenreich/diaguard/feature/entry/edit/input/InsulinInputView.java
+++ app/src/main/java/com/faltenreich/diaguard/feature/entry/edit/input/InsulinInputView.java
@@ -3,6 +3,7 @@ package com.faltenreich.diaguard.feature.entry.edit.input;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.inputmethod.EditorInfo;
 
 import com.faltenreich.diaguard.databinding.ListItemMeasurementInsulinBinding;
 import com.faltenreich.diaguard.feature.preference.data.PreferenceStore;
@@ -58,6 +59,14 @@ public class InsulinInputView extends MeasurementInputView<ListItemMeasurementIn
                     measurement.getCategory(),
                     FloatUtils.parseNumber(correctionInputField.getText())
                 ) : 0);
+        });
+        // Workaround: Fixing imeOptions for TextInputLayout in horizontal LinearLayout
+        correctionInputField.getEditText().setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                basalInputField.getEditText().requestFocus();
+                return true;
+            }
+            return false;
         });
 
         basalInputField.setText(measurement.getValuesForUI()[2]);
--- app/src/main/java/com/faltenreich/diaguard/feature/entry/edit/input/PressureInputView.java
+++ app/src/main/java/com/faltenreich/diaguard/feature/entry/edit/input/PressureInputView.java
@@ -3,6 +3,7 @@ package com.faltenreich.diaguard.feature.entry.edit.input;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.inputmethod.EditorInfo;
 
 import com.faltenreich.diaguard.databinding.ListItemMeasurementPressureBinding;
 import com.faltenreich.diaguard.feature.preference.data.PreferenceStore;
@@ -42,22 +43,30 @@ public class PressureInputView extends MeasurementInputView<ListItemMeasurementP
         EditTextUtils.afterTextChanged(systolicInputField.getEditText(), () ->
             measurement.setSystolic(PreferenceStore.getInstance().formatCustomToDefaultUnit(
                 measurement.getCategory(),
-                FloatUtils.parseNumber(systolicInputField.getText())))
+                systolicInputField.getText() != null ? FloatUtils.parseNumber(systolicInputField.getText()) : 0))
         );
+        // Workaround: Fixing imeOptions for TextInputLayout in horizontal LinearLayout
+        systolicInputField.getEditText().setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                diastolicInputField.getEditText().requestFocus();
+                return true;
+            }
+            return false;
+        });
 
         diastolicInputField.setText(measurement.getValuesForUI()[1]);
         diastolicInputField.setSuffixText(PreferenceStore.getInstance().getUnitAcronym(Category.PRESSURE));
         EditTextUtils.afterTextChanged(diastolicInputField.getEditText(), () ->
             measurement.setDiastolic(PreferenceStore.getInstance().formatCustomToDefaultUnit(
                 measurement.getCategory(),
-                FloatUtils.parseNumber(diastolicInputField.getText())))
+                diastolicInputField.getText() != null ? FloatUtils.parseNumber(diastolicInputField.getText()) : 0))
         );
     }
 
     @Override
     public boolean isValid() {
-        String systolic = systolicInputField.getText().trim();
-        String diastolic = diastolicInputField.getText().trim();
+        String systolic = systolicInputField.getText() != null ? systolicInputField.getText().trim() : "";
+        String diastolic = diastolicInputField.getText() != null ? diastolicInputField.getText().trim() : "";
         if (StringUtils.isBlank(systolic) && StringUtils.isBlank(diastolic)) {
             return true;
         }

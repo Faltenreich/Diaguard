d91cb28cf4446809ce4af6ef810423170246b07f
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-04-24 19:20:53 +0200

    Introducing interface for EntryDao

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/EntryDao.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/EntryDao.kt
@@ -1,33 +1,12 @@
 package com.faltenreich.diaguard.entry
 
-import app.cash.sqldelight.coroutines.asFlow
-import app.cash.sqldelight.coroutines.mapToList
-import com.faltenreich.diaguard.shared.database.Database
-import com.faltenreich.diaguard.shared.datetime.DateTimeRepository
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import org.koin.core.annotation.Single
 
-@Single
-class EntryDao(
-    private val dispatcher: CoroutineDispatcher,
-    private val database: Database,
-    // TODO: Remove when dao has been encapsulated better
-    private val dateTimeRepository: DateTimeRepository,
-) {
+interface EntryDao {
 
-    fun getAll(): Flow<List<Entry>> {
-        return database.sqlDelightDatabase.database.entryQueries.selectAll { id, date_time, note ->
-            Entry(id, dateTimeRepository.convertIsoStringToDateTime(date_time), note)
-        }.asFlow().mapToList(dispatcher)
-    }
+    fun getAll(): Flow<List<Entry>>
 
-    fun insert(entry: Entry) {
-        database.sqlDelightDatabase.database.entryQueries.insert(dateTimeRepository.convertDateTimeToIsoString(entry.dateTime), entry.note)
-    }
+    fun insert(entry: Entry)
 
-    fun delete(entry: Entry) {
-        val entryId = entry.id ?: return
-        database.sqlDelightDatabase.database.entryQueries.delete(entryId)
-    }
+    fun delete(entry: Entry)
 }
---
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/database/sqldelight/dao/EntrySqlDelightDao.kt
@@ -0,0 +1,39 @@
+package com.faltenreich.diaguard.shared.database.sqldelight.dao
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import com.faltenreich.diaguard.entry.Entry
+import com.faltenreich.diaguard.entry.EntryDao
+import com.faltenreich.diaguard.shared.database.Database
+import com.faltenreich.diaguard.shared.database.sqldelight.EntryQueries
+import com.faltenreich.diaguard.shared.datetime.DateTimeRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import org.koin.core.annotation.Single
+
+@Single
+class EntrySqlDelightDao(
+    private val dispatcher: CoroutineDispatcher,
+    private val database: Database,
+    // TODO: Remove when dao has been encapsulated better
+    private val dateTimeRepository: DateTimeRepository,
+) : EntryDao {
+
+    private val queries: EntryQueries
+        get() = database.sqlDelightDatabase.database.entryQueries
+
+    override fun getAll(): Flow<List<Entry>> {
+        return queries.selectAll { id, date_time, note ->
+            Entry(id, dateTimeRepository.convertIsoStringToDateTime(date_time), note)
+        }.asFlow().mapToList(dispatcher)
+    }
+
+    override fun insert(entry: Entry) {
+        queries.insert(dateTimeRepository.convertDateTimeToIsoString(entry.dateTime), entry.note)
+    }
+
+    override fun delete(entry: Entry) {
+        val entryId = entry.id ?: return
+        queries.delete(entryId)
+    }
+}

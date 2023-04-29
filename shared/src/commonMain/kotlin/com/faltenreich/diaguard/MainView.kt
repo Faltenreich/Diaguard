f385ff4b6ba3c1f1c31481fbeda6a9d3436e0560
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-04-29 21:23:14 +0200

    Fixing recomposition by removing slide transition

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/MainView.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/MainView.kt
@@ -1,6 +1,5 @@
 package com.faltenreich.diaguard
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
@@ -21,7 +20,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.SlideTransition
 import com.faltenreich.diaguard.navigation.BottomSheetNavigation
 import com.faltenreich.diaguard.navigation.DashboardTarget
 import com.faltenreich.diaguard.navigation.EntryFormTarget
@@ -29,7 +27,6 @@ import com.faltenreich.diaguard.shared.view.BottomSheetState
 import dev.icerock.moko.resources.compose.stringResource
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MainView() {
     MainTheme {
@@ -40,34 +37,31 @@ fun MainView() {
             Navigator(screen = DashboardTarget) { navigator ->
                 val scope = rememberCoroutineScope()
                 val sheetState = remember { BottomSheetState() }
-                // FIXME: Recomposes more than once initially
-                SlideTransition(navigator) {
-                    Box {
-                        Scaffold(
-                            content = { CurrentScreen() },
-                            bottomBar = {
-                                BottomAppBar(
-                                    actions = {
-                                        IconButton(onClick = { scope.launch { sheetState.show() } }) {
-                                            Icon(Icons.Filled.Menu, "")
-                                        }
-                                        // TODO: Actions
-                                    },
-                                    floatingActionButton = {
-                                        FloatingActionButton(
-                                            onClick = { navigator.push(EntryFormTarget(null)) },
-                                            containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                                            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
-                                        ) {
-                                            Icon(Icons.Filled.Add, stringResource(MR.strings.entry_new))
-                                        }
-                                    },
-                                )
-                            },
-                        )
-                        if (sheetState.isVisible) {
-                            BottomSheetNavigation(sheetState = sheetState)
-                        }
+                Box {
+                    Scaffold(
+                        content = { CurrentScreen() },
+                        bottomBar = {
+                            BottomAppBar(
+                                actions = {
+                                    IconButton(onClick = { scope.launch { sheetState.show() } }) {
+                                        Icon(Icons.Filled.Menu, "")
+                                    }
+                                    // TODO: Actions
+                                },
+                                floatingActionButton = {
+                                    FloatingActionButton(
+                                        onClick = { navigator.push(EntryFormTarget(null)) },
+                                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                                    ) {
+                                        Icon(Icons.Filled.Add, stringResource(MR.strings.entry_new))
+                                    }
+                                },
+                            )
+                        },
+                    )
+                    if (sheetState.isVisible) {
+                        BottomSheetNavigation(sheetState = sheetState)
                     }
                 }
             }

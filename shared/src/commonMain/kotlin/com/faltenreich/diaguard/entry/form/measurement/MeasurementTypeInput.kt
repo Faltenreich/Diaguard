ca0d954924b68464460d740374703424e1cbe5a4
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-05-21 18:24:46 +0200

    Setting input type for measurement input

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/measurement/MeasurementTypeInput.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/measurement/MeasurementTypeInput.kt
@@ -1,7 +1,9 @@
 package com.faltenreich.diaguard.entry.form.measurement
 
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import com.faltenreich.diaguard.measurement.type.MeasurementType
 import com.faltenreich.diaguard.shared.view.TextInput
 
@@ -16,5 +18,6 @@ fun MeasurementTypeInput(
         hint = value.type.name,
         modifier = modifier,
         onInputChange = { input -> onInputChange(input, value.type) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
     )
 }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/TextInput.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/TextInput.kt
@@ -1,5 +1,6 @@
 package com.faltenreich.diaguard.shared.view
 
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -11,11 +12,13 @@ fun TextInput(
     hint: String,
     onInputChange: (String) -> Unit,
     modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
 ) {
     TextField(
         value = input,
         onValueChange = onInputChange,
         modifier = modifier,
         placeholder = { Text(hint) },
+        keyboardOptions = keyboardOptions,
     )
 }

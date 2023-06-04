b2a501947536778128b53738a5e993670cfc8e6f
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-06-04 15:17:44 +0200

    Removing sticky headers to fix pagination

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/log/Log.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/log/Log.kt
@@ -16,11 +16,11 @@ import com.faltenreich.diaguard.log.item.LogDay
 import com.faltenreich.diaguard.log.item.LogEmpty
 import com.faltenreich.diaguard.log.item.LogEntry
 import com.faltenreich.diaguard.log.item.LogItem
-import com.faltenreich.diaguard.log.item.LogMonth
 import com.faltenreich.diaguard.shared.di.inject
 import com.faltenreich.diaguard.shared.view.Skeleton
 import com.faltenreich.diaguard.shared.view.SwipeToDismiss
 import com.faltenreich.diaguard.shared.view.collectAsPaginationItems
+import com.faltenreich.diaguard.shared.view.items
 import com.faltenreich.diaguard.shared.view.rememberSwipeToDismissState
 import kotlinx.coroutines.flow.distinctUntilChanged
 
@@ -53,11 +53,11 @@ fun Log(
         modifier = modifier.fillMaxSize(),
         state = listState,
     ) {
-        (0 until items.itemCount).forEach { index ->
-            when (val item = items.peek(index)) {
-                is LogItem.MonthHeader -> stickyHeader { LogMonth(item.date.monthOfYear) }
-                is LogItem.DayHeader -> item { LogDay(item.date) }
-                is LogItem.EntryContent -> item {
+        items(items, key = { it.key }) { item ->
+            when (item) {
+                is LogItem.MonthHeader -> Unit
+                is LogItem.DayHeader -> LogDay(item.date)
+                is LogItem.EntryContent -> {
                     val swipeToDismissState = rememberSwipeToDismissState()
                     // TODO: Animate item replacement
                     if (swipeToDismissState.isDismissed()) {
@@ -78,14 +78,12 @@ fun Log(
                         )
                     }
                 }
-                is LogItem.EmptyContent -> item { LogEmpty() }
-                null -> item {
-                    Skeleton(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(AppTheme.dimensions.size.MediumTouchSize),
-                    )
-                }
+                is LogItem.EmptyContent -> LogEmpty()
+                null -> Skeleton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(AppTheme.dimensions.size.MediumTouchSize),
+                )
             }
         }
     }

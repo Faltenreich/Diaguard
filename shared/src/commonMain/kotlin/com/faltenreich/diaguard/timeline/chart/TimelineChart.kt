a70e74b8db9a0d2084bedee0d24acbf03e7c397b
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-06-20 21:29:01 +0200

    First steps for scrollable timeline

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/chart/TimelineChart.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/chart/TimelineChart.kt
@@ -1,13 +1,19 @@
 package com.faltenreich.diaguard.timeline.chart
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.shared.view.drawText
@@ -19,44 +25,64 @@ private const val X_MAX = 24
 
 @Composable
 fun TimelineChart(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
-    val offset = LocalDensity.current.run { AppTheme.dimensions.padding.P_4.toPx() }
+    val padding = LocalDensity.current.run { AppTheme.dimensions.padding.P_2.toPx() }
     val fontSize = LocalDensity.current.run { AppTheme.typography.bodyMedium.fontSize.toPx() }
-    Canvas(
-        modifier = modifier.fillMaxSize(),
-    ) {
-        drawAxis(offset = offset, fontSize = fontSize)
-    }
-}
-
-private fun DrawScope.drawAxis(
-    offset: Float,
-    fontSize: Float,
-) {
-    drawLine(
-        color = Color.Black,
-        start = Offset(x = offset, y = offset),
-        end = Offset(x = offset, y = size.height - offset),
-        strokeWidth = 5f,
-    )
-    drawLine(
-        color = Color.Black,
-        start = Offset(x = 0f, y = size.height - offset),
-        end = Offset(x = size.width, y = size.height - offset),
-        strokeWidth = 5f,
-    )
-    val hours = 0 .. 24 step 2
-    val hoursCount = hours.last / hours.step
     val paint = Paint().apply {
         color = Color.Black
     }
-    val graphWidth = size.width - offset
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    Canvas(
+        modifier = modifier
+            .fillMaxSize()
+            .pointerInput(key1 = Unit) {
+                detectDragGestures(onDrag = { _, dragAmount -> offset += dragAmount })
+            },
+    ) {
+        drawYAxis(fontSize, paint, padding)
+        drawXAxis(offset, fontSize, paint, padding)
+        drawValues(offset, fontSize, paint)
+    }
+}
+
+private fun DrawScope.drawYAxis(
+    fontSize: Float,
+    paint: Paint,
+    padding: Float,
+) {
+    val values = 0 .. 250 step 50
+    val valuesCount = values.last / values.step
+    values.drop(1).forEach { value ->
+        val index = value / values.step
+        val height = size.height / valuesCount
+        val x = 0f + padding
+        val y = size.height - index * height
+        drawText(value.toString(), x, y, fontSize, paint)
+    }
+}
+
+private fun DrawScope.drawXAxis(
+    offset: Offset,
+    fontSize: Float,
+    paint: Paint,
+    padding: Float,
+) {
+    val hours = 0 .. 24 step 2
+    val hoursCount = hours.last / hours.step
     hours.forEach { hour ->
         val index = hour / hours.step
-        val hourWidth = graphWidth / hoursCount
-        val x = offset + index * hourWidth
-        val y = size.height - offset
+        val width = (size.width - padding) / hoursCount
+        val x = padding + (index * width) + offset.x
+        val y = size.height - padding
         drawText(hour.toString(), x, y, fontSize, paint)
     }
+}
+
+private fun DrawScope.drawValues(
+    offset: Offset,
+    fontSize: Float,
+    paint: Paint,
+) {
+    drawText("$offset", x = size.width / 2 - 160, y = size.height / 2, fontSize, paint)
 }

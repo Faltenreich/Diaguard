db9c1c6de08320ea6e5fb07f919cf2e5736679a5
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-07-01 19:44:37 +0200

    Revert "Merging timeline with canvas"
    This reverts commit 92e055b27b8538bf57c0ec63b0de8d720443d1b8.

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/Timeline.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/Timeline.kt
@@ -1,61 +1,21 @@
 package com.faltenreich.diaguard.timeline
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
-import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.shared.di.inject
-import com.faltenreich.diaguard.timeline.chart.TimelineChart
-import com.faltenreich.diaguard.timeline.list.TimelineList
-import kotlin.math.ceil
 
 @Composable
 fun Timeline(
     modifier: Modifier = Modifier,
     viewModel: TimelineViewModel = inject(),
 ) {
-    // TODO: Reset remember when initialDate changes
-    var offset by remember { mutableStateOf(Offset.Zero) }
-    // FIXME: Merge offset with view state
-    val state = viewModel.viewState.collectAsState().value.copy(offset = offset)
-    val config = TimelineConfig(
-        padding = LocalDensity.current.run { AppTheme.dimensions.padding.P_2.toPx() },
-        fontPaint = Paint().apply { color = AppTheme.colors.material.onBackground },
-        fontSize = LocalDensity.current.run { AppTheme.typography.bodyMedium.fontSize.toPx() },
-        gridStrokeColor = AppTheme.colors.material.onSurfaceVariant,
-        gridShadowColor = AppTheme.colors.material.scrim,
-        valueColorNormal = AppTheme.colors.Green,
-        valueColorLow = AppTheme.colors.Blue,
-        valueColorHigh = AppTheme.colors.Red,
+    val viewState = viewModel.viewState.collectAsState().value
+    TimelineCanvas(
+        initialDate = viewState.initialDate,
+        values = viewState.bloodSugarList,
+        onDateChange = viewModel::setDate,
+        modifier = modifier.fillMaxSize(),
     )
-    Canvas(
-        modifier = modifier
-            .fillMaxSize()
-            .pointerInput(key1 = Unit) {
-                detectDragGestures(
-                    onDrag = { _, dragAmount ->
-                        offset += dragAmount * 1.5f
-
-                        val widthPerDay = size.width
-                        val offsetInDays = ceil(offset.x * -1) / widthPerDay
-                        val date = state.initialDate.plusDays(offsetInDays.toInt())
-                        viewModel.setDate(date)
-                    },
-                )
-            },
-    ) {
-        TimelineChart(state, config)
-        TimelineList(state, config)
-    }
 }

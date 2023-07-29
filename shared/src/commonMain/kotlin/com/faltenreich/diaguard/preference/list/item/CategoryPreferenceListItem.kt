609fb4f4f9e793ac34c72be5e7cc1c54217a3f79
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-07-29 17:49:48 +0200

    Styling CategoryPreferenceListItem

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/list/item/CategoryPreferenceListItem.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/list/item/CategoryPreferenceListItem.kt
@@ -2,13 +2,17 @@ package com.faltenreich.diaguard.preference.list.item
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
 import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.preference.list.Preference
 import dev.icerock.moko.resources.compose.painterResource
@@ -19,16 +23,28 @@ fun CategoryPreferenceListItem(
     preference: Preference.Category,
     modifier: Modifier = Modifier,
 ) {
-    Row(modifier = modifier
-        .padding(all = AppTheme.dimensions.padding.P_3)
-        .clickable {},
+    Divider()
+    Row(
+        modifier = modifier
+            .padding(
+                start = AppTheme.dimensions.padding.P_3,
+                top = AppTheme.dimensions.padding.P_3,
+                end = AppTheme.dimensions.padding.P_3,
+            )
+            .clickable {},
+        horizontalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_2),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Image(
             painter = painterResource(preference.icon),
             contentDescription = null,
-            modifier = Modifier.size(AppTheme.dimensions.size.ImageMedium),
+            modifier = Modifier.size(AppTheme.dimensions.size.ImageSmall),
             colorFilter = ColorFilter.tint(AppTheme.colors.material.primary),
         )
-        Text(stringResource(preference.title))
+        Text(
+            text = stringResource(preference.title),
+            color = AppTheme.colors.material.primary,
+            fontWeight = FontWeight.Bold,
+        )
     }
 }

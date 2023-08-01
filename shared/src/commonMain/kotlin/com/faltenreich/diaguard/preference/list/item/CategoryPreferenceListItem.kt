b404dd88626bd617aaae3bdc4aff0ef792a62d45
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-08-01 19:15:34 +0200

    Separating CategoryPreferenceListItem from PreferenceListItemContentLayout

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/list/item/CategoryPreferenceListItem.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/list/item/CategoryPreferenceListItem.kt
@@ -1,10 +1,15 @@
 package com.faltenreich.diaguard.preference.list.item
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
@@ -18,23 +23,29 @@ fun CategoryPreferenceListItem(
     preference: Preference.Category,
     modifier: Modifier = Modifier,
 ) {
-    PreferenceListItem(
-        modifier = modifier,
-        top = { Divider() },
-        left = {
+    Divider()
+    Row(
+        modifier = modifier
+            .padding(
+                start = AppTheme.dimensions.padding.P_3,
+                top = AppTheme.dimensions.padding.P_3,
+                end = AppTheme.dimensions.padding.P_3,
+                bottom = AppTheme.dimensions.padding.P_1,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(modifier = Modifier.width(AppTheme.dimensions.size.ListOffsetWidth)) {
             Image(
                 painter = painterResource(preference.icon),
                 contentDescription = null,
                 modifier = Modifier.size(AppTheme.dimensions.size.ImageSmall),
                 colorFilter = ColorFilter.tint(AppTheme.colors.material.primary),
             )
-        },
-        content = {
-            Text(
-                text = stringResource(preference.title),
-                color = AppTheme.colors.material.primary,
-                fontWeight = FontWeight.Bold,
-            )
         }
-    )
+        Text(
+            text = stringResource(preference.title),
+            color = AppTheme.colors.material.primary,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }

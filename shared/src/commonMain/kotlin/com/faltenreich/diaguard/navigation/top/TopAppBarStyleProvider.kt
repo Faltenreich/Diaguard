d981f2a46fe2336575f6d9bdc94b1ba3f2bf7c92
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-08-20 19:27:17 +0200

    Adding top bar to MeasurementTypeList

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/navigation/top/TopAppBarStyleProvider.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/navigation/top/TopAppBarStyleProvider.kt
@@ -2,21 +2,21 @@ package com.faltenreich.diaguard.navigation.top
 
 import androidx.compose.material3.Text
 import com.faltenreich.diaguard.MR
-import com.faltenreich.diaguard.entry.form.EntryFormViewModel
 import com.faltenreich.diaguard.navigation.Screen
-import com.faltenreich.diaguard.shared.di.getViewModel
 import dev.icerock.moko.resources.compose.stringResource
-import org.koin.core.parameter.parametersOf
 
 fun Screen.topAppBarStyle(): TopAppBarStyle {
     return when (this) {
         is Screen.EntryForm -> TopAppBarStyle.CenterAligned {
-            val viewModel = getViewModel<EntryFormViewModel> { parametersOf(entry, date) }
-            val titleResource = if (viewModel.isEditing) MR.strings.entry_edit else MR.strings.entry_new
+            val isEditing = entry?.id != null
+            val titleResource = if (isEditing) MR.strings.entry_edit else MR.strings.entry_new
             Text(stringResource(titleResource))
         }
         is Screen.MeasurementPropertyList -> TopAppBarStyle.CenterAligned {
             Text(stringResource(MR.strings.measurement_properties))
+        }
+        is Screen.MeasurementTypeList -> TopAppBarStyle.CenterAligned {
+            Text(stringResource(MR.strings.measurement_property))
         }
         is Screen.PreferenceList -> TopAppBarStyle.CenterAligned {
             Text(stringResource(MR.strings.preferences))

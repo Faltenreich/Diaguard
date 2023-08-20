2d4793b02f92ed92e110e3bf4408fedb2e7a76c6
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-08-20 21:08:33 +0200

    Styling MeasurementPropertyForm

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/property/form/MeasurementPropertyForm.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/property/form/MeasurementPropertyForm.kt
@@ -1,16 +1,14 @@
 package com.faltenreich.diaguard.measurement.property.form
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.MR
 import com.faltenreich.diaguard.shared.di.inject
-import com.faltenreich.diaguard.shared.view.FormRow
-import com.faltenreich.diaguard.shared.view.ResourceIcon
 import com.faltenreich.diaguard.shared.view.TextInput
 import dev.icerock.moko.resources.compose.stringResource
 
@@ -20,24 +18,20 @@ fun MeasurementPropertyForm(
     viewModel: MeasurementPropertyFormViewModel = inject(),
 ){
     Column(
-        modifier = modifier.verticalScroll(rememberScrollState()),
+        modifier = modifier.padding(all = AppTheme.dimensions.padding.P_3),
+        verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_3),
     ) {
-        FormRow(icon = { ResourceIcon(MR.images.ic_note) }) {
-            TextInput(
-                input = viewModel.name,
-                onInputChange = { input -> viewModel.name = input },
-                label = stringResource(MR.strings.name),
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        Divider()
-        FormRow(icon = { ResourceIcon(MR.images.ic_note) }) {
-            TextInput(
-                input = viewModel.icon,
-                onInputChange = { input -> viewModel.icon = input },
-                label = stringResource(MR.strings.icon),
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
+        TextInput(
+            input = viewModel.name,
+            onInputChange = { input -> viewModel.name = input },
+            label = stringResource(MR.strings.name),
+            modifier = Modifier.fillMaxWidth(),
+        )
+        TextInput(
+            input = viewModel.icon,
+            onInputChange = { input -> viewModel.icon = input },
+            label = stringResource(MR.strings.icon),
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }

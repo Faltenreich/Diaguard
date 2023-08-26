0fb1af511993b1e9601b989ea0a063788c117281
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-08-26 20:42:48 +0200

    Highlight selected type unit with icon

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/type/form/MeasurementTypeUnitListItem.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/type/form/MeasurementTypeUnitListItem.kt
@@ -1,12 +1,19 @@
 package com.faltenreich.diaguard.measurement.type.form
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
+import com.faltenreich.diaguard.MR
 import com.faltenreich.diaguard.measurement.unit.MeasurementTypeUnit
 import com.faltenreich.diaguard.shared.view.FormRow
+import dev.icerock.moko.resources.compose.stringResource
 
 @Composable
 fun MeasurementTypeUnitListItem(
@@ -15,11 +22,21 @@ fun MeasurementTypeUnitListItem(
 ) {
     Column(modifier = modifier) {
         FormRow(icon = { /* TODO */ }) {
-            Text(
-                text = typeUnit.unit.name,
-                modifier = Modifier.weight(1f),
-            )
-            Text(typeUnit.factor.toString()) // TODO: Format
+            Column(modifier = Modifier.weight(1f)) {
+                Text(typeUnit.unit.name)
+                Text(
+                    text = typeUnit.factor.toString(), // TODO: Format
+                    style = AppTheme.typography.bodySmall,
+                )
+            }
+            if (typeUnit.type.selectedUnitId == typeUnit.unitId) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(MR.strings.measurement_type_unit_selected_description),
+                    modifier = modifier.size(AppTheme.dimensions.size.ImageMedium),
+                    tint = AppTheme.colors.Green,
+                )
+            }
         }
         Divider()
     }

519083b6f36d36f2361fe66313e29ae3dd06053e
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-08-26 20:48:23 +0200

    Integrate type title into divider of property form

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/property/form/MeasurementPropertyForm.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/property/form/MeasurementPropertyForm.kt
@@ -3,13 +3,17 @@ package com.faltenreich.diaguard.measurement.property.form
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -48,7 +52,17 @@ fun MeasurementPropertyForm(
             )
         }
 
-        Divider()
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_1),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Divider(modifier = Modifier.width(AppTheme.dimensions.padding.P_3_5))
+            Text(
+                text = stringResource(MR.strings.measurement_types),
+                style = AppTheme.typography.bodySmall,
+            )
+            Divider(modifier = Modifier.weight(1f))
+        }
 
         when (state) {
             is MeasurementPropertyFormViewState.Loading -> Unit

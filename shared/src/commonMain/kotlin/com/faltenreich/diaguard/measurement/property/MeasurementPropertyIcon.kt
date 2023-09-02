0574d6620b0ad352b3c9610671bd3cc494099423
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-09-02 20:07:49 +0200

    Constrain size of MeasurementPropertyIcon

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/property/MeasurementPropertyIcon.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/property/MeasurementPropertyIcon.kt
@@ -1,8 +1,12 @@
 package com.faltenreich.diaguard.measurement.property
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
 
 @Composable
 fun MeasurementPropertyIcon(
@@ -12,8 +16,13 @@ fun MeasurementPropertyIcon(
     val text = property.icon?.takeIf(String::isNotBlank)
         ?: property.name.firstOrNull()?.toString()
         ?: return
-    Text(
-        text = text,
-        modifier = modifier,
-    )
+    Box(
+        modifier = modifier.size(AppTheme.dimensions.size.ImageLarge),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = text,
+            style = AppTheme.typography.headlineLarge,
+        )
+    }
 }

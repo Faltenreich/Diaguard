df67c2c653a9f05d60cb4a3113115442e34e1fe2
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-09-23 18:13:03 +0200

    Replace platform-specific with multiplatform BottomSheet

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/BottomSheet.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/BottomSheet.kt
@@ -1,13 +1,22 @@
 package com.faltenreich.diaguard.shared.view
 
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
-expect fun BottomSheet(
+fun BottomSheet(
+    // FIXME: Not called when pulled down
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
     sheetState: BottomSheetState = remember { BottomSheetState() },
     content: @Composable () -> Unit,
-)
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        sheetState = sheetState.delegate,
+        content = { content() },
+    )
+}
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/BottomSheetState.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/BottomSheetState.kt
@@ -1,20 +1,40 @@
 package com.faltenreich.diaguard.shared.view
 
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
 
-expect class BottomSheetState constructor() {
+class BottomSheetState {
+
+    var delegate: SheetState = SheetState(skipPartiallyExpanded = true)
 
     val isVisible: Boolean
+        get() = delegate.isVisible
 
-    suspend fun show()
+    suspend fun show() {
+        delegate.show()
+    }
 
-    suspend fun hide()
+    suspend fun hide() {
+        delegate.hide()
+    }
 
     companion object {
 
-        val Saver: Saver<BottomSheetState, *>
+        val Saver: Saver<BottomSheetState, *> = Saver(
+            save = { it.isVisible },
+            restore = { isVisible ->
+                BottomSheetState().apply {
+                    delegate = SheetState(
+                        skipPartiallyExpanded = true,
+                        initialValue = if (isVisible) SheetValue.Expanded else SheetValue.Hidden,
+                    )
+                }
+            }
+        )
+
     }
 }
 

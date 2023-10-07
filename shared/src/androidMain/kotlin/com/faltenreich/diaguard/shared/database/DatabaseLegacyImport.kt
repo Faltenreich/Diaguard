2081dbc9aa258dffa0222685f5ece18697d0a995
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-10-07 21:34:42 +0200

    Refactoring legacy import

--- shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/database/DatabaseLegacyImport.kt
+++ shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/database/DatabaseLegacyImport.kt
@@ -1,11 +1,9 @@
 package com.faltenreich.diaguard.shared.database
 
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import com.faltenreich.diaguard.entry.Entry
 import com.faltenreich.diaguard.measurement.value.MeasurementValue
-import com.faltenreich.diaguard.shared.datetime.DateTime
 import com.faltenreich.diaguard.shared.di.inject
 
 actual class DatabaseLegacyImport {
@@ -13,89 +11,181 @@ actual class DatabaseLegacyImport {
     actual fun import() {
         val context = inject<Context>()
         val databaseFile = context.getDatabasePath("diaguard.db")
+        // TODO: Return if file does not exist
         val database = SQLiteDatabase.openDatabase(databaseFile.absolutePath, null, 0)
 
         val entries = mutableListOf<Entry>()
-        with (database.query("entry")) {
-            while (moveToNext()) {
-                val entry = Entry(
+        database.queryEach("entry") {
+            entries.add(
+                Entry(
                     id = getLong("_id"),
                     createdAt = getDateTime("createdAt"),
                     updatedAt = getDateTime("updatedAt"),
                     dateTime = getDateTime("date"),
                     note = getString("note"),
                 )
-                entries.add(entry)
-            }
-            close()
+            )
         }
 
         val values = mutableListOf<MeasurementValue>()
-        with (database.query("bloodsugar")) {
-            while (moveToNext()) {
-                val value = MeasurementValue(
-                    id = getLong("_id"),
+        // value ids must be recreated since legacy data might be merged
+        var valueId = 0L
+        val autoIncrement = { valueId++ }
+
+        database.queryEach("bloodsugar") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
                     createdAt = getDateTime("createdAt"),
                     updatedAt = getDateTime("updatedAt"),
                     value = getDouble("mgDl"),
                     typeId = 1, // TODO: Determine typeId of mgDl
                     entryId = getLong("entry"),
                 )
-                values.add(value)
-            }
-            close()
+            )
         }
-        with (database.query("activity")) {
-            while (moveToNext()) {
-                val value = MeasurementValue(
-                    id = getLong("_id"),
+        database.queryEach("insulin") {
+            val createdAt = getDateTime("createdAt")
+            val updatedAt = getDateTime("updatedAt")
+            val entryId = getLong("entry")
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = createdAt,
+                    updatedAt = updatedAt,
+                    value = getDouble("bolus"),
+                    typeId = 1, // TODO: Determine typeId of bolus
+                    entryId = entryId,
+                )
+            )
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = createdAt,
+                    updatedAt = updatedAt,
+                    value = getDouble("basal"),
+                    typeId = 1, // TODO: Determine typeId of basal
+                    entryId = entryId,
+                )
+            )
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = createdAt,
+                    updatedAt = updatedAt,
+                    value = getDouble("correction"),
+                    typeId = 1, // TODO: Determine typeId of correction
+                    entryId = entryId,
+                )
+            )
+        }
+        database.queryEach("meal") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = getDateTime("createdAt"),
+                    updatedAt = getDateTime("updatedAt"),
+                    value = getDouble("carbohydrates"),
+                    typeId = 1, // TODO: Determine typeId of carbohydrates
+                    entryId = getLong("entry"),
+                )
+            )
+        }
+        database.queryEach("activity") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
                     createdAt = getDateTime("createdAt"),
                     updatedAt = getDateTime("updatedAt"),
                     value = getDouble("minutes"),
                     typeId = 1, // TODO: Determine typeId of minutes
                     entryId = getLong("entry"),
                 )
-                values.add(value)
-            }
-            close()
+            )
         }
-        // TODO: Add other categories to values
+        database.queryEach("hba1c") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = getDateTime("createdAt"),
+                    updatedAt = getDateTime("updatedAt"),
+                    value = getDouble("percent"),
+                    typeId = 1, // TODO: Determine typeId of percent
+                    entryId = getLong("entry"),
+                )
+            )
+        }
+        database.queryEach("weight") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = getDateTime("createdAt"),
+                    updatedAt = getDateTime("updatedAt"),
+                    value = getDouble("kilogram"),
+                    typeId = 1, // TODO: Determine typeId of kilogram
+                    entryId = getLong("entry"),
+                )
+            )
+        }
+        database.queryEach("pulse") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = getDateTime("createdAt"),
+                    updatedAt = getDateTime("updatedAt"),
+                    value = getDouble("frequency"),
+                    typeId = 1, // TODO: Determine typeId of frequency
+                    entryId = getLong("entry"),
+                )
+            )
+        }
+        database.queryEach("pressure") {
+            val createdAt = getDateTime("createdAt")
+            val updatedAt = getDateTime("updatedAt")
+            val entryId = getLong("entry")
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = createdAt,
+                    updatedAt = updatedAt,
+                    value = getDouble("systolic"),
+                    typeId = 1, // TODO: Determine typeId of systolic
+                    entryId = entryId,
+                )
+            )
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = createdAt,
+                    updatedAt = updatedAt,
+                    value = getDouble("diastolic"),
+                    typeId = 1, // TODO: Determine typeId of diastolic
+                    entryId = entryId,
+                )
+            )
+        }
+        database.queryEach("oxygensaturation") {
+            values.add(
+                MeasurementValue(
+                    id = autoIncrement(),
+                    createdAt = getDateTime("createdAt"),
+                    updatedAt = getDateTime("updatedAt"),
+                    value = getDouble("percent"),
+                    typeId = 1, // TODO: Determine typeId of percent
+                    entryId = getLong("entry"),
+                )
+            )
+        }
 
         val tags = mutableListOf<String>()
-        with (database.query("tag")) {
-            while (moveToNext()) {
-                try {
-                    val id = getLong("_id")
-                    val createdAt = getDateTime("createdAt")
-                    val updatedAt = getDateTime("updatedAt")
-                    val name = getString("name")
-                    tags.add(name)
-                } catch (exception: IllegalArgumentException) {
-                    println(exception.message)
-                }
-            }
-            close()
+        database.queryEach("tag") {
+            val id = getLong("_id")
+            val createdAt = getDateTime("createdAt")
+            val updatedAt = getDateTime("updatedAt")
+            val name = getString("name")
+            tags.add(name)
         }
+
         println("Found ${entries.size} entries, ${values.size} values and ${tags.size} tags")
     }
-}
-
-private fun SQLiteDatabase.query(table: String): Cursor {
-    return query(table, null, null, null, null, null, null)
-}
-
-private fun Cursor.getLong(column: String): Long {
-    return getLong(getColumnIndexOrThrow(column))
-}
-
-private fun Cursor.getDateTime(column: String): DateTime {
-    return DateTime(millis = getLong(column))
-}
-
-private fun Cursor.getDouble(column: String): Double {
-    return getDouble(getColumnIndexOrThrow(column))
-}
-
-private fun Cursor.getString(column: String): String {
-    return getString(getColumnIndexOrThrow(column))
 }
---
+++ shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/database/SqlLiteExtensions.kt
@@ -0,0 +1,34 @@
+package com.faltenreich.diaguard.shared.database
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.faltenreich.diaguard.shared.datetime.DateTime
+
+fun SQLiteDatabase.queryEach(table: String, onEach: Cursor.() -> Unit) {
+    with (query(table, null, null, null, null, null, null)) {
+        while (moveToNext()) {
+            try {
+                onEach()
+            } catch (exception: IllegalArgumentException) {
+                println(exception.message)
+            }
+        }
+        close()
+    }
+}
+
+fun Cursor.getLong(column: String): Long {
+    return getLong(getColumnIndexOrThrow(column))
+}
+
+fun Cursor.getDateTime(column: String): DateTime {
+    return DateTime(millis = getLong(column))
+}
+
+fun Cursor.getDouble(column: String): Double {
+    return getDouble(getColumnIndexOrThrow(column))
+}
+
+fun Cursor.getString(column: String): String {
+    return getString(getColumnIndexOrThrow(column))
+}

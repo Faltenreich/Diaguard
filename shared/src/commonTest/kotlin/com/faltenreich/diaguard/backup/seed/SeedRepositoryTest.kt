36cba4cbbe75df73af5b9e80841f0078ddfb0370
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-10-18 20:46:32 +0200

    Simplify test for key uniqueness of SeedMeasurementProperty

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/backup/seed/SeedRepositoryTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/backup/seed/SeedRepositoryTest.kt
@@ -1,36 +1,19 @@
 package com.faltenreich.diaguard.backup.seed
 
 import kotlin.test.Test
-import kotlin.test.assertFalse
+import kotlin.test.assertEquals
 
 class SeedRepositoryTest {
 
     private val seedRepository = SeedRepository()
 
     @Test
-    fun `Seeded properties and its types and units have unique keys`() {
-        val propertyKeys = mutableListOf<String>()
-        val typeKeys = mutableListOf<String>()
-        val unitKeys = mutableListOf<String>()
-
+    fun `Seeded properties have unique keys`() {
         val seed = seedRepository.seedMeasurementProperties()
         val properties = seed.map(Seed<SeedMeasurementProperty>::harvest)
-        properties.forEach { property ->
-            val propertyKey = property.key
-            assertFalse(propertyKey in propertyKeys)
-            propertyKeys += propertyKey
-
-            property.types.forEach { type ->
-                val typeKey = type.key
-                assertFalse(typeKey in typeKeys)
-                typeKeys += typeKey
-
-                type.units.forEach { unit ->
-                    val unitKey = unit.key
-                    assertFalse(unitKey in unitKeys)
-                    unitKeys += unitKey
-                }
-            }
-        }
+        assertEquals(
+            expected = properties,
+            actual = properties.distinctBy(SeedMeasurementProperty::key),
+        )
     }
 }

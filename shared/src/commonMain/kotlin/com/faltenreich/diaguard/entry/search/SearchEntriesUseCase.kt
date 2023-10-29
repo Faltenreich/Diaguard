203ec73b2449569cde915b9b06e6ddc5b2cf0cd0
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-10-29 12:54:40 +0100

    Fix search results for entries

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/SearchEntriesUseCase.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/SearchEntriesUseCase.kt
@@ -2,14 +2,30 @@ package com.faltenreich.diaguard.entry.search
 
 import com.faltenreich.diaguard.entry.Entry
 import com.faltenreich.diaguard.entry.EntryRepository
-import com.faltenreich.diaguard.entry.deep
+import com.faltenreich.diaguard.measurement.property.MeasurementPropertyRepository
+import com.faltenreich.diaguard.measurement.type.MeasurementTypeRepository
+import com.faltenreich.diaguard.measurement.value.MeasurementValueRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class SearchEntriesUseCase(
     private val entryRepository: EntryRepository,
+    private val valueRepository: MeasurementValueRepository,
+    private val typeRepository: MeasurementTypeRepository,
+    private val propertyRepository: MeasurementPropertyRepository,
 ) {
 
     operator fun invoke(query: String): Flow<List<Entry>> {
-        return entryRepository.search(query).deep()
+        return entryRepository.search(query).map { entries ->
+            entries.map { entry ->
+                entry.values = valueRepository.getByEntryId(entry.id).map { value ->
+                    value.type = typeRepository.getById(value.typeId)?.apply {
+                        property = propertyRepository.getById(propertyId) ?: throw IllegalStateException()
+                    } ?: throw IllegalStateException()
+                    value
+                }
+                entry
+            }
+        }
     }
 }

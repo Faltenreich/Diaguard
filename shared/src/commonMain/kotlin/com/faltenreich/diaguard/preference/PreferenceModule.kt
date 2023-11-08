88988dcf750854bd922d111521579e36ab5c1694
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-11-08 20:30:43 +0100

    Remove obsolete use cases

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/PreferenceModule.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/PreferenceModule.kt
@@ -3,23 +3,9 @@ package com.faltenreich.diaguard.preference
 import com.faltenreich.diaguard.preference.list.GetDefaultPreferencesUseCase
 import com.faltenreich.diaguard.preference.list.Preference
 import com.faltenreich.diaguard.preference.list.PreferenceListViewModel
-import com.faltenreich.diaguard.preference.list.item.about.GetAboutPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.about.GetAppVersionPreferenceUseCase
 import com.faltenreich.diaguard.preference.list.item.about.GetAppVersionUseCase
-import com.faltenreich.diaguard.preference.list.item.about.GetLicensesPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.about.GetPrivacyPolicyPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.about.GetSourceCodePreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.about.GetTermsAndConditionsPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.color.GetColorSchemePreferenceUseCase
 import com.faltenreich.diaguard.preference.list.item.color.GetColorSchemeUseCase
 import com.faltenreich.diaguard.preference.list.item.color.SetColorSchemeUseCase
-import com.faltenreich.diaguard.preference.list.item.contact.GetContactPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.contact.GetFacebookPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.contact.GetHomepagePreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.contact.GetMailPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.data.GetDataPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.data.GetMeasurementPreferenceUseCase
-import com.faltenreich.diaguard.preference.list.item.screen.GetStartScreenPreferenceUseCase
 import com.faltenreich.diaguard.preference.list.item.screen.GetStartScreenUseCase
 import com.faltenreich.diaguard.preference.list.item.screen.SetStartScreenUseCase
 import org.koin.core.module.dsl.singleOf
@@ -30,23 +16,9 @@ fun preferenceModule() = module {
 
     singleOf(::GetColorSchemeUseCase)
     singleOf(::SetColorSchemeUseCase)
-    singleOf(::GetColorSchemePreferenceUseCase)
     singleOf(::GetStartScreenUseCase)
     singleOf(::SetStartScreenUseCase)
-    singleOf(::GetStartScreenPreferenceUseCase)
-    singleOf(::GetDataPreferenceUseCase)
-    singleOf(::GetMeasurementPreferenceUseCase)
-    singleOf(::GetContactPreferenceUseCase)
-    singleOf(::GetHomepagePreferenceUseCase)
-    singleOf(::GetMailPreferenceUseCase)
-    singleOf(::GetFacebookPreferenceUseCase)
-    singleOf(::GetAboutPreferenceUseCase)
-    singleOf(::GetSourceCodePreferenceUseCase)
-    singleOf(::GetLicensesPreferenceUseCase)
-    singleOf(::GetPrivacyPolicyPreferenceUseCase)
-    singleOf(::GetTermsAndConditionsPreferenceUseCase)
     singleOf(::GetAppVersionUseCase)
-    singleOf(::GetAppVersionPreferenceUseCase)
 
     singleOf(::GetDefaultPreferencesUseCase)
 

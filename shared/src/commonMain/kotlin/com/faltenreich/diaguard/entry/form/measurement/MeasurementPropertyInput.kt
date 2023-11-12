bb7902522975a1a1c6c8295fa10cb87448385117
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-11-12 18:01:17 +0100

    Style food input

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/measurement/MeasurementPropertyInput.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/measurement/MeasurementPropertyInput.kt
@@ -1,11 +1,16 @@
 package com.faltenreich.diaguard.entry.form.measurement
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,6 +24,7 @@ import com.faltenreich.diaguard.food.eaten.FoodEatenInput
 import com.faltenreich.diaguard.food.eaten.FoodEatenInputData
 import com.faltenreich.diaguard.measurement.property.MeasurementPropertyIcon
 import com.faltenreich.diaguard.navigation.screen.FoodListScreen
+import com.faltenreich.diaguard.shared.view.FormRow
 import com.faltenreich.diaguard.shared.view.ResourceIcon
 
 @Composable
@@ -29,47 +35,56 @@ fun MeasurementPropertyInput(
     modifier: Modifier = Modifier,
 ) {
     val navigator = LocalNavigator.currentOrThrow
-    Row(
-        modifier = modifier
-            .defaultMinSize(minHeight = AppTheme.dimensions.size.TouchSizeLarge)
-            .padding(AppTheme.dimensions.padding.P_3),
-        horizontalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_3),
-        verticalAlignment = Alignment.Top,
-    ) {
-        MeasurementPropertyIcon(
-            property = data.property,
-            modifier = Modifier.padding(top = AppTheme.dimensions.padding.P_3_25),
-        )
-        FlowRow(modifier = modifier) {
-            val types = data.typeInputDataList
-            types.forEach { type ->
-                MeasurementTypeInput(
-                    data = type,
-                    modifier = Modifier.weight(1f),
-                    action =
-                    if (data.property.isMeal) {
-                        {
-                            IconButton(
-                                onClick = {
-                                    navigator.push(FoodListScreen(onSelection = { food ->
-                                        onIntent(EntryFormIntent.AddFood(food))
-                                    }))
-                                })
+    Column {
+        Row(
+            modifier = modifier
+                .defaultMinSize(minHeight = AppTheme.dimensions.size.TouchSizeLarge)
+                .padding(AppTheme.dimensions.padding.P_3),
+            horizontalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_3),
+            verticalAlignment = Alignment.Top,
+        ) {
+            MeasurementPropertyIcon(
+                property = data.property,
+                modifier = Modifier.padding(top = AppTheme.dimensions.padding.P_3_25),
+            )
+            FlowRow(modifier = modifier) {
+                val types = data.typeInputDataList
+                types.forEach { type ->
+                    MeasurementTypeInput(
+                        data = type,
+                        modifier = Modifier.weight(1f),
+                        action =
+                        if (data.property.isMeal) {
                             {
-                                ResourceIcon(MR.images.ic_search)
+                                IconButton(
+                                    onClick = {
+                                        navigator.push(FoodListScreen(onSelection = { food ->
+                                            onIntent(EntryFormIntent.AddFood(food))
+                                        }))
+                                    })
+                                {
+                                    ResourceIcon(MR.images.ic_search)
+                                }
                             }
-                        }
-                    } else {
-                        null
-                    },
-                    onIntent = onIntent,
-                )
+                        } else {
+                            null
+                        },
+                        onIntent = onIntent,
+                    )
+                }
             }
-            if (data.property.isMeal) {
-                foodEaten.forEach { data ->
+        }
+        if (data.property.isMeal) {
+            foodEaten.forEach { data ->
+                Divider()
+                FormRow(
+                    modifier = modifier.background(AppTheme.colors.scheme.surfaceVariant),
+                    icon = {
+                        Spacer(modifier = Modifier.width(AppTheme.dimensions.size.ImageMedium))
+                    }
+                ) {
                     FoodEatenInput(
                         data = data,
-                        // TODO: modifier = Modifier.background(AppTheme.colors.scheme.background),
                         modifier = Modifier.fillMaxWidth(),
                         onIntent = onIntent,
                     )
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/TextInput.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/TextInput.kt
@@ -1,6 +1,5 @@
 package com.faltenreich.diaguard.shared.view
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
@@ -9,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.faltenreich.diaguard.AppTheme
 
 @Composable
 fun TextInput(
@@ -31,7 +29,7 @@ fun TextInput(
     OutlinedTextField(
         value = input,
         onValueChange = onInputChange,
-        modifier = modifier.padding(bottom = AppTheme.dimensions.padding.P_1),
+        modifier = modifier,
         label = { Text(label) },
         placeholder = placeholder,
         leadingIcon = leadingIcon,

3355cff606617b980a0038e054d905d36003759a
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2023-12-01 21:51:11 +0100

    Refactor logger

--- shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/logging/Logger.kt
+++ shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/logging/Logger.kt
@@ -4,37 +4,35 @@ import android.util.Log
 
 actual object Logger {
 
-    // Steps until call-site
-    private const val STACK_TRACE_INDEX_CALLER = 5
+    private const val CALLS_UNTIL_CALL_SITE = 5
 
-    private val stackTraceElement: StackTraceElement
-        get() = Thread.currentThread().stackTrace[STACK_TRACE_INDEX_CALLER]
-
-    private val tag: String
-        get() = stackTraceElement.className.substringAfterLast(".")
-
-    private val hyperlink: String
-        get() = stackTraceElement.toString()
+    private fun tracing(message: String, traced: (tag: String, message: String) -> Unit) {
+        val stackTrace = Thread.currentThread().stackTrace
+        val callSite = stackTrace[CALLS_UNTIL_CALL_SITE]
+        val tag = callSite.className.substringAfterLast(".")
+        val hyperlink = callSite.toString()
             .substringAfterLast("(")
             .substringBeforeLast(")")
-
-    actual fun verbose(message: String, throwable: Throwable?) {
-        Log.v(tag, "$message ($hyperlink)", throwable)
+        traced(tag, "$message ($hyperlink)")
     }
 
-    actual fun debug(message: String, throwable: Throwable?) {
-        Log.d(tag, "$message ($hyperlink)", throwable)
+    actual fun verbose(message: String, throwable: Throwable?) = tracing(message) { tag, msg ->
+        Log.v(tag, msg, throwable)
     }
 
-    actual fun info(message: String, throwable: Throwable?) {
-        Log.i(tag, "$message ($hyperlink)", throwable)
+    actual fun debug(message: String, throwable: Throwable?) = tracing(message) { tag, msg ->
+        Log.d(tag, msg, throwable)
     }
 
-    actual fun warning(message: String, throwable: Throwable?) {
-        Log.w(tag, "$message ($hyperlink)", throwable)
+    actual fun info(message: String, throwable: Throwable?) = tracing(message) { tag, msg ->
+        Log.i(tag, msg, throwable)
     }
 
-    actual fun error(message: String, throwable: Throwable?) {
-        Log.e(tag, "$message ($hyperlink)", throwable)
+    actual fun warning(message: String, throwable: Throwable?) = tracing(message) { tag, msg ->
+        Log.w(tag, msg, throwable)
+    }
+
+    actual fun error(message: String, throwable: Throwable?) = tracing(message) { tag, msg ->
+        Log.e(tag, msg, throwable)
     }
 }

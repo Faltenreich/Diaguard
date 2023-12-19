051897bd9067e21d60adc5af059b56f18451a85e
Faltenreich <philipp.fahlteich@gmail.com>
2023-12-19 20:07:29 +0100

    Change state of FormViewModel

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/architecture/FormViewModel.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/architecture/FormViewModel.kt
@@ -1,14 +1,13 @@
 package com.faltenreich.diaguard.shared.architecture
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 /**
  * Forms use MutableState instead of (State-)Flow,
  * since the latter is not compatible with Composables like [androidx.compose.material3.TextField]
  */
-abstract class FormViewModel<Intent> : ViewModel<Unit, Intent>() {
+abstract class FormViewModel<Intent> : ViewModel<Nothing, Intent>() {
 
-    override val state: Flow<Unit>
-        get() = flowOf(Unit)
+    override val state: Flow<Nothing>
+        get() = throw UnsupportedOperationException()
 }

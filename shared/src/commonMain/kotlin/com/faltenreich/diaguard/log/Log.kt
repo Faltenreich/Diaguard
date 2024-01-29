98f9657b394871ad84813f47c31aea12a08b3a1b
Faltenreich <philipp.fahlteich@gmail.com>
2024-01-29 19:01:46 +0100

    Prepare sticky day header in log

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/log/Log.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/log/Log.kt
@@ -1,31 +1,40 @@
 package com.faltenreich.diaguard.log
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntOffset
 import com.faltenreich.diaguard.AppTheme
+import com.faltenreich.diaguard.log.item.LogDay
+import com.faltenreich.diaguard.log.item.LogDayStyle
 import com.faltenreich.diaguard.log.item.LogEmpty
 import com.faltenreich.diaguard.log.item.LogEntry
 import com.faltenreich.diaguard.log.item.LogItem
 import com.faltenreich.diaguard.log.item.LogMonth
+import com.faltenreich.diaguard.shared.datetime.DateUnit
 import com.faltenreich.diaguard.shared.di.inject
 import com.faltenreich.diaguard.shared.view.Skeleton
 import com.faltenreich.diaguard.shared.view.collectAsPaginationItems
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlin.math.min
 
 @Composable
 fun Log(
@@ -104,5 +113,23 @@ fun Log(
                 }
             }
         }
+
+        val currentDate = viewModel.currentDate.collectAsState().value
+        var dayItemHeightPx by remember { mutableStateOf(0) }
+        LogDay(
+            date = currentDate.minus(1, DateUnit.DAY),
+            style = LogDayStyle.NORMAL,
+            modifier = Modifier
+                .onGloballyPositioned { dayItemHeightPx = it.size.height }
+                .offset {
+                    if (listState.layoutInfo.totalItemsCount < 2) return@offset IntOffset.Zero
+                    val firstVisibleItem = listState.layoutInfo.visibleItemsInfo
+                        .first { it.offset >= monthHeaderHeightPx }
+                    val yOffset = min(monthHeaderHeightPx, firstVisibleItem.offset - dayItemHeightPx)
+                    IntOffset(0, yOffset)
+                }
+                .background(Color.Red)
+                .padding(all = AppTheme.dimensions.padding.P_3)
+        )
     }
 }

1743e854af9a4d6a482bf4d5908109eb117af4ad
Faltenreich <philipp.fahlteich@gmail.com>
2024-02-20 20:57:58 +0100

    Implement rule of realistic measurement value

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/validation/RealisticMeasurementValueRule.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/validation/RealisticMeasurementValueRule.kt
@@ -1,17 +1,33 @@
 package com.faltenreich.diaguard.entry.form.validation
 
+import com.faltenreich.diaguard.MR
 import com.faltenreich.diaguard.entry.form.measurement.MeasurementTypeInputState
 import com.faltenreich.diaguard.measurement.value.MeasurementValueFormatter
 import com.faltenreich.diaguard.shared.di.inject
+import com.faltenreich.diaguard.shared.localization.Localization
 import com.faltenreich.diaguard.shared.validation.ValidationResult
 import com.faltenreich.diaguard.shared.validation.ValidationRule
 
 class RealisticMeasurementValueRule(
     private val measurementValueFormatter: MeasurementValueFormatter = inject(),
+    private val localization: Localization = inject(),
 ) : ValidationRule<MeasurementTypeInputState> {
 
     override fun check(input: MeasurementTypeInputState): ValidationResult<MeasurementTypeInputState> {
-        // TODO: Check whether values are within MeasurementType.minimumValue and .maximumValue
-        return ValidationResult.Success(input)
+        // TODO: Format according to selected unit
+        val value = input.input.toDoubleOrNull()
+        val (minimumValue, maximumValue) = input.type.minimumValue to input.type.maximumValue
+        return when (value) {
+            null -> ValidationResult.Success(input)
+            in minimumValue ..< maximumValue -> ValidationResult.Success(input)
+            else -> ValidationResult.Failure(
+                input,
+                error = localization.getString(
+                    MR.strings.entry_form_error_unrealistic_value,
+                    measurementValueFormatter.formatValue(minimumValue),
+                    measurementValueFormatter.formatValue(maximumValue),
+                ),
+            )
+        }
     }
 }

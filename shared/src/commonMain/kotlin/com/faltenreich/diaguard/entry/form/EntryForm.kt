7de1f59d4548f7e4b98092e29ed2fb857c5f9284
Faltenreich <philipp.fahlteich@gmail.com>
2024-03-24 16:46:51 +0100

    Redesign entry form

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/EntryForm.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/EntryForm.kt
@@ -1,5 +1,6 @@
 package com.faltenreich.diaguard.entry.form
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -25,7 +27,6 @@ import com.faltenreich.diaguard.shared.localization.getString
 import com.faltenreich.diaguard.shared.view.Divider
 import com.faltenreich.diaguard.shared.view.FormRow
 import com.faltenreich.diaguard.shared.view.ResourceIcon
-import com.faltenreich.diaguard.shared.view.TextDivider
 import com.faltenreich.diaguard.shared.view.TextInput
 
 @Composable
@@ -35,75 +36,79 @@ fun EntryForm(
 ) {
     val state = viewModel.collectState()
 
-    Column(
-        modifier = modifier.verticalScroll(rememberScrollState()),
-    ) {
-        FormRow(icon = { ResourceIcon(MR.images.ic_time) }) {
-            TextButton(onClick = { viewModel.dispatchIntent(EntryFormIntent.SelectDate) }) {
-                Text(viewModel.dateFormatted)
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        Column(modifier = modifier.background(AppTheme.colors.scheme.surfaceVariant)) {
+            FormRow(icon = { ResourceIcon(MR.images.ic_time) }) {
+                TextButton(
+                    onClick = { viewModel.dispatchIntent(EntryFormIntent.SelectDate) },
+                    colors = ButtonDefaults.textButtonColors(contentColor = AppTheme.colors.scheme.onSurfaceVariant),
+                ) {
+                    Text(viewModel.dateFormatted)
+                }
+                TextButton(
+                    onClick = { viewModel.dispatchIntent(EntryFormIntent.SelectDate) },
+                    colors = ButtonDefaults.textButtonColors(contentColor = AppTheme.colors.scheme.onSurfaceVariant),
+                ) {
+                    Text(viewModel.timeFormatted)
+                }
             }
-            TextButton(onClick = { viewModel.dispatchIntent(EntryFormIntent.SelectTime) }) {
-                Text(viewModel.timeFormatted)
+
+            Divider(modifier = Modifier.background(AppTheme.colors.scheme.onSurfaceVariant))
+
+            FormRow(icon = { ResourceIcon(MR.images.ic_tag) }) {
+                Column {
+                    EntryTagInput(
+                        input = viewModel.tag.collectAsState().value,
+                        onInputChange = { viewModel.tag.value = it },
+                        suggestions = state?.tags ?: emptyList(),
+                        onSuggestionSelected = { tag ->
+                            viewModel.dispatchIntent(EntryFormIntent.AddTag(tag))
+                            viewModel.tag.value = ""
+                        }
+                    )
+                    EntryTagList(
+                        tags = viewModel.tags,
+                        onTagClick = { tag -> viewModel.dispatchIntent(EntryFormIntent.RemoveTag(tag)) },
+                        trailingIcon = { tag ->
+                            ResourceIcon(
+                                imageResource = MR.images.ic_clear,
+                                contentDescription = getString(MR.strings.tag_remove_description, tag.name),
+                                modifier = Modifier.size(InputChipDefaults.AvatarSize),
+                            )
+                        },
+                        modifier = Modifier.padding(horizontal = AppTheme.dimensions.padding.P_3),
+                    )
+                }
             }
-        }
 
-        Divider()
+            Divider(modifier = Modifier.background(AppTheme.colors.scheme.onSurfaceVariant))
 
-        FormRow(icon = { ResourceIcon(MR.images.ic_tag) }) {
-            Column {
-                EntryTagInput(
-                    input = viewModel.tag.collectAsState().value,
-                    onInputChange = { viewModel.tag.value = it },
-                    suggestions = state?.tags ?: emptyList(),
-                    onSuggestionSelected = { tag ->
-                        viewModel.dispatchIntent(EntryFormIntent.AddTag(tag))
-                        viewModel.tag.value = ""
-                    }
+            FormRow(icon = { ResourceIcon(MR.images.ic_note) }) {
+                TextInput(
+                    input = viewModel.note,
+                    onInputChange = { viewModel.note = it },
+                    label = getString(MR.strings.note),
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next ),
                 )
-                EntryTagList(
-                    tags = viewModel.tags,
-                    onTagClick = { tag -> viewModel.dispatchIntent(EntryFormIntent.RemoveTag(tag)) },
-                    trailingIcon = { tag ->
-                        ResourceIcon(
-                            imageResource = MR.images.ic_clear,
-                            contentDescription = getString(MR.strings.tag_remove_description, tag.name),
-                            modifier = Modifier.size(InputChipDefaults.AvatarSize),
-                        )
-                    },
-                    modifier = Modifier.padding(horizontal = AppTheme.dimensions.padding.P_3),
+            }
+
+            Divider(modifier = Modifier.background(AppTheme.colors.scheme.onSurfaceVariant))
+
+            FormRow(icon = { ResourceIcon(MR.images.ic_alarm) }) {
+                TextInput(
+                    input = viewModel.alarmDelayInMinutes?.toString() ?: "",
+                    onInputChange = { viewModel.alarmDelayInMinutes = it.toIntOrNull() },
+                    label = getString(MR.strings.alarm),
+                    suffix = { Text(getString(MR.strings.minutes_until)) },
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next,
+                    ),
                 )
             }
         }
-
-        Divider()
-
-        FormRow(icon = { ResourceIcon(MR.images.ic_note) }) {
-            TextInput(
-                input = viewModel.note,
-                onInputChange = { viewModel.note = it },
-                label = getString(MR.strings.note),
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next ),
-            )
-        }
-
-        Divider()
-
-        FormRow(icon = { ResourceIcon(MR.images.ic_alarm) }) {
-            TextInput(
-                input = viewModel.alarmDelayInMinutes?.toString() ?: "",
-                onInputChange = { viewModel.alarmDelayInMinutes = it.toIntOrNull() },
-                label = getString(MR.strings.alarm),
-                suffix = { Text(getString(MR.strings.minutes_until)) },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Next,
-                ),
-            )
-        }
-
-        TextDivider(getString(MR.strings.measurement_properties))
 
         viewModel.measurements.forEach { measurement ->
             MeasurementPropertyInput(

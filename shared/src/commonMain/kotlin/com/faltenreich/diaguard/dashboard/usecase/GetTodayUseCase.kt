3792ac41a3fbbc448e89195e83aa9babacc0345b
Faltenreich <philipp.fahlteich@gmail.com>
2024-04-01 16:22:17 +0200

    Count extreme values in today's dashboard

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/usecase/GetTodayUseCase.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/usecase/GetTodayUseCase.kt
@@ -3,7 +3,6 @@ package com.faltenreich.diaguard.dashboard.usecase
 import com.faltenreich.diaguard.dashboard.DashboardViewState
 import com.faltenreich.diaguard.datetime.factory.DateTimeFactory
 import com.faltenreich.diaguard.measurement.property.MeasurementPropertyRepository
-import com.faltenreich.diaguard.measurement.value.MeasurementValue
 import com.faltenreich.diaguard.measurement.value.MeasurementValueRepository
 import com.faltenreich.diaguard.shared.di.inject
 import kotlinx.coroutines.flow.Flow
@@ -22,11 +21,19 @@ class GetTodayUseCase(
             propertyId = property.id,
             minDateTime = today.atStartOfDay(),
             maxDateTime = today.atEndOfDay(),
-        ).map { values: List<MeasurementValue> ->
+        ).map { values ->
             DashboardViewState.Revisit.Today(
                 totalCount = values.size,
-                hyperCount = 0, // TODO: Calculate count
-                hypoCount = 0, // TODO: Calculate count
+                hyperCount = values.count { value ->
+                    value.type.highValue?.let { hyperValue ->
+                        value.value > hyperValue
+                    } ?: false
+                },
+                hypoCount = values.count { value ->
+                    value.type.lowValue?.let { hypoValue ->
+                        value.value < hypoValue
+                    } ?: false
+                },
             )
         }
     }

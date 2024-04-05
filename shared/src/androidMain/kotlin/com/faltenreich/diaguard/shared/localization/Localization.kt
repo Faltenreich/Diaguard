14ff29e8e6bd9c3f8e0b2d596bc449132c1763df
Faltenreich <philipp.fahlteich@gmail.com>
2024-04-05 17:20:08 +0200

    Implement Android API for Localization Convert images from SVG to Android XML

--- shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/localization/Localization.kt
+++ shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/localization/Localization.kt
@@ -1,14 +1,16 @@
 package com.faltenreich.diaguard.shared.localization
 
+import diaguard.shared.generated.resources.Res
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.compose.resources.StringResource
 
 actual class Localization {
 
-    actual fun getString(resource: StringResource, vararg args: Any): String {
-        TODO()
+    actual fun getString(resource: StringResource, vararg args: Any): String = runBlocking {
+        org.jetbrains.compose.resources.getString(resource, args)
     }
 
-    actual fun getFile(path: String): String {
-        TODO()
+    actual fun getFile(path: String): String = runBlocking {
+        Res.readBytes(path).decodeToString()
     }
 }

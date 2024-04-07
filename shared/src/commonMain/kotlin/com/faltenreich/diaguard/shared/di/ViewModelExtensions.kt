864c924d79b92074f56e0a196590c4249d1c9048
Faltenreich <philipp.fahlteich@gmail.com>
2024-04-07 17:02:02 +0200

    Replace native- with multiplatform implementation for retrieving ViewModels within Screen

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/di/ViewModelExtensions.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/di/ViewModelExtensions.kt
@@ -1,13 +1,16 @@
 package com.faltenreich.diaguard.shared.di
 
 import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.koin.getScreenModel
 import com.faltenreich.diaguard.navigation.screen.Screen
 import com.faltenreich.diaguard.shared.architecture.ViewModel
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
 
 @Composable
-expect inline fun <reified T : ViewModel<*, *>> Screen.getViewModel(
+inline fun <reified T : ViewModel<*, *>> Screen.getViewModel(
     qualifier: Qualifier? = null,
     noinline parameters: ParametersDefinition? = null,
-): T
+): T {
+    return getScreenModel(qualifier, parameters)
+}

a19188514daeb329291b73e7df2962b6376c4d30
Faltenreich <philipp.fahlteich@gmail.com>
2024-04-07 13:06:55 +0200

    Extract FoodListSkeleton

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/list/FoodList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/list/FoodList.kt
@@ -3,27 +3,20 @@ package com.faltenreich.diaguard.food.list
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.food.Food
 import com.faltenreich.diaguard.shared.di.inject
 import com.faltenreich.diaguard.shared.localization.getString
 import com.faltenreich.diaguard.shared.view.Divider
-import com.faltenreich.diaguard.shared.view.skeleton
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.carbohydrates_per_100g
 import diaguard.shared.generated.resources.food
@@ -59,22 +52,7 @@ fun FoodList(
             )
         }
         when (state) {
-            is FoodListState.Loading, null -> Column(modifier) {
-                repeat(10) {
-                    Row(
-                        modifier = modifier
-                            .height(IntrinsicSize.Min)
-                            .defaultMinSize(minHeight = AppTheme.dimensions.size.TouchSizeLarge)
-                            .padding(AppTheme.dimensions.padding.P_3),
-                        horizontalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_2),
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Box(modifier = Modifier.weight(1f).skeleton(true))
-                        Box(modifier = Modifier.width(50.dp).skeleton(true))
-                    }
-                    Divider()
-                }
-            }
+            is FoodListState.Loading, null -> FoodListSkeleton(modifier = modifier)
             is FoodListState.Loaded -> LazyColumn(modifier = modifier) {
                 items(state.foodList, key = Food::id) { food ->
                     Column {
---
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/list/FoodListSkeleton.kt
@@ -0,0 +1,41 @@
+package com.faltenreich.diaguard.food.list
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.faltenreich.diaguard.AppTheme
+import com.faltenreich.diaguard.shared.view.Divider
+import com.faltenreich.diaguard.shared.view.skeleton
+
+@Composable
+fun FoodListSkeleton(modifier: Modifier = Modifier) {
+    LazyColumn(modifier) {
+        items(count = 20) {
+            Column {
+                Row(
+                    modifier = modifier
+                        .height(IntrinsicSize.Min)
+                        .defaultMinSize(minHeight = AppTheme.dimensions.size.TouchSizeLarge)
+                        .padding(AppTheme.dimensions.padding.P_3),
+                    horizontalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_2),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(modifier = Modifier.weight(1f).skeleton(true))
+                    Box(modifier = Modifier.width(50.dp).skeleton(true))
+                }
+                Divider()
+            }
+        }
+    }
+}

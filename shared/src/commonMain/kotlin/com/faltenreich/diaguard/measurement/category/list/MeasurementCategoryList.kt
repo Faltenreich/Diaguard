24008416d7715e5823ebbd6a17284bb81615793b
Faltenreich <philipp.fahlteich@gmail.com>
2024-05-07 17:32:32 +0200

    Animate changing sort index within MeasurementCategoryList

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/list/MeasurementCategoryList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/list/MeasurementCategoryList.kt
@@ -3,9 +3,8 @@ package com.faltenreich.diaguard.measurement.category.list
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.faltenreich.diaguard.shared.di.inject
@@ -19,11 +18,13 @@ fun MeasurementCategoryList(
 
     AnimatedVisibility(
         visible = state != null,
+        modifier = modifier,
         enter = fadeIn(),
     ) {
         val categories = state?.categories ?: emptyList()
-        Column(modifier = modifier.verticalScroll(rememberScrollState())) {
-            categories.forEachIndexed { index, category ->
+
+        LazyColumn {
+            itemsIndexed(categories, key = { _, category -> category.id }) { index, category ->
                 MeasurementCategoryListItem(
                     category = category,
                     onArrowUp = {
@@ -38,9 +39,9 @@ fun MeasurementCategoryList(
                         )
                     },
                     showArrowDown = index < categories.size - 1,
-                    modifier = Modifier.clickable {
-                        viewModel.dispatchIntent(MeasurementCategoryListIntent.Edit(category))
-                    },
+                    modifier = Modifier
+                        .animateItemPlacement()
+                        .clickable { viewModel.dispatchIntent(MeasurementCategoryListIntent.Edit(category)) },
                 )
             }
         }

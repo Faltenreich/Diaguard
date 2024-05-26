45313476b1c32a900f5d18fd0965e881ddcf4e35
Faltenreich <philipp.fahlteich@gmail.com>
2024-05-26 17:12:08 +0200

    Refactor DashboardViewModelTest

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/dashboard/DashboardViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/dashboard/DashboardViewModelTest.kt
@@ -2,22 +2,24 @@ package com.faltenreich.diaguard.dashboard
 
 import com.faltenreich.diaguard.DependencyInjection
 import com.faltenreich.diaguard.appModules
-import com.faltenreich.diaguard.shared.di.inject
 import com.faltenreich.diaguard.testModules
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.koin.test.KoinTest
+import org.koin.test.inject
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DashboardViewModelTest : KoinTest {
 
+    private val viewModel: DashboardViewModel by inject()
+
+    init {
+        DependencyInjection.setup(modules = appModules() + testModules())
+    }
+
     @Test
     fun `state is initially null`() = runBlocking {
-        // TODO: Implement operations
-        val modules = appModules() + testModules()
-        DependencyInjection.setup(modules = modules)
-        val viewModel: DashboardViewModel = inject()
         val state = viewModel.stateInScope.first()
         assertEquals(null, state)
     }

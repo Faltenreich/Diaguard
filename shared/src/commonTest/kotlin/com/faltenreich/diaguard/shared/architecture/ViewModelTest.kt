16b73eccb79db0c487afced83280f63d9c34de52
Faltenreich <philipp.fahlteich@gmail.com>
2024-05-27 19:18:27 +0200

    Test ViewModel

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/shared/architecture/ViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/shared/architecture/ViewModelTest.kt
@@ -1,23 +1,14 @@
 package com.faltenreich.diaguard.shared.architecture
 
-import com.faltenreich.diaguard.DependencyInjection
-import com.faltenreich.diaguard.appModules
-import com.faltenreich.diaguard.testModules
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.koin.test.KoinTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class ViewModelTest : KoinTest {
-
-    @BeforeTest
-    fun setup() {
-        DependencyInjection.setup(modules = appModules() + testModules())
-    }
+class ViewModelTest {
 
     @Test
     fun `state is initially null`() = runTest {
@@ -26,7 +17,7 @@ class ViewModelTest : KoinTest {
         assertEquals(null, state)
     }
 
-    private class SampleViewModel : ViewModel<Unit, Unit, Unit>() {
+    private class SampleViewModel : ViewModel<Unit, Unit, Unit>(dispatcher = StandardTestDispatcher()) {
         override val state: Flow<Unit> = flowOf()
     }
 }

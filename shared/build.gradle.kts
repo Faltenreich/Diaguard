70ef686391cbd02b0a3aecfac34a931651961160
Faltenreich <philipp.fahlteich@gmail.com>
2024-06-03 18:44:20 +0200

    Update to Kover 0.8.0

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.android.library)
     alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.sqldelight)

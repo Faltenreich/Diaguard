588b5bea6f22c17120c3b2c0d2efc1c383c6991e
Faltenreich <philipp.fahlteich@gmail.com>
2024-06-11 18:54:01 +0200

    Fix LegacySqliteDaoTest

--- shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacySqliteDaoTest.kt
+++ shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacySqliteDaoTest.kt
@@ -3,16 +3,16 @@ package com.faltenreich.diaguard.backup.legacy
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.faltenreich.diaguard.datetime.kotlinx.KotlinxDateTimeFactory
 import com.faltenreich.diaguard.shared.database.sqlite.SqliteDatabase
+import com.faltenreich.diaguard.shared.test.FileFactory
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.File
 
 
 @RunWith(AndroidJUnit4::class)
 class LegacySqliteDaoTest {
 
-    private val database = SqliteDatabase(file = File("TODO"))
+    private val database = SqliteDatabase(file = FileFactory.createFromAssets("diaguard.db"))
     private val dateTimeFactory = KotlinxDateTimeFactory()
     private val dao = LegacySqliteDao(database, dateTimeFactory)
 
--- shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/shared/database/sqlite/SqliteDatabaseTest.kt
+++ shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/shared/database/sqlite/SqliteDatabaseTest.kt
@@ -1,13 +1,10 @@
 package com.faltenreich.diaguard.shared.database.sqlite
 
-import android.os.FileUtils
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
+import com.faltenreich.diaguard.shared.test.FileFactory
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.File
-import java.io.FileOutputStream
 
 
 @RunWith(AndroidJUnit4::class)
@@ -15,15 +12,7 @@ class SqliteDatabaseTest {
 
     @Test
     fun readsData() {
-        val path = "diaguard.db"
-
-        val context = InstrumentationRegistry.getInstrumentation().context
-        val inputStream = context.resources.assets.open(path)
-
-        val file = File(context.externalCacheDir, path)
-        val outputStream = FileOutputStream(file)
-        FileUtils.copy(inputStream, outputStream)
-
+        val file = FileFactory.createFromAssets("diaguard.db")
         val database = SqliteDatabase(file)
 
         val ids = mutableListOf<Long?>()

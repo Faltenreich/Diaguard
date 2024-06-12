6ebe0b1f2c20d2874d9e9369a5da1bf386cf6c52
Faltenreich <philipp.fahlteich@gmail.com>
2024-06-12 18:37:05 +0200

    Improve test of legacy tags

--- shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacySqliteDaoTest.kt
+++ shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacySqliteDaoTest.kt
@@ -4,6 +4,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.faltenreich.diaguard.datetime.kotlinx.KotlinxDateTimeFactory
 import com.faltenreich.diaguard.shared.database.sqlite.SqliteDatabase
 import com.faltenreich.diaguard.shared.test.FileFactory
+import com.faltenreich.diaguard.tag.Tag
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,6 +29,52 @@ class LegacySqliteDaoTest {
 
     @Test
     fun readsTags() {
-        Assert.assertTrue(dao.getTags().isNotEmpty())
+        val expected = arrayOf(
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.888"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.888"),
+                name = "after the sport",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.898"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.898"),
+                name = "before the sport",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.898"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.898"),
+                name = "sick",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                name = "tired",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                name = "excited",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                name = "happy",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                name = "falling asleep",
+            ),
+            Tag.Legacy(
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:37.899"),
+                name = "just woke up",
+            ),
+        )
+        val actual = dao.getTags().toTypedArray()
+        Assert.assertArrayEquals(
+            expected,
+            actual,
+        )
     }
 }

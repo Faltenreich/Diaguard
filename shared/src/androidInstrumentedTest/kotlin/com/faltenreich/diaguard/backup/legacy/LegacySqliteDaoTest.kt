e8ea0efc3c23309e6a2194b7d20cc321c9958eea
Faltenreich <philipp.fahlteich@gmail.com>
2024-06-13 18:50:14 +0200

    Test real legacy food

--- shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacySqliteDaoTest.kt
+++ shared/src/androidInstrumentedTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacySqliteDaoTest.kt
@@ -3,6 +3,7 @@ package com.faltenreich.diaguard.backup.legacy
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.faltenreich.diaguard.datetime.kotlinx.KotlinxDateTimeFactory
 import com.faltenreich.diaguard.entry.tag.EntryTag
+import com.faltenreich.diaguard.food.Food
 import com.faltenreich.diaguard.shared.database.sqlite.SqliteDatabase
 import com.faltenreich.diaguard.shared.test.FileFactory
 import com.faltenreich.diaguard.tag.Tag
@@ -30,7 +31,48 @@ class LegacySqliteDaoTest {
 
     @Test
     fun readsFood() {
-        Assert.assertTrue(dao.getFood().isNotEmpty())
+        val expected = arrayOf(
+            Food.Legacy(
+                id = 1,
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:38.076"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:38.076"),
+                uuid = null,
+                name = "Onion quiche, baked (with cake dough)",
+                brand = null,
+                ingredients = "Onion quiche, baked (with cake dough)",
+                labels = "Common food",
+                carbohydrates = 15.0,
+                energy = 175.0,
+                fat = 10.0,
+                fatSaturated = 5.0,
+                fiber = 1.6,
+                proteins = 5.0,
+                salt = 4.1,
+                sodium = 0.4,
+                sugar = 5.1,
+            ),
+            Food.Legacy(
+                id = 962,
+                createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:38.194"),
+                updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:38.194"),
+                uuid = null,
+                name = "Cola",
+                brand = "",
+                ingredients = "",
+                labels = null,
+                carbohydrates = 10.0,
+                energy = -1.0,
+                fat = -1.0,
+                fatSaturated = -1.0,
+                fiber = -1.0,
+                proteins = -1.0,
+                salt = -1.0,
+                sodium = -1.0,
+                sugar = -1.0,
+            ),
+        )
+        val actual = dao.getFood().toTypedArray()
+        Assert.assertArrayEquals(expected, actual)
     }
 
     @Test
@@ -91,10 +133,7 @@ class LegacySqliteDaoTest {
             ),
         )
         val actual = dao.getTags().toTypedArray()
-        Assert.assertArrayEquals(
-            expected,
-            actual,
-        )
+        Assert.assertArrayEquals(expected, actual)
     }
 
     @Test
@@ -108,9 +147,6 @@ class LegacySqliteDaoTest {
             )
         )
         val actual = dao.getEntryTags().toTypedArray()
-        Assert.assertArrayEquals(
-            expected,
-            actual,
-        )
+        Assert.assertArrayEquals(expected, actual)
     }
 }

312df9327d07c7edc50da8ccfae6282c0b461d26
Faltenreich <philipp.fahlteich@gmail.com>
2024-06-15 12:47:53 +0200

    Fix LegacyFakeDao

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacyFakeDao.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/backup/legacy/LegacyFakeDao.kt
@@ -1,6 +1,9 @@
 package com.faltenreich.diaguard.backup.legacy
 
 import com.faltenreich.diaguard.entry.Entry
+import com.faltenreich.diaguard.entry.tag.EntryTag
+import com.faltenreich.diaguard.food.Food
+import com.faltenreich.diaguard.food.eaten.FoodEaten
 import com.faltenreich.diaguard.measurement.value.MeasurementValue
 import com.faltenreich.diaguard.tag.Tag
 
@@ -14,7 +17,20 @@ open class LegacyFakeDao : LegacyDao {
         return emptyList()
     }
 
+
+    override fun getFood(): List<Food.Legacy> {
+        return emptyList()
+    }
+
+    override fun getFoodEaten(): List<FoodEaten.Legacy> {
+        return emptyList()
+    }
+
     override fun getTags(): List<Tag.Legacy> {
+        return emptyList()
+    }
+
+    override fun getEntryTags(): List<EntryTag.Legacy> {
         return emptyList()
     }
 }

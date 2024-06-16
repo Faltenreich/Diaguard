2b32dc6b1d64e50eaa96731b1f5c9cab04d8b449
Faltenreich <philipp.fahlteich@gmail.com>
2024-06-16 15:44:34 +0200

    Simplify DashboardViewModelTest

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/dashboard/DashboardViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/dashboard/DashboardViewModelTest.kt
@@ -2,18 +2,15 @@ package com.faltenreich.diaguard.dashboard
 
 import com.faltenreich.diaguard.TestSuite
 import com.faltenreich.diaguard.backup.ImportUseCase
-import com.faltenreich.diaguard.datetime.factory.DateTimeFactory
-import com.faltenreich.diaguard.entry.Entry
-import com.faltenreich.diaguard.measurement.value.tint.MeasurementValueTint
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.koin.test.inject
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class DashboardViewModelTest : TestSuite {
 
-    private val dateTimeFactory: DateTimeFactory by inject()
     private val import: ImportUseCase by inject()
 
     private val viewModel: DashboardViewModel by inject()
@@ -33,37 +30,15 @@ class DashboardViewModelTest : TestSuite {
     }
 
     @Test
-    fun `state contains blank content if seed data is available`() = runTest {
+    fun `state contains content if seed data is available`() = runTest {
         import()
 
-        assertEquals(
-            DashboardViewState(
-                latestBloodSugar = DashboardViewState.LatestBloodSugar(
-                    entry = Entry.Local(
-                        id= 0,
-                        createdAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:38.200"),
-                        updatedAt = dateTimeFactory.dateTime(isoString = "2024-06-08T18:46:38.200"),
-                        dateTime = dateTimeFactory.dateTime(isoString = "2024-05-09T08:00"),
-                        note = "Hello, World",
-                    ),
-                    value = "5,55",
-                    tint = MeasurementValueTint.NORMAL,
-                    timePassed = "date_time_ago_days",
-                ),
-                today = DashboardViewState.Today(
-                    totalCount = 3,
-                    hypoCount = 0,
-                    hyperCount = 0,
-                ),
-                average = DashboardViewState.Average(
-                    day = "5,92",
-                    week = "5,92",
-                    month = "5,92",
-                ),
-                hbA1c = null,
-                trend = null,
-            ),
-            viewModel.state.first(),
-        )
+        val state = viewModel.state.first()
+
+        assertNotNull(state.latestBloodSugar)
+        assertNotNull(state.today)
+        assertNotNull(state.average)
+        // TODO: assertNotNull(state.hbA1c)
+        // TODO: assertNotNull(state.trend)
     }
 }

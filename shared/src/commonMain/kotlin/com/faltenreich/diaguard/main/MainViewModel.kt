b9e473e76f50c3f8db2ecb94ab5f597ac9d00542
Faltenreich <philipp.fahlteich@gmail.com>
2024-08-02 18:25:22 +0200

    Fix navigation from BottomSheetNavigation

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/main/MainViewModel.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/main/MainViewModel.kt
@@ -3,15 +3,15 @@ package com.faltenreich.diaguard.main
 import com.faltenreich.diaguard.backup.ImportUseCase
 import com.faltenreich.diaguard.dashboard.DashboardScreen
 import com.faltenreich.diaguard.log.LogScreen
-import com.faltenreich.diaguard.navigation.bottomsheet.CloseBottomSheetUseCase
-import com.faltenreich.diaguard.navigation.screen.GetActiveScreenUseCase
-import com.faltenreich.diaguard.navigation.screen.GetCurrentScreenUseCase
-import com.faltenreich.diaguard.navigation.modal.GetModalUseCase
 import com.faltenreich.diaguard.navigation.NavigateBackUseCase
 import com.faltenreich.diaguard.navigation.NavigateToScreenUseCase
 import com.faltenreich.diaguard.navigation.NavigationIntent
-import com.faltenreich.diaguard.navigation.bottomsheet.OpenBottomSheetUseCase
+import com.faltenreich.diaguard.navigation.bottomsheet.CloseBottomSheetUseCase
 import com.faltenreich.diaguard.navigation.bottomsheet.GetBottomSheetUseCase
+import com.faltenreich.diaguard.navigation.bottomsheet.OpenBottomSheetUseCase
+import com.faltenreich.diaguard.navigation.modal.GetModalUseCase
+import com.faltenreich.diaguard.navigation.screen.GetActiveScreenUseCase
+import com.faltenreich.diaguard.navigation.screen.GetCurrentScreenUseCase
 import com.faltenreich.diaguard.preference.StartScreen
 import com.faltenreich.diaguard.preference.store.GetPreferenceUseCase
 import com.faltenreich.diaguard.shared.architecture.ViewModel
@@ -72,7 +72,10 @@ class MainViewModel(
 
     override suspend fun handleIntent(intent: NavigationIntent) = with(intent) {
         when (this) {
-            is NavigationIntent.NavigateTo -> closeBottomSheet()
+            is NavigationIntent.NavigateTo -> {
+                closeBottomSheet()
+                navigateToScreen(screen)
+            }
             is NavigationIntent.NavigateBack -> navigateBack()
             is NavigationIntent.OpenBottomSheet -> openBottomSheet(screen)
             is NavigationIntent.CloseBottomSheet -> closeBottomSheet()

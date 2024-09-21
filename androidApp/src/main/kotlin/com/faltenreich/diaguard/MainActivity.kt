323d36a0bd85c049a3c02a8d624913aaace6e1d0
Faltenreich <philipp.fahlteich@gmail.com>
2024-09-21 19:33:46 +0200

    Simplify theme

--- androidApp/src/main/kotlin/com/faltenreich/diaguard/MainActivity.kt
+++ androidApp/src/main/kotlin/com/faltenreich/diaguard/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.faltenreich.diaguard
 
-import android.os.Build
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
@@ -10,10 +11,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window.isNavigationBarContrastEnforced = false
-        }
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+        )
 
         setContent {
             AppView()

03b08771f04a535dabc494c7449b24a1e31c8106
Faltenreich <philipp.fahlteich@gmail.com>
2024-09-23 19:09:15 +0200

    Copy aboutlibraries.json manually into resources

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/license/LicenseScreen.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/license/LicenseScreen.kt
@@ -1,27 +1,40 @@
 package com.faltenreich.diaguard.preference.license
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.navigation.bar.top.TopAppBarStyle
 import com.faltenreich.diaguard.navigation.screen.Screen
+import com.faltenreich.diaguard.shared.localization.getString
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import com.mikepenz.aboutlibraries.ui.compose.m3.rememberLibraries
 import diaguard.shared.generated.resources.Res
+import diaguard.shared.generated.resources.licenses
 import kotlinx.serialization.Serializable
 
 @Serializable
 data object LicenseScreen : Screen {
 
     @Composable
+    override fun TopAppBar(): TopAppBarStyle {
+        return TopAppBarStyle.CenterAligned {
+            Text(getString(Res.string.licenses))
+        }
+    }
+
+    @Composable
     override fun Content() {
         val libraries by rememberLibraries {
-            // FIXME: Missing resource with path: composeResources/diaguard.shared.generated.resources/files/aboutlibraries.json
             Res.readBytes("files/aboutlibraries.json").decodeToString()
         }
         LibrariesContainer(
             libraries = libraries,
             modifier = Modifier.fillMaxSize(),
+            onLibraryClick = { library ->
+                TODO()
+            },
         )
     }
 }

c7b0cdeec3c978755f0cff40b97414d5de2f3b06
Faltenreich <philipp.fahlteich@gmail.com>
2024-09-27 17:26:52 +0200

    Add padding to EntryListItems in search and tag detail

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearch.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearch.kt
@@ -1,8 +1,9 @@
 package com.faltenreich.diaguard.entry.search
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -11,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.entry.Entry
 import com.faltenreich.diaguard.entry.list.EntryListItem
 import com.faltenreich.diaguard.shared.di.inject
 import com.faltenreich.diaguard.shared.localization.getString
-import com.faltenreich.diaguard.shared.view.Divider
 import com.faltenreich.diaguard.shared.view.LoadingIndicator
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.entry_search_placeholder
@@ -39,19 +40,21 @@ fun EntrySearch(
                 val listState = rememberLazyListState()
                 LazyColumn(
                     state = listState,
-                    modifier = modifier) {
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(all = AppTheme.dimensions.padding.P_2),
+                    verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_2),
+                ) {
                     items(items = viewState.items ?: emptyList(), key = Entry.Local::id) { entry ->
-                        Column(modifier = Modifier.animateItem()) {
-                            EntryListItem(
-                                entry = entry,
-                                onClick = { viewModel.dispatchIntent(EntrySearchIntent.OpenEntry(entry)) },
-                                onTagClick = { tag ->
-                                    viewModel.query = tag.name
-                                    scope.launch { listState.scrollToItem(0) }
-                                }
-                            )
-                            Divider()
-                        }
+                        EntryListItem(
+                            entry = entry,
+                            onClick = { viewModel.dispatchIntent(EntrySearchIntent.OpenEntry(entry)) },
+                            onTagClick = { tag ->
+                                viewModel.query = tag.name
+                                scope.launch { listState.scrollToItem(0) }
+                            },
+                            modifier = Modifier.animateItem(),
+                        )
                     }
                 }
             }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/tag/detail/TagDetail.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/tag/detail/TagDetail.kt
@@ -1,13 +1,14 @@
 package com.faltenreich.diaguard.tag.detail
 
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.entry.list.EntryListItem
 import com.faltenreich.diaguard.entry.tag.EntryTag
 import com.faltenreich.diaguard.shared.di.inject
@@ -38,21 +39,27 @@ fun TagDetail(
                 )
             }
         }
+
         item { Divider(orientation = Orientation.Vertical) }
+
         items(
             items = state?.entryTags ?: emptyList(),
             key = EntryTag.Local::id,
         ) { entryTag ->
-            Column(modifier = Modifier.animateItem()) {
-                EntryListItem(
-                    entry = entryTag.entry,
-                    onClick = { viewModel.dispatchIntent(TagDetailIntent.OpenEntry(entryTag.entry)) },
-                    onTagClick = { tag ->
-                        viewModel.dispatchIntent(TagDetailIntent.OpenEntrySearch(query = tag.name))
-                    },
-                )
-                Divider(orientation = Orientation.Vertical)
-            }
+            EntryListItem(
+                entry = entryTag.entry,
+                onClick = { viewModel.dispatchIntent(TagDetailIntent.OpenEntry(entryTag.entry)) },
+                onTagClick = { tag ->
+                    viewModel.dispatchIntent(TagDetailIntent.OpenEntrySearch(query = tag.name))
+                },
+                modifier = Modifier
+                    .padding(
+                        start = AppTheme.dimensions.padding.P_2,
+                        top = AppTheme.dimensions.padding.P_2,
+                        end = AppTheme.dimensions.padding.P_2,
+                    )
+                    .animateItem(),
+            )
         }
     }
 }

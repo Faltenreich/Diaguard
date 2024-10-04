4299746660db1eb105d6588ce29fe148f15cec8d
Faltenreich <philipp.fahlteich@gmail.com>
2024-10-04 16:01:57 +0200

    Simplify EntryList

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/list/EntryList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/list/EntryList.kt
@@ -1,13 +1,15 @@
 package com.faltenreich.diaguard.entry.list
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import app.cash.paging.compose.LazyPagingItems
@@ -15,8 +17,11 @@ import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.datetime.list.DateListItemStyle
 import com.faltenreich.diaguard.entry.Entry
 import com.faltenreich.diaguard.log.item.LogLoadingIndicator
-import com.faltenreich.diaguard.shared.logging.Logger
+import com.faltenreich.diaguard.shared.localization.getString
+import com.faltenreich.diaguard.shared.view.LoadingIndicator
 import com.faltenreich.diaguard.tag.Tag
+import diaguard.shared.generated.resources.Res
+import diaguard.shared.generated.resources.entry_search_placeholder
 
 @Composable
 fun EntryList(
@@ -25,42 +30,46 @@ fun EntryList(
     onTagClick: (Tag) -> Unit,
     modifier: Modifier = Modifier,
     listState: LazyListState = rememberLazyListState(),
-    header: LazyListScope.() -> Unit = {},
 ) {
-    Logger.debug("EntryList updated: ${items.loadState}")
-
-    LazyColumn(
-        state = listState,
+    Box(
         modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
     ) {
-        header()
+        when {
+            items.loadState.refresh == LoadState.Loading -> LoadingIndicator()
 
-        items(
-            count = items.itemCount,
-            key = { index -> checkNotNull(items.peek(index)).id },
-        ) { index ->
-            val entry = checkNotNull(items[index])
-            EntryListItem(
-                entry = entry,
-                style = DateListItemStyle(
-                    isVisible = true,
-                    isHighlighted = false,
-                ),
-                onClick = { onEntryClick(entry) },
-                onTagClick = onTagClick,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        horizontal = AppTheme.dimensions.padding.P_3,
-                        vertical = AppTheme.dimensions.padding.P_2,
+            items.itemCount == 0 -> Text(getString(Res.string.entry_search_placeholder))
+
+            else -> LazyColumn(
+                state = listState,
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                items(
+                    count = items.itemCount,
+                    key = { index -> checkNotNull(items.peek(index)).id },
+                ) { index ->
+                    val entry = checkNotNull(items[index])
+
+                    EntryListItem(
+                        entry = entry,
+                        style = DateListItemStyle(
+                            isVisible = true,
+                            isHighlighted = false,
+                        ),
+                        onClick = { onEntryClick(entry) },
+                        onTagClick = onTagClick,
+                        modifier = Modifier.fillMaxWidth().padding(
+                            horizontal = AppTheme.dimensions.padding.P_3,
+                            vertical = AppTheme.dimensions.padding.P_2,
+                        ).animateItem(),
                     )
-                    .animateItem(),
-            )
-        }
+                }
 
-        if (items.loadState.append == LoadState.Loading) {
-            item {
-                LogLoadingIndicator(modifier = Modifier.fillMaxWidth())
+                if (items.loadState.append == LoadState.Loading) {
+                    item {
+                        LogLoadingIndicator(modifier = Modifier.fillMaxWidth())
+                    }
+                }
             }
         }
     }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/tag/detail/TagDetail.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/tag/detail/TagDetail.kt
@@ -1,6 +1,7 @@
 package com.faltenreich.diaguard.tag.detail
 
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,27 +23,24 @@ fun TagDetail(
 ) {
     val items = viewModel.pagingData.collectAsLazyPagingItems()
 
-    EntryList(
-        items = items,
-        onEntryClick = { entry -> viewModel.dispatchIntent(TagDetailIntent.OpenEntry(entry)) },
-        onTagClick = { tag ->
-            viewModel.dispatchIntent(TagDetailIntent.OpenEntrySearch(query = tag.name))
-        },
-        modifier = modifier,
-        header = {
-            stickyHeader {
-                FormRow {
-                    TextInput(
-                        input = viewModel.name,
-                        onInputChange = { input -> viewModel.name = input },
-                        label = getString(Res.string.name),
-                        modifier = Modifier.fillMaxWidth(),
-                        supportingText = viewModel.error?.let { error -> { Text(error) } },
-                        isError = viewModel.error != null,
-                    )
-                }
-            }
-            item { Divider(orientation = Orientation.Vertical) }
-        },
-    )
+    Column(modifier = modifier) {
+        FormRow {
+            TextInput(
+                input = viewModel.name,
+                onInputChange = { input -> viewModel.name = input },
+                label = getString(Res.string.name),
+                modifier = Modifier.fillMaxWidth(),
+                supportingText = viewModel.error?.let { error -> { Text(error) } },
+                isError = viewModel.error != null,
+            )
+        }
+
+        Divider(orientation = Orientation.Vertical)
+
+        EntryList(
+            items = items,
+            onEntryClick = { entry -> viewModel.dispatchIntent(TagDetailIntent.OpenEntry(entry)) },
+            onTagClick = { tag -> viewModel.dispatchIntent(TagDetailIntent.OpenEntrySearch(query = tag.name)) },
+        )
+    }
 }

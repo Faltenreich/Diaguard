9bf06672ba22b31e29494f723cd40fab44b489f6
Faltenreich <philipp.fahlteich@gmail.com>
2024-10-16 19:17:21 +0200

    Prepare Composable for FoodPreferenceList

---
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/food/FoodPreferenceList.kt
@@ -0,0 +1,12 @@
+package com.faltenreich.diaguard.preference.food
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.preference.list.PreferenceList
+
+@Composable
+fun FoodPreferenceList(
+    modifier: Modifier = Modifier,
+) {
+    PreferenceList(modifier = modifier)
+}
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/food/FoodPreferenceListScreen.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/food/FoodPreferenceListScreen.kt
@@ -1,14 +1,26 @@
 package com.faltenreich.diaguard.preference.food
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.faltenreich.diaguard.navigation.bar.top.TopAppBarStyle
 import com.faltenreich.diaguard.navigation.screen.Screen
+import com.faltenreich.diaguard.shared.localization.getString
+import diaguard.shared.generated.resources.Res
+import diaguard.shared.generated.resources.food
 import kotlinx.serialization.Serializable
 
 @Serializable
 data object FoodPreferenceListScreen : Screen {
 
     @Composable
+    override fun TopAppBar(): TopAppBarStyle {
+        return TopAppBarStyle.CenterAligned {
+            Text(getString(Res.string.food))
+        }
+    }
+
+    @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+        FoodPreferenceList()
     }
 }

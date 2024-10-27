7b1c826d520f5a8b2b12ca8c65efbed3d69186fd
Faltenreich <philipp.fahlteich@gmail.com>
2024-10-27 14:51:30 +0100

    Extend MainViewModelTest

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/main/MainViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/main/MainViewModelTest.kt
@@ -2,14 +2,25 @@ package com.faltenreich.diaguard.main
 
 import app.cash.turbine.test
 import com.faltenreich.diaguard.TestSuite
+import com.faltenreich.diaguard.dashboard.DashboardScreen
+import com.faltenreich.diaguard.log.LogScreen
+import com.faltenreich.diaguard.main.menu.MainMenuScreen
+import com.faltenreich.diaguard.navigation.Navigation
+import com.faltenreich.diaguard.preference.StartScreen
+import com.faltenreich.diaguard.preference.store.SetPreferenceUseCase
+import com.faltenreich.diaguard.timeline.TimelineScreen
 import kotlinx.coroutines.test.runTest
 import org.koin.test.inject
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class MainViewModelTest : TestSuite {
 
     private val viewModel: MainViewModel by inject()
+    private val setPreference: SetPreferenceUseCase by inject()
+    private val navigation: Navigation by inject()
 
     @Test
     fun `shows loading on first start`() = runTest {
@@ -28,5 +39,63 @@ class MainViewModelTest : TestSuite {
             assertTrue(awaitItem() is MainState.SubsequentStart)
             ensureAllEventsConsumed()
         }
+    }
+
+    @Test
+    fun `shows dashboard as start screen by default`() = runTest {
+        importSeed()
+
+        viewModel.state.test {
+            val state = awaitItem()
+            assertTrue(state is MainState.SubsequentStart)
+            assertEquals(DashboardScreen, state.startScreen)
+            ensureAllEventsConsumed()
+        }
+    }
+
+    @Test
+    fun `shows timeline as start screen if selected`() = runTest {
+        importSeed()
+        setPreference(StartScreen.Preference, StartScreen.TIMELINE)
+
+        viewModel.state.test {
+            val state = awaitItem()
+            assertTrue(state is MainState.SubsequentStart)
+            assertEquals(TimelineScreen, state.startScreen)
+            ensureAllEventsConsumed()
+        }
+    }
+
+    @Test
+    fun `shows log as start screen if selected`() = runTest {
+        importSeed()
+        setPreference(StartScreen.Preference, StartScreen.LOG)
+
+        viewModel.state.test {
+            val state = awaitItem()
+            assertTrue(state is MainState.SubsequentStart)
+            assertEquals(LogScreen, state.startScreen)
+            ensureAllEventsConsumed()
+        }
+    }
+
+    @Test
+    fun `opens bottom sheet`() = runTest {
+        val screen = MainMenuScreen
+
+        viewModel.handleIntent(MainIntent.OpenBottomSheet(screen))
+
+        assertEquals(
+            expected = screen,
+            actual = navigation.bottomSheet.value,
+        )
+    }
+
+    @Test
+    fun `closes bottom sheet`() = runTest {
+        viewModel.handleIntent(MainIntent.OpenBottomSheet(MainMenuScreen))
+        viewModel.handleIntent(MainIntent.CloseBottomSheet)
+
+        assertNull(navigation.bottomSheet.value)
     }
 }

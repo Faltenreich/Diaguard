9c22fd281c6597766c1bd016c7e7c97ddff4507e
Faltenreich <philipp.fahlteich@gmail.com>
2024-10-27 15:19:46 +0100

    Extend DashboardViewModelTest

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/dashboard/DashboardViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/dashboard/DashboardViewModelTest.kt
@@ -16,7 +16,7 @@ class DashboardViewModelTest : TestSuite {
     private val viewModel: DashboardViewModel by inject()
 
     @Test
-    fun `state contains empty content if no data is available`() = runTest {
+    fun `shows empty content if no data is available`() = runTest {
         val state = viewModel.state.first()
 
         assertNull(state.latestBloodSugar)
@@ -35,7 +35,17 @@ class DashboardViewModelTest : TestSuite {
     }
 
     @Test
-    fun `state contains content if data is available`() = runTest {
+    fun `shows latest blood sugar if data is available`() = runTest {
+        importSeed()
+        storeValue(value = 120.0, propertyKey = DatabaseKey.MeasurementProperty.BLOOD_SUGAR)
+
+        val state = viewModel.state.first()
+
+        assertNotNull(state.latestBloodSugar)
+    }
+
+    @Test
+    fun `shows today's counts if data is available`() = runTest {
         importSeed()
         storeValue(value = 50.0, propertyKey = DatabaseKey.MeasurementProperty.BLOOD_SUGAR)
         storeValue(value = 120.0, propertyKey = DatabaseKey.MeasurementProperty.BLOOD_SUGAR)
@@ -43,18 +53,44 @@ class DashboardViewModelTest : TestSuite {
 
         val state = viewModel.state.first()
 
-        assertNotNull(state.latestBloodSugar)
-
         assertEquals(expected = 3, actual = state.today.totalCount)
         assertEquals(expected = 1, actual = state.today.hypoCount)
         assertEquals(expected = 1, actual = state.today.hyperCount)
+    }
+
+    @Test
+    fun `shows average values if data is available`() = runTest {
+        importSeed()
+        storeValue(value = 120.0, propertyKey = DatabaseKey.MeasurementProperty.BLOOD_SUGAR)
+
+        val state = viewModel.state.first()
 
         assertNotNull(state.average.day)
         assertNotNull(state.average.week)
         assertNotNull(state.average.month)
+    }
 
-        assertNotNull(state.hbA1c.value)
+    @Test
+    fun `shows latest HbA1c if data is available`() = runTest {
+        importSeed()
+        storeValue(value = 6.0, propertyKey = DatabaseKey.MeasurementProperty.HBA1C)
 
-        // TODO: assertTrue(state.trend.values.isNotEmpty())
+        val state = viewModel.state.first()
+
+        assertNotNull(state.hbA1c)
+        assertEquals(expected = "hba1c_latest", actual = state.hbA1c.label)
+        // FIXME: assertEquals(expected = MeasurementValue.Localized("6"), actual = state.hbA1c.value)
+    }
+
+    @Test
+    fun `shows estimated HbA1c if no values for HbA1c but Blood Sugar have been entered`() = runTest {
+        importSeed()
+        storeValue(value = 120.0, propertyKey = DatabaseKey.MeasurementProperty.BLOOD_SUGAR)
+
+        val state = viewModel.state.first()
+
+        assertNotNull(state.hbA1c)
+        assertEquals(expected = "hba1c_estimated", actual = state.hbA1c.label)
+        // FIXME: assertEquals(expected = MeasurementValue.Localized("6"), actual = state.hbA1c.value)
     }
 }

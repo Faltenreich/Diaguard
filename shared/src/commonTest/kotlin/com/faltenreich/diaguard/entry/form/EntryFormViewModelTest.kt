7b3ecabddc672c3b54f6b87362555636f56c2e2a
Faltenreich <philipp.fahlteich@gmail.com>
2024-10-29 19:55:58 +0100

    Test tags in EntryFormViewModel

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/entry/form/EntryFormViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/entry/form/EntryFormViewModelTest.kt
@@ -2,11 +2,16 @@ package com.faltenreich.diaguard.entry.form
 
 import app.cash.turbine.test
 import com.faltenreich.diaguard.TestSuite
+import com.faltenreich.diaguard.datetime.picker.DatePickerModal
+import com.faltenreich.diaguard.datetime.picker.TimePickerModal
+import com.faltenreich.diaguard.navigation.Navigation
+import com.faltenreich.diaguard.tag.Tag
 import kotlinx.coroutines.test.runTest
 import org.koin.core.parameter.parametersOf
 import org.koin.test.inject
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class EntryFormViewModelTest : TestSuite {
@@ -14,6 +19,7 @@ class EntryFormViewModelTest : TestSuite {
     private val viewModel: EntryFormViewModel by inject(
         parameters = { parametersOf(null, null, null) },
     )
+    private val navigation: Navigation by inject()
 
     @BeforeTest
     override fun beforeTest() {
@@ -22,10 +28,52 @@ class EntryFormViewModelTest : TestSuite {
     }
 
     @Test
+    fun `opens dialog when selecting date`() = runTest {
+        viewModel.handleIntent(EntryFormIntent.SelectDate)
+        assertTrue(navigation.modal.value is DatePickerModal)
+    }
+
+    @Test
+    fun `opens dialog when selecting time`() = runTest {
+        viewModel.handleIntent(EntryFormIntent.SelectTime)
+        assertTrue(navigation.modal.value is TimePickerModal)
+    }
+
+    @Test
+    fun `opens food search when selecting food`() = runTest {
+        viewModel.handleIntent(EntryFormIntent.SelectFood)
+        // TODO: Test NavController
+        // assertTrue(navigation.currentScreen.value is FoodSearchScreen)
+    }
+
+    @Test
     fun `suggests tags`() = runTest {
         viewModel.state.test {
             val state = awaitItem()
             assertTrue(state.tags.isNotEmpty())
+        }
+    }
+
+    @Test
+    fun `adds tag to selection`() = runTest {
+        val tag = Tag.User(name = "tag")
+        viewModel.handleIntent(EntryFormIntent.AddTag(tag))
+
+        viewModel.tagSelection.test {
+            val tags = awaitItem()
+            assertTrue(tags.contains(tag))
+        }
+    }
+
+    @Test
+    fun `removes tag from selection`() = runTest {
+        val tag = Tag.User(name = "tag")
+        viewModel.handleIntent(EntryFormIntent.AddTag(tag))
+        viewModel.handleIntent(EntryFormIntent.RemoveTag(tag))
+
+        viewModel.tagSelection.test {
+            val tags = awaitItem()
+            assertFalse(tags.contains(tag))
         }
     }
 }

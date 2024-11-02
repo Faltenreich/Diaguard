23d125823f70246aee0a8cb4152179087b076fd6
Faltenreich <philipp.fahlteich@gmail.com>
2024-11-02 19:35:38 +0100

    Test FoodFormIntent.EditNutrient

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/food/form/FoodFormViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/food/form/FoodFormViewModelTest.kt
@@ -5,6 +5,8 @@ import com.faltenreich.diaguard.TestSuite
 import com.faltenreich.diaguard.food.Food
 import com.faltenreich.diaguard.food.FoodRepository
 import com.faltenreich.diaguard.food.eaten.list.FoodEatenListScreen
+import com.faltenreich.diaguard.food.nutrient.FoodNutrient
+import com.faltenreich.diaguard.food.nutrient.FoodNutrientData
 import com.faltenreich.diaguard.navigation.Navigation
 import com.faltenreich.diaguard.navigation.NavigationEvent
 import kotlinx.coroutines.test.runTest
@@ -13,6 +15,7 @@ import org.koin.test.get
 import org.koin.test.inject
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
@@ -31,6 +34,21 @@ class FoodFormViewModelTest : TestSuite {
         val foodId = foodRepository.create(FOOD_BY_USER)
         food = requireNotNull(foodRepository.getById(foodId))
         viewModel = get(parameters = { parametersOf(foodId) })
+    }
+
+    @Test
+    fun `edit nutrient`() = runTest {
+        val data = FoodNutrientData(
+            nutrient = FoodNutrient.CARBOHYDRATES,
+            per100g = "100",
+            isLast = false,
+        )
+        viewModel.handleIntent(FoodFormIntent.EditNutrient(data))
+
+        assertEquals(
+            expected = data.per100g,
+            actual = viewModel.carbohydrates,
+        )
     }
 
     @Test

62388902f6354f54b71e88a56c962d8c7e62d265
Faltenreich <philipp.fahlteich@gmail.com>
2024-11-03 16:30:11 +0100

    Test state changes of ExportFormViewModel

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/export/ExportFormViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/export/ExportFormViewModelTest.kt
@@ -4,6 +4,7 @@ import app.cash.turbine.test
 import com.faltenreich.diaguard.TestSuite
 import com.faltenreich.diaguard.datetime.DateUnit
 import com.faltenreich.diaguard.datetime.factory.DateTimeFactory
+import com.faltenreich.diaguard.export.form.ExportFormIntent
 import com.faltenreich.diaguard.export.form.ExportFormMeasurementCategory
 import com.faltenreich.diaguard.export.form.ExportFormViewModel
 import com.faltenreich.diaguard.export.pdf.PdfLayout
@@ -137,6 +138,126 @@ class ExportFormViewModelTest : TestSuite {
             assertContentEquals(
                 expected = exportCategories,
                 actual = awaitItem().content.categories,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set date range`() = runTest {
+        val dateRangeEnd = dateTimeFactory.date(year = 1970, monthNumber = 1, dayOfMonth = 1)
+        val dateRangeStart = dateRangeEnd.minus(1, DateUnit.WEEK)
+        val dateRange = dateRangeStart .. dateRangeEnd
+
+        viewModel.handleIntent(ExportFormIntent.SetDateRange(dateRange))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = dateRange,
+                awaitItem().date.dateRange,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to select type`() = runTest {
+        val type = ExportType.CSV
+        viewModel.handleIntent(ExportFormIntent.SelectType(type))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = type,
+                actual = awaitItem().type.selection,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to select layout`() = runTest {
+        val layout = PdfLayout.LOG
+        viewModel.handleIntent(ExportFormIntent.SelectLayout(layout))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = layout,
+                actual = awaitItem().layout.selection,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set includeCalendarWeek`() = runTest {
+        val includeCalendarWeek = false
+        viewModel.handleIntent(ExportFormIntent.SetIncludeCalendarWeek(includeCalendarWeek))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = includeCalendarWeek,
+                actual = awaitItem().date.includeCalendarWeek,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set includeDateOfExport`() = runTest {
+        val includeDateOfExport = false
+        viewModel.handleIntent(ExportFormIntent.SetIncludeDateOfExport(includeDateOfExport))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = includeDateOfExport,
+                actual = awaitItem().date.includeDateOfExport,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set includePageNumber`() = runTest {
+        val includePageNumber = false
+        viewModel.handleIntent(ExportFormIntent.SetIncludePageNumber(includePageNumber))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = includePageNumber,
+                actual = awaitItem().layout.includePageNumber,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set includeNotes`() = runTest {
+        val includeNotes = false
+        viewModel.handleIntent(ExportFormIntent.SetIncludeNotes(includeNotes))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = includeNotes,
+                actual = awaitItem().content.includeNotes,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set includeTags`() = runTest {
+        val includeTags = false
+        viewModel.handleIntent(ExportFormIntent.SetIncludeTags(includeTags))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = includeTags,
+                actual = awaitItem().content.includeTags,
+            )
+        }
+    }
+
+    @Test
+    fun `update state when intending to set includeDaysWithoutEntries`() = runTest {
+        val includeDaysWithoutEntries = false
+        viewModel.handleIntent(ExportFormIntent.SetIncludeDaysWithoutEntries(includeDaysWithoutEntries))
+
+        viewModel.state.test {
+            assertEquals(
+                expected = includeDaysWithoutEntries,
+                actual = awaitItem().layout.includeDaysWithoutEntries,
             )
         }
     }

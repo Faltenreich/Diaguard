d6a53cb17ff8d5261abbbe4058b856e79e0f1c1a
Faltenreich <philipp.fahlteich@gmail.com>
2024-11-10 16:00:34 +0100

    Test deletion of entry in EntryFormViewModel

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/entry/form/EntryFormViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/entry/form/EntryFormViewModelTest.kt
@@ -9,9 +9,12 @@ import com.faltenreich.diaguard.entry.tag.EntryTagRepository
 import com.faltenreich.diaguard.measurement.value.MeasurementValueRepository
 import com.faltenreich.diaguard.navigation.Navigation
 import com.faltenreich.diaguard.navigation.NavigationEvent
+import com.faltenreich.diaguard.shared.database.DatabaseKey
+import com.faltenreich.diaguard.shared.view.DeleteModal
 import com.faltenreich.diaguard.tag.Tag
 import kotlinx.coroutines.test.runTest
 import org.koin.core.parameter.parametersOf
+import org.koin.test.get
 import org.koin.test.inject
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -21,12 +24,11 @@ import kotlin.test.assertTrue
 
 class EntryFormViewModelTest : TestSuite {
 
-    private val viewModel: EntryFormViewModel by inject(
-        parameters = { parametersOf(null, null, null) },
-    )
     private val navigation: Navigation by inject()
     private val entryRepository: EntryTagRepository by inject()
     private val valueRepository: MeasurementValueRepository by inject()
+
+    private lateinit var viewModel: EntryFormViewModel
 
     @BeforeTest
     override fun beforeTest() {
@@ -36,6 +38,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `launch with no and then some categories`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         viewModel.state.test {
             assertTrue(awaitItem().measurements.isEmpty())
             assertTrue(awaitItem().measurements.isNotEmpty())
@@ -44,6 +48,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `launch with tags`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         viewModel.state.test {
             assertTrue(awaitItem().tags.isNotEmpty())
         }
@@ -51,6 +57,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `open dialog when selecting date`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         navigation.events.test {
             viewModel.handleIntent(EntryFormIntent.SelectDate)
 
@@ -62,6 +70,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `open dialog when selecting time`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         navigation.events.test {
             viewModel.handleIntent(EntryFormIntent.SelectTime)
 
@@ -73,6 +83,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `update tag selection when adding tag`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         val tag = Tag.User(name = "tag")
         viewModel.handleIntent(EntryFormIntent.AddTag(tag))
 
@@ -83,6 +95,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `update tag selection when removing tag`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         val tag = Tag.User(name = "tag")
         viewModel.handleIntent(EntryFormIntent.AddTag(tag))
         viewModel.handleIntent(EntryFormIntent.RemoveTag(tag))
@@ -94,6 +108,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `update measurements when editing input`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         viewModel.state.test {
             awaitItem()
 
@@ -112,6 +128,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `store entry and pop screen when submitting with success`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         turbineScope {
             val state = viewModel.state.testIn(backgroundScope)
             val navigation = navigation.events.testIn(backgroundScope)
@@ -139,6 +157,8 @@ class EntryFormViewModelTest : TestSuite {
 
     @Test
     fun `show error in snackbar when missing input`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
         navigation.events.test {
             viewModel.handleIntent(EntryFormIntent.Submit)
 
@@ -148,6 +168,31 @@ class EntryFormViewModelTest : TestSuite {
                 expected = "entry_form_error_missing_input",
                 actual = event.message,
             )
+        }
+    }
+
+    @Test
+    fun `show modal if deleting entry`() = runTest {
+        storeValue(10.0, DatabaseKey.MeasurementProperty.BLOOD_SUGAR)
+        val entryId = entryRepository.getLastId()!!
+        viewModel = get(parameters = { parametersOf(entryId, null, null) })
+
+        navigation.events.test {
+            viewModel.handleIntent(EntryFormIntent.Delete)
+
+            val event = awaitItem()
+            assertTrue(event is NavigationEvent.OpenModal)
+            assertTrue(event.modal is DeleteModal)
+        }
+    }
+
+    @Test
+    fun `pop screen if deleting non-existing entry`() = runTest {
+        viewModel = get(parameters = { parametersOf(null, null, null) })
+
+        navigation.events.test {
+            viewModel.handleIntent(EntryFormIntent.Delete)
+            assertTrue(awaitItem() is NavigationEvent.PopScreen)
         }
     }
 }

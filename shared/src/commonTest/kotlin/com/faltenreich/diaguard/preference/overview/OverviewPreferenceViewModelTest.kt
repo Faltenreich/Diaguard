521f77a713c434ce2bc4bd96777fb627f020c069
Faltenreich <philipp.fahlteich@gmail.com>
2024-11-10 21:25:20 +0100

    Fix OverviewPreferenceViewModelTest

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/preference/overview/OverviewPreferenceViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/preference/overview/OverviewPreferenceViewModelTest.kt
@@ -37,7 +37,6 @@ class OverviewPreferenceViewModelTest : TestSuite {
                 ),
                 actual = awaitItem().map(PreferenceListItem::title),
             )
-            awaitComplete()
         }
     }
 }

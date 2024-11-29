012ca5af15b96968e674d550921c997d4b06a937
Faltenreich <philipp.fahlteich@gmail.com>
2024-11-29 16:14:01 +0100

    Open system settings for reminder preferences on older API levels

--- shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/system/AndroidSystemSettings.kt
+++ shared/src/androidMain/kotlin/com/faltenreich/diaguard/shared/system/AndroidSystemSettings.kt
@@ -2,14 +2,21 @@ package com.faltenreich.diaguard.shared.system
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.provider.Settings
 
 class AndroidSystemSettings(private val context: Context) : SystemSettings {
 
     override fun openNotificationSettings() {
-        val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            .putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+        val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+        } else {
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                .setData(Uri.parse("package:" + context.packageName))
+        }
         context.startActivity(intent)
     }
 }

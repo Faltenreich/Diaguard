7c7782a1e3bbf8e6cfc94b50b1f0d9c7db60e3a1
Faltenreich <philipp.fahlteich@gmail.com>
2024-11-29 18:07:49 +0100

    Prepare data for HbA1cInfo

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/hba1c/info/HbA1cInfo.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/hba1c/info/HbA1cInfo.kt
@@ -1,14 +1,62 @@
 package com.faltenreich.diaguard.dashboard.hba1c.info
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
+import com.faltenreich.diaguard.shared.localization.getString
+import diaguard.shared.generated.resources.Res
+import diaguard.shared.generated.resources.estimated
+import diaguard.shared.generated.resources.hba1c
+import diaguard.shared.generated.resources.hba1c_estimated_formula
+import diaguard.shared.generated.resources.latest
+import diaguard.shared.generated.resources.placeholder
 
 @Composable
 fun HbA1cInfo(
     viewModel: HbA1cInfoViewModel,
     modifier: Modifier = Modifier,
 ) {
-    val state = viewModel.state
-    Text(modifier = modifier, text = "HbA1c Info")
+    val state = viewModel.collectState()
+
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = getString(Res.string.hba1c),
+            style = AppTheme.typography.titleLarge,
+        )
+
+        Spacer(modifier = Modifier.height(AppTheme.dimensions.padding.P_3))
+
+        Text(
+            text = getString(Res.string.latest),
+            style = AppTheme.typography.titleMedium,
+        )
+
+        Text(
+            text = state?.latest?.value ?: getString(Res.string.placeholder),
+            style = AppTheme.typography.bodyLarge,
+        )
+
+        Spacer(modifier = Modifier.height(AppTheme.dimensions.padding.P_3))
+
+        Text(
+            text = getString(Res.string.estimated),
+            style = AppTheme.typography.titleMedium,
+        )
+
+        Text(
+            text = state?.estimated?.value ?: getString(Res.string.placeholder),
+            style = AppTheme.typography.bodyLarge,
+        )
+
+        Text(getString(Res.string.hba1c_estimated_formula))
+    }
 }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/hba1c/info/HbA1cInfoState.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/hba1c/info/HbA1cInfoState.kt
@@ -1,8 +1,22 @@
 package com.faltenreich.diaguard.dashboard.hba1c.info
 
-import com.faltenreich.diaguard.measurement.value.MeasurementValue
+import com.faltenreich.diaguard.measurement.value.tint.MeasurementValueTint
 
 data class HbA1cInfoState(
-    val latest: MeasurementValue.Local?,
-    val average: MeasurementValue.Average?,
-)
+    val latest: Latest?,
+    val estimated: Estimated?,
+) {
+
+    data class Latest(
+        val value: String,
+        val dateTime: String,
+        val tint: MeasurementValueTint,
+    )
+
+    data class Estimated(
+        val value: String,
+        val dateRange: String,
+        val valueCount: Int,
+        val tint: MeasurementValueTint,
+    )
+}

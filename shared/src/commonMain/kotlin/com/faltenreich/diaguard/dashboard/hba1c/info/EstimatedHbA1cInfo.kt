c31cede42ef1be042149d31b22d557620d208de6
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-08 21:50:24 +0100

    Improve layout of EstimatedHbA1cInfo

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/hba1c/info/EstimatedHbA1cInfo.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/dashboard/hba1c/info/EstimatedHbA1cInfo.kt
@@ -1,20 +1,22 @@
 package com.faltenreich.diaguard.dashboard.hba1c.info
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.shared.localization.getString
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.hba1c_estimated
 import diaguard.shared.generated.resources.hba1c_estimated_description
+import diaguard.shared.generated.resources.hba1c_estimated_disclaimer
+import diaguard.shared.generated.resources.hba1c_estimated_formula
 
 @Composable
 fun EstimatedHbA1cInfo(modifier: Modifier = Modifier) {
@@ -22,6 +24,7 @@ fun EstimatedHbA1cInfo(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxWidth()
             .padding(AppTheme.dimensions.padding.P_3),
+        verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_3_5),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
@@ -29,10 +32,22 @@ fun EstimatedHbA1cInfo(modifier: Modifier = Modifier) {
             style = AppTheme.typography.titleLarge,
         )
 
-        Spacer(modifier = Modifier.height(AppTheme.dimensions.padding.P_4))
-
         Text(
             text = getString(Res.string.hba1c_estimated_description),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Justify,
+            style = AppTheme.typography.bodyLarge,
+        )
+
+        Text(
+            text = getString(Res.string.hba1c_estimated_formula),
+            fontWeight = FontWeight.Bold,
+            style = AppTheme.typography.bodyLarge,
+        )
+
+        Text(
+            text = getString(Res.string.hba1c_estimated_disclaimer),
+            modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Justify,
             style = AppTheme.typography.bodyLarge,
         )

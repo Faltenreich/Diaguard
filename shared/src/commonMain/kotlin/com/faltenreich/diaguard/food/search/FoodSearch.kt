bcb28b95d1467157514596ae10b58d46b7cee110
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-08 18:15:41 +0100

    Simplify FoodListEmpty

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/FoodSearch.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/FoodSearch.kt
@@ -56,7 +56,7 @@ fun FoodSearch(
         if (items.loadState.refresh == LoadState.Loading) {
             FoodListSkeleton()
         } else if (isAtTheEnd && items.itemCount == 0) {
-            FoodListEmpty(onIntent = viewModel::dispatchIntent)
+            FoodListEmpty()
         } else {
             FoodList(
                 items = items,
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/list/FoodListEmpty.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/list/FoodListEmpty.kt
@@ -3,27 +3,24 @@ package com.faltenreich.diaguard.food.search.list
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import com.faltenreich.diaguard.AppTheme
-import com.faltenreich.diaguard.food.search.FoodSearchIntent
 import com.faltenreich.diaguard.shared.localization.getString
 import diaguard.shared.generated.resources.Res
-import diaguard.shared.generated.resources.food_create
 import diaguard.shared.generated.resources.food_search_empty_description
 import diaguard.shared.generated.resources.food_search_empty_title
 
 @Composable
-fun FoodListEmpty(
-    modifier: Modifier = Modifier,
-    onIntent: (FoodSearchIntent) -> Unit,
-) {
+fun FoodListEmpty(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(AppTheme.dimensions.padding.P_3),
         verticalArrangement = Arrangement.spacedBy(
             space = AppTheme.dimensions.padding.P_3,
             alignment = Alignment.CenterVertically,
@@ -35,11 +32,6 @@ fun FoodListEmpty(
             fontWeight = FontWeight.Bold,
             style = AppTheme.typography.titleLarge,
         )
-
         Text(getString(Res.string.food_search_empty_description))
-
-        TextButton(onClick = { onIntent(FoodSearchIntent.Create) }) {
-            Text(getString(Res.string.food_create))
-        }
     }
 }

bdd484791e7fc04932500cc6dee6fda1a4897b7d
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-09 17:11:54 +0100

    Improve FoodListEmpty

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/list/FoodListEmpty.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/list/FoodListEmpty.kt
@@ -1,9 +1,11 @@
 package com.faltenreich.diaguard.food.search.list
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,23 +16,29 @@ import com.faltenreich.diaguard.shared.localization.getString
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.food_search_empty_description
 import diaguard.shared.generated.resources.food_search_empty_title
+import diaguard.shared.generated.resources.ic_sad
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun FoodListEmpty(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(AppTheme.dimensions.padding.P_3),
+            .padding(AppTheme.dimensions.padding.P_4),
         verticalArrangement = Arrangement.spacedBy(
             space = AppTheme.dimensions.padding.P_3,
             alignment = Alignment.CenterVertically,
         ),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Image(
+            painter = painterResource(Res.drawable.ic_sad),
+            contentDescription = null,
+            modifier = Modifier.size(AppTheme.dimensions.size.ImageXLarge),
+        )
         Text(
             text = getString(Res.string.food_search_empty_title),
             fontWeight = FontWeight.Bold,
-            style = AppTheme.typography.titleLarge,
         )
         Text(getString(Res.string.food_search_empty_description))
     }

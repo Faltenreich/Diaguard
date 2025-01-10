96f5e1f25ea070fb654acb27b82c72855d136713
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-10 16:58:51 +0100

    Add fixme

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/measurement/MeasurementPropertyInputState.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/form/measurement/MeasurementPropertyInputState.kt
@@ -4,6 +4,7 @@ import com.faltenreich.diaguard.measurement.property.MeasurementProperty
 
 data class MeasurementPropertyInputState(
     val property: MeasurementProperty.Local,
+    // FIXME: Sometimes updated seemingly asynchronously, e.g. when entering multiple numbers into EntryForm
     val input: String,
     val isLast: Boolean,
     val error: String?,

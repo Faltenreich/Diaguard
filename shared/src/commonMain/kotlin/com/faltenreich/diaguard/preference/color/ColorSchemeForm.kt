3d886e22b83d6c1b358854442ae27b14e1666903
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-11 12:21:49 +0100

    Simplify ColorSchemeForm

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/color/ColorSchemeForm.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/color/ColorSchemeForm.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,26 +38,16 @@ fun ColorSchemeForm(
                 selection = state.selection,
                 onClick = { viewModel.dispatchIntent(ColorSchemeFormIntent.Select(it)) },
             )
-
-            AppTheme(isDarkColorScheme = false) {
-                Surface(modifier = Modifier.fillMaxWidth()) {
-                    ColorSchemeListItem(
-                        colorScheme = ColorScheme.LIGHT,
-                        selection = state.selection,
-                        onClick = { viewModel.dispatchIntent(ColorSchemeFormIntent.Select(it)) },
-                    )
-                }
-            }
-
-            AppTheme(isDarkColorScheme = true) {
-                Surface(modifier = Modifier.fillMaxWidth()) {
-                    ColorSchemeListItem(
-                        colorScheme = ColorScheme.DARK,
-                        selection = state.selection,
-                        onClick = { viewModel.dispatchIntent(ColorSchemeFormIntent.Select(it)) },
-                    )
-                }
-            }
+            ColorSchemeListItem(
+                colorScheme = ColorScheme.LIGHT,
+                selection = state.selection,
+                onClick = { viewModel.dispatchIntent(ColorSchemeFormIntent.Select(it)) },
+            )
+            ColorSchemeListItem(
+                colorScheme = ColorScheme.DARK,
+                selection = state.selection,
+                onClick = { viewModel.dispatchIntent(ColorSchemeFormIntent.Select(it)) },
+            )
         }
 
         Spacer(modifier = Modifier.height(AppTheme.dimensions.padding.P_3))

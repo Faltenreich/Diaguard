9862d7f7a06848005f54a4b1c485a3282a0b4ded
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-11 23:14:42 +0100

    Animate layout changes of TopAppBar

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/navigation/bar/top/TopAppBar.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/navigation/bar/top/TopAppBar.kt
@@ -1,27 +1,39 @@
 package com.faltenreich.diaguard.navigation.bar.top
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.faltenreich.diaguard.AppTheme
 
 @Composable
 fun TopAppBar(
     style: TopAppBarStyle,
     navigationIcon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    when (style) {
-        is TopAppBarStyle.Hidden -> Unit
-        is TopAppBarStyle.CenterAligned -> CenterAlignedTopAppBar(
-            title = { style.content() },
-            navigationIcon = navigationIcon,
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = AppTheme.colors.scheme.primary,
-                navigationIconContentColor = AppTheme.colors.scheme.onPrimary,
-                titleContentColor = AppTheme.colors.scheme.onPrimary,
-                actionIconContentColor = AppTheme.colors.scheme.onPrimary,
-            ),
-        )
-        is TopAppBarStyle.Custom -> style.content()
+    Box(
+        modifier = modifier
+            .animateContentSize()
+            // Limits animation to y-axis
+            .fillMaxWidth(),
+    ) {
+        when (style) {
+            is TopAppBarStyle.Hidden -> Unit
+            is TopAppBarStyle.CenterAligned -> CenterAlignedTopAppBar(
+                title = { style.content() },
+                navigationIcon = navigationIcon,
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = AppTheme.colors.scheme.primary,
+                    navigationIconContentColor = AppTheme.colors.scheme.onPrimary,
+                    titleContentColor = AppTheme.colors.scheme.onPrimary,
+                    actionIconContentColor = AppTheme.colors.scheme.onPrimary,
+                ),
+            )
+            is TopAppBarStyle.Custom -> style.content()
+        }
     }
 }

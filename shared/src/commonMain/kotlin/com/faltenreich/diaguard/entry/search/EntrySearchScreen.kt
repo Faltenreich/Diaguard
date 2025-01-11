28b01fcd6a3f2ecdb403d4fe52e78c59ee969dd3
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-11 19:09:49 +0100

    Add TopAppBar to EntrySearchScreen

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearchScreen.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearchScreen.kt
@@ -1,14 +1,26 @@
 package com.faltenreich.diaguard.entry.search
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.faltenreich.diaguard.navigation.bar.bottom.BottomAppBarStyle
+import com.faltenreich.diaguard.navigation.bar.top.TopAppBarStyle
 import com.faltenreich.diaguard.navigation.screen.Screen
 import com.faltenreich.diaguard.shared.di.viewModel
+import com.faltenreich.diaguard.shared.localization.getString
+import diaguard.shared.generated.resources.Res
+import diaguard.shared.generated.resources.entry_search
 import kotlinx.serialization.Serializable
 import org.koin.core.parameter.parametersOf
 
 @Serializable
 data class EntrySearchScreen(private val query: String = "") : Screen {
+
+    @Composable
+    override fun TopAppBar(): TopAppBarStyle {
+        return TopAppBarStyle.CenterAligned {
+            Text(getString(Res.string.entry_search))
+        }
+    }
 
     @Composable
     override fun BottomAppBar(): BottomAppBarStyle {

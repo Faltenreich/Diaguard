e3f599b24c9d5b12b7b8ebd0dd2d698ab4774e47
Faltenreich <philipp.fahlteich@gmail.com>
2025-01-11 12:15:50 +0100

    Improve EntrySearchField

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearchField.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearchField.kt
@@ -1,40 +1,40 @@
-package com.faltenreich.diaguard.food.search
+package com.faltenreich.diaguard.entry.search
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import com.faltenreich.diaguard.shared.localization.getString
 import com.faltenreich.diaguard.shared.view.ClearButton
 import com.faltenreich.diaguard.shared.view.SearchField
+import com.faltenreich.diaguard.shared.view.rememberFocusRequester
 import diaguard.shared.generated.resources.Res
-import diaguard.shared.generated.resources.food_search_prompt
-import diaguard.shared.generated.resources.ic_arrow_back
+import diaguard.shared.generated.resources.entry_search_prompt
+import diaguard.shared.generated.resources.ic_search
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun FoodSearchField(
+fun EntrySearchField(
     query: String,
     onQueryChange: (String) -> Unit,
-    popScreen: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val focusRequester = rememberFocusRequester(requestFocus = true)
     val focusManager = LocalFocusManager.current
 
     SearchField(
         query = query,
-        placeholder = getString(Res.string.food_search_prompt),
+        placeholder = getString(Res.string.entry_search_prompt),
         leadingIcon = {
-            IconButton(onClick = popScreen) {
-                Icon(
-                    painter = painterResource(Res.drawable.ic_arrow_back),
-                    contentDescription = null,
-                )
-            }
+            Icon(
+                painter = painterResource(Res.drawable.ic_search),
+                contentDescription = null,
+            )
         },
         trailingIcon = {
             AnimatedVisibility(
@@ -50,7 +50,9 @@ fun FoodSearchField(
                 )
             }
         },
-        onQueryChange = onQueryChange,
-        modifier = modifier,
+        onQueryChange = { query -> onQueryChange(query) },
+        modifier = modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester),
     )
 }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearchScreen.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/search/EntrySearchScreen.kt
@@ -1,24 +1,10 @@
 package com.faltenreich.diaguard.entry.search
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusRequester
-import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.navigation.bar.bottom.BottomAppBarStyle
 import com.faltenreich.diaguard.navigation.screen.Screen
 import com.faltenreich.diaguard.shared.di.viewModel
-import com.faltenreich.diaguard.shared.localization.getString
-import com.faltenreich.diaguard.shared.view.ClearButton
-import com.faltenreich.diaguard.shared.view.SearchField
-import com.faltenreich.diaguard.shared.view.rememberFocusRequester
-import diaguard.shared.generated.resources.Res
-import diaguard.shared.generated.resources.entry_search_prompt
-import diaguard.shared.generated.resources.ic_search
 import kotlinx.serialization.Serializable
-import org.jetbrains.compose.resources.painterResource
 import org.koin.core.parameter.parametersOf
 
 @Serializable
@@ -29,23 +15,9 @@ data class EntrySearchScreen(private val query: String = "") : Screen {
         val viewModel = viewModel<EntrySearchViewModel>(parameters = { parametersOf(query) })
         return BottomAppBarStyle.Visible(
             actions = {
-                val focusRequester = rememberFocusRequester(requestFocus = true)
-
-                SearchField(
+                EntrySearchField(
                     query = viewModel.query,
-                    placeholder = getString(Res.string.entry_search_prompt),
-                    leadingIcon = {
-                        Icon(
-                            painter = painterResource(Res.drawable.ic_search),
-                            contentDescription = null,
-                        )
-                    },
-                    trailingIcon = { ClearButton(onClick = { viewModel.query = "" }) },
-                    onQueryChange = { query -> viewModel.query = query },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(end = AppTheme.dimensions.padding.P_3)
-                        .focusRequester(focusRequester),
+                    onQueryChange = { viewModel.query = it },
                 )
             }
         )

6d432e49805959637ca6817bc0f19f49a09ae751
Faltenreich <philipp.fahlteich@gmail.com>
2025-02-21 17:38:12 +0100

    Redesign TextCheckbox

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/form/MeasurementCategoryForm.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/form/MeasurementCategoryForm.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,7 +12,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.measurement.category.icon.MeasurementCategoryIcon
@@ -21,16 +19,14 @@ import com.faltenreich.diaguard.measurement.property.list.MeasurementPropertyLis
 import com.faltenreich.diaguard.shared.architecture.collectAsStateWithLifecycle
 import com.faltenreich.diaguard.shared.localization.getString
 import com.faltenreich.diaguard.shared.view.Divider
-import com.faltenreich.diaguard.shared.view.NoticeBar
-import com.faltenreich.diaguard.shared.view.NoticeBarStyle
+import com.faltenreich.diaguard.shared.view.FormRow
 import com.faltenreich.diaguard.shared.view.TextCheckbox
-import com.faltenreich.diaguard.shared.view.TextDivider
 import com.faltenreich.diaguard.shared.view.TextInput
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.icon
-import diaguard.shared.generated.resources.measurement_category_active
-import diaguard.shared.generated.resources.measurement_category_active_info
 import diaguard.shared.generated.resources.measurement_category_visibility
+import diaguard.shared.generated.resources.measurement_category_visibility_hidden
+import diaguard.shared.generated.resources.measurement_category_visibility_visible
 import diaguard.shared.generated.resources.name
 import org.jetbrains.compose.resources.stringResource
 
@@ -44,68 +40,66 @@ fun MeasurementCategoryForm(
     val icon = viewModel.icon.collectAsStateWithLifecycle().value
     val isActive = viewModel.isActive.collectAsStateWithLifecycle().value
 
-    Column(modifier = modifier.fillMaxSize()) {
-        Column(
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+    ) {
+        TextInput(
+            input = name,
+            onInputChange = { viewModel.name.value = it },
+            label = getString(Res.string.name),
             modifier = Modifier
-                .weight(1f)
-                .verticalScroll(rememberScrollState()),
+                .fillMaxWidth()
+                .padding(
+                    start = AppTheme.dimensions.padding.P_0,
+                    top = AppTheme.dimensions.padding.P_3,
+                    end = AppTheme.dimensions.padding.P_0,
+                    bottom = AppTheme.dimensions.padding.P_0,
+                ),
+        )
+
+        Divider()
+
+        FormRow(
+            modifier = Modifier.clickable {
+                viewModel.dispatchIntent(MeasurementCategoryFormIntent.OpenIconPicker)
+            },
         ) {
-            TextInput(
-                input = name,
-                onInputChange = { viewModel.name.value = it },
-                label = getString(Res.string.name),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        horizontal = AppTheme.dimensions.padding.P_1,
-                        vertical = AppTheme.dimensions.padding.P_3,
-                    ),
+            Text(
+                text = stringResource(Res.string.icon),
+                modifier = Modifier.weight(1f),
             )
+            MeasurementCategoryIcon(
+                icon = icon,
+                fallback = name,
+                modifier = Modifier.minimumInteractiveComponentSize(),
+            )
+        }
 
-            Divider()
+        Divider()
 
-            Row(
-                modifier = Modifier
-                    .clickable { viewModel.dispatchIntent(MeasurementCategoryFormIntent.OpenIconPicker) }
-                    .fillMaxWidth()
-                    .padding(all = AppTheme.dimensions.padding.P_3),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = stringResource(Res.string.icon),
-                    modifier = Modifier.weight(1f),
-                )
-                MeasurementCategoryIcon(
-                    icon = icon,
-                    fallback = name,
-                    modifier = Modifier.minimumInteractiveComponentSize(),
-                )
-            }
-
-            TextDivider(getString(Res.string.measurement_category_visibility))
-
+        FormRow {
             TextCheckbox(
-                title = stringResource(Res.string.measurement_category_active),
+                title = stringResource(Res.string.measurement_category_visibility),
+                subtitle = stringResource(
+                    if (isActive) Res.string.measurement_category_visibility_visible
+                    else Res.string.measurement_category_visibility_hidden
+                ),
                 checked = isActive,
                 onCheckedChange = { viewModel.isActive.value = it },
             )
-
-            AnimatedVisibility(
-                visible = state != null,
-                enter = fadeIn(),
-            ) {
-                val properties = state?.properties ?: emptyList()
-                MeasurementPropertyList(
-                    category = viewModel.category,
-                    properties = properties,
-                )
-            }
         }
 
-        NoticeBar(
-            text = stringResource(Res.string.measurement_category_active_info),
-            isVisible = !isActive,
-            style = NoticeBarStyle.WARNING,
-        )
+        AnimatedVisibility(
+            visible = state != null,
+            enter = fadeIn(),
+        ) {
+            val properties = state?.properties ?: emptyList()
+            MeasurementPropertyList(
+                category = viewModel.category,
+                properties = properties,
+            )
+        }
     }
 }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/TextCheckbox.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/view/TextCheckbox.kt
@@ -1,13 +1,7 @@
 package com.faltenreich.diaguard.shared.view
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,33 +17,22 @@ fun TextCheckbox(
     onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Surface(
-        onClick = { onCheckedChange(!checked) },
-        shape = AppTheme.shapes.medium,
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
-            modifier = modifier
-                .defaultMinSize(minHeight = ButtonDefaults.MinHeight)
-                .fillMaxWidth()
-                .padding(all = AppTheme.dimensions.padding.P_3),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Column(
-                modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.spacedBy(AppTheme.dimensions.padding.P_2),
-            ) {
-                Text(title)
-                subtitle?.let {
-                    Text(
-                        text = subtitle,
-                        style = AppTheme.typography.bodySmall,
-                    )
-                }
+        Column(modifier = Modifier.weight(1f)) {
+            Text(title)
+            subtitle?.let {
+                Text(
+                    text = subtitle,
+                    style = AppTheme.typography.bodySmall,
+                )
             }
-            Switch(
-                checked = checked,
-                onCheckedChange = onCheckedChange,
-            )
         }
+        Switch(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+        )
     }
 }

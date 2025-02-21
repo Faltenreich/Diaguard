ddfddf584baa835150a87f2d20a6e927ad6651d3
Faltenreich <philipp.fahlteich@gmail.com>
2025-02-21 13:58:29 +0100

    Show dividers between MeasurementCategoryListItem

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/list/MeasurementCategoryList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/list/MeasurementCategoryList.kt
@@ -3,10 +3,12 @@ package com.faltenreich.diaguard.measurement.category.list
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.shared.view.Divider
 
 @Composable
 fun MeasurementCategoryList(
@@ -24,15 +26,20 @@ fun MeasurementCategoryList(
 
         LazyColumn {
             itemsIndexed(categories, key = { _, category -> category.id }) { index, category ->
-                MeasurementCategoryListItem(
-                    category = category,
-                    onIntent = { intent -> viewModel.dispatchIntent(intent) },
-                    showArrowUp = index > 1,
-                    showArrowDown = index > 0 && index < categories.size - 1,
-                    modifier = Modifier
-                        .animateItem()
-                        .clickable { viewModel.dispatchIntent(MeasurementCategoryListIntent.Edit(category)) },
-                )
+                Column {
+                    if (index != 0) {
+                        Divider()
+                    }
+                    MeasurementCategoryListItem(
+                        category = category,
+                        onIntent = { intent -> viewModel.dispatchIntent(intent) },
+                        showArrowUp = index > 1,
+                        showArrowDown = index > 0 && index < categories.size - 1,
+                        modifier = Modifier
+                            .animateItem()
+                            .clickable { viewModel.dispatchIntent(MeasurementCategoryListIntent.Edit(category)) },
+                    )
+                }
             }
         }
     }
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/list/MeasurementCategoryListItem.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/measurement/category/list/MeasurementCategoryListItem.kt
@@ -1,6 +1,5 @@
 package com.faltenreich.diaguard.measurement.category.list
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
@@ -9,7 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextDecoration
 import com.faltenreich.diaguard.measurement.category.MeasurementCategory
 import com.faltenreich.diaguard.measurement.category.icon.MeasurementCategoryIcon
-import com.faltenreich.diaguard.shared.view.Divider
 import com.faltenreich.diaguard.shared.view.FormRow
 import com.faltenreich.diaguard.shared.view.ResourceIcon
 import diaguard.shared.generated.resources.Res
@@ -24,28 +22,30 @@ fun MeasurementCategoryListItem(
     showArrowDown: Boolean,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier) {
-        FormRow(icon = { MeasurementCategoryIcon(category) }) {
-            Text(
-                text = category.name,
-                modifier = Modifier.weight(1f),
-                style = LocalTextStyle.current.copy(
-                    textDecoration = if (category.isActive) TextDecoration.None else TextDecoration.LineThrough,
-                ),
-            )
-            IconButton(
-                onClick = { onIntent(MeasurementCategoryListIntent.DecrementSortIndex(category)) },
-                enabled = showArrowUp,
-            ) {
-                ResourceIcon(Res.drawable.ic_arrow_up)
-            }
-            IconButton(
-                onClick = { onIntent(MeasurementCategoryListIntent.IncrementSortIndex(category)) },
-                enabled = showArrowDown,
-            ) {
-                ResourceIcon(Res.drawable.ic_arrow_down)
-            }
+    FormRow(
+        modifier = modifier,
+        icon = { MeasurementCategoryIcon(category) },
+    ) {
+        Text(
+            text = category.name,
+            modifier = Modifier.weight(1f),
+            style = LocalTextStyle.current.copy(
+                textDecoration =
+                    if (category.isActive) TextDecoration.None
+                    else TextDecoration.LineThrough,
+            ),
+        )
+        IconButton(
+            onClick = { onIntent(MeasurementCategoryListIntent.DecrementSortIndex(category)) },
+            enabled = showArrowUp,
+        ) {
+            ResourceIcon(Res.drawable.ic_arrow_up)
         }
-        Divider()
+        IconButton(
+            onClick = { onIntent(MeasurementCategoryListIntent.IncrementSortIndex(category)) },
+            enabled = showArrowDown,
+        ) {
+            ResourceIcon(Res.drawable.ic_arrow_down)
+        }
     }
 }

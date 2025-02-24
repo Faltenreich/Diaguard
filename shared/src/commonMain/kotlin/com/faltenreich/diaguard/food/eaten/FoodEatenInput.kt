6edee6eb4096f06e5489663c71e973a50c10d2b8
Faltenreich <philipp.fahlteich@gmail.com>
2025-02-24 20:21:50 +0100

    Improve layout of FoodEatenInput

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/eaten/FoodEatenInput.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/eaten/FoodEatenInput.kt
@@ -1,19 +1,25 @@
 package com.faltenreich.diaguard.food.eaten
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.entry.form.EntryFormIntent
 import com.faltenreich.diaguard.shared.localization.getString
 import com.faltenreich.diaguard.shared.primitive.format
-import com.faltenreich.diaguard.shared.view.ClearButton
 import com.faltenreich.diaguard.shared.view.TextInput
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.carbohydrates_per_100g
+import diaguard.shared.generated.resources.food_delete
 import diaguard.shared.generated.resources.grams_abbreviation
+import diaguard.shared.generated.resources.ic_delete
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun FoodEatenInput(
@@ -21,16 +27,23 @@ fun FoodEatenInput(
     onIntent: (EntryFormIntent) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    // TODO: Improve design
     TextInput(
+        // TODO: Format via ViewModel
         input = data.amountInGrams?.toString() ?: "",
         onInputChange = { input ->
             onIntent(EntryFormIntent.EditFood(data.copy(amountInGrams = input.toDoubleOrNull())))
         },
         label = data.food.name,
-        modifier = modifier,
-        trailingIcon = { ClearButton { onIntent(EntryFormIntent.RemoveFood(data)) } },
-        suffix = { Text(getString(Res.string.grams_abbreviation)) },
+        modifier = modifier.padding(vertical = AppTheme.dimensions.padding.P_1),
+        placeholder = { Text(getString(Res.string.grams_abbreviation)) },
+        trailingIcon = {
+            IconButton(onClick = { onIntent(EntryFormIntent.RemoveFood(data)) }) {
+                Icon(
+                    painter = painterResource(Res.drawable.ic_delete),
+                    contentDescription = getString(Res.string.food_delete),
+                )
+            }
+        },
         supportingText = {
             Text(
                 "%.2f %s".format(

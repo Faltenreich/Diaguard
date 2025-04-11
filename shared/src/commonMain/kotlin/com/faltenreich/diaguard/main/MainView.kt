f50bcb5a5eda9edca4bc768eed0fd30c0ec8bb1e
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-11 18:35:31 +0200

    Remove TopAppBar and BottomAppBar from MainView

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/main/MainView.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/main/MainView.kt
@@ -1,15 +1,6 @@
 package com.faltenreich.diaguard.main
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.ModalBottomSheetProperties
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
@@ -20,7 +11,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.backup.user.read.ReadBackupFormScreen
 import com.faltenreich.diaguard.backup.user.write.WriteBackupFormScreen
 import com.faltenreich.diaguard.dashboard.DashboardScreen
@@ -31,14 +21,11 @@ import com.faltenreich.diaguard.food.eaten.list.FoodEatenListScreen
 import com.faltenreich.diaguard.food.form.FoodFormScreen
 import com.faltenreich.diaguard.food.search.FoodSearchScreen
 import com.faltenreich.diaguard.log.LogScreen
-import com.faltenreich.diaguard.main.menu.MainMenuScreen
 import com.faltenreich.diaguard.measurement.category.form.MeasurementCategoryFormScreen
 import com.faltenreich.diaguard.measurement.category.list.MeasurementCategoryListScreen
 import com.faltenreich.diaguard.measurement.property.form.MeasurementPropertyFormScreen
 import com.faltenreich.diaguard.measurement.unit.list.MeasurementUnitListScreen
 import com.faltenreich.diaguard.navigation.NavigationEvent
-import com.faltenreich.diaguard.navigation.bar.bottom.BottomAppBar
-import com.faltenreich.diaguard.navigation.bar.top.TopAppBar
 import com.faltenreich.diaguard.navigation.modal.Modal
 import com.faltenreich.diaguard.navigation.navigate
 import com.faltenreich.diaguard.navigation.screen
@@ -52,15 +39,10 @@ import com.faltenreich.diaguard.preference.screen.StartScreenFormScreen
 import com.faltenreich.diaguard.shared.di.LocalSharedViewModelStoreOwner
 import com.faltenreich.diaguard.shared.di.inject
 import com.faltenreich.diaguard.shared.di.rememberViewModelStoreOwner
-import com.faltenreich.diaguard.shared.localization.getString
 import com.faltenreich.diaguard.statistic.StatisticScreen
 import com.faltenreich.diaguard.tag.detail.TagDetailScreen
 import com.faltenreich.diaguard.tag.list.TagListScreen
 import com.faltenreich.diaguard.timeline.TimelineScreen
-import diaguard.shared.generated.resources.Res
-import diaguard.shared.generated.resources.ic_arrow_back
-import diaguard.shared.generated.resources.navigate_back
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun MainView(
@@ -98,89 +80,38 @@ fun MainView(
     val startScreen = remember { state.startScreen }
 
     CompositionLocalProvider(LocalSharedViewModelStoreOwner provides viewModelStoreOwner) {
-        Scaffold(
+        NavHost(
+            navController = navController,
+            startDestination = startScreen,
             modifier = modifier,
-            topBar = {
-                TopAppBar(
-                    style = state.topAppBarStyle,
-                    navigationIcon = {
-                        if (navController.previousBackStackEntry != null) {
-                            IconButton(onClick = { viewModel.dispatchIntent(MainIntent.PopScreen) }) {
-                                Icon(
-                                    painter = painterResource(Res.drawable.ic_arrow_back),
-                                    contentDescription = getString(Res.string.navigate_back),
-                                )
-                            }
-                        }
-                    },
-                )
-            },
-            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
-            // Support edge-to-edge content, e.g. in Log
-            contentWindowInsets = WindowInsets(top = 0),
-            content = { padding ->
-                NavHost(
-                    navController = navController,
-                    startDestination = startScreen,
-                    modifier = Modifier.padding(padding),
-                ) {
-                    screen<DashboardScreen>()
-                    screen<TimelineScreen>()
-                    screen<LogScreen>()
+        ) {
+            screen<DashboardScreen>()
+            screen<TimelineScreen>()
+            screen<LogScreen>()
 
-                    screen<EntryFormScreen>()
-                    screen<EntrySearchScreen>()
-                    screen<FoodFormScreen>()
-                    screen<FoodSearchScreen>()
-                    screen<FoodEatenListScreen>()
-                    screen<FoodPreferenceListScreen>()
+            screen<EntryFormScreen>()
+            screen<EntrySearchScreen>()
+            screen<FoodFormScreen>()
+            screen<FoodSearchScreen>()
+            screen<FoodEatenListScreen>()
+            screen<FoodPreferenceListScreen>()
 
-                    screen<StatisticScreen>()
-                    screen<ExportFormScreen>()
+            screen<StatisticScreen>()
+            screen<ExportFormScreen>()
 
-                    screen<OverviewPreferenceScreen>()
-                    screen<ColorSchemeFormScreen>()
-                    screen<StartScreenFormScreen>()
-                    screen<DecimalPlacesFormScreen>()
-                    screen<WriteBackupFormScreen>()
-                    screen<ReadBackupFormScreen>()
-                    screen<MeasurementCategoryListScreen>()
-                    screen<MeasurementCategoryFormScreen>()
-                    screen<MeasurementPropertyFormScreen>()
-                    screen<MeasurementUnitListScreen>()
-                    screen<TagListScreen>()
-                    screen<TagDetailScreen>()
-                    screen<LicenseListScreen>()
-                }
-
-                bottomSheet?.let { bottomSheet ->
-                    ModalBottomSheet(
-                        onDismissRequest = { viewModel.dispatchIntent(MainIntent.CloseBottomSheet) },
-                        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
-                        containerColor = AppTheme.colors.scheme.background,
-                        contentColor = AppTheme.colors.scheme.onBackground,
-                        properties = ModalBottomSheetProperties(
-                            // Workaround: Force status bar content colors
-                            isAppearanceLightStatusBars = false,
-                            isAppearanceLightNavigationBars = false,
-                        ),
-                    ) {
-                        bottomSheet.Content()
-                    }
-                }
-
-                // FIXME: Leads to everlasting ViewModels, e.g. TagFormModal, MeasurementUnitFormModal
-                modal?.Content()
-            },
-            bottomBar = {
-                BottomAppBar(
-                    style = state.bottomAppBarStyle,
-                    onMenuClick = {
-                        val currentDestination = navController.currentDestination?.route
-                        viewModel.dispatchIntent(MainIntent.OpenBottomSheet(MainMenuScreen(currentDestination)))
-                    },
-                )
-            },
-        )
+            screen<OverviewPreferenceScreen>()
+            screen<ColorSchemeFormScreen>()
+            screen<StartScreenFormScreen>()
+            screen<DecimalPlacesFormScreen>()
+            screen<WriteBackupFormScreen>()
+            screen<ReadBackupFormScreen>()
+            screen<MeasurementCategoryListScreen>()
+            screen<MeasurementCategoryFormScreen>()
+            screen<MeasurementPropertyFormScreen>()
+            screen<MeasurementUnitListScreen>()
+            screen<TagListScreen>()
+            screen<TagDetailScreen>()
+            screen<LicenseListScreen>()
+        }
     }
 }

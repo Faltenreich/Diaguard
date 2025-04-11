0472eab0c7c6286169f0d2f010af88bd32bed1da
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-11 18:33:58 +0200

    Remove update of TopAppBar and BottomAppBar from NavigationExtensions

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/navigation/NavigationExtensions.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/navigation/NavigationExtensions.kt
@@ -13,10 +13,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import com.faltenreich.diaguard.navigation.bar.top.StatusBarStyle
 import com.faltenreich.diaguard.navigation.screen.Screen
-import com.faltenreich.diaguard.shared.di.inject
-import com.faltenreich.diaguard.shared.view.WindowController
 import kotlin.jvm.JvmSuppressWildcards
 import kotlin.reflect.KType
 
@@ -33,8 +30,6 @@ inline fun <reified T : Screen> NavGraphBuilder.screen(
     (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards ExitTransition?)? = exitTransition,
     noinline sizeTransform:
     (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards SizeTransform?)? = null,
-    navigation: Navigation = inject(),
-    windowController: WindowController = inject(),
 ) {
     composable<T>(
         typeMap,
@@ -47,11 +42,9 @@ inline fun <reified T : Screen> NavGraphBuilder.screen(
     ) { backStackEntry ->
         val screen = backStackEntry.toRoute<T>()
 
-        val topAppBarStyle = screen.TopAppBar()
-        windowController.setIsAppearanceLightStatusBars(topAppBarStyle.statusBarStyle == StatusBarStyle.Light)
-        navigation.setTopAppBarStyle(topAppBarStyle)
-
-        navigation.setBottomAppBarStyle(screen.BottomAppBar())
+        @Suppress("UNUSED_EXPRESSION")
+        exitTransition
+        // FIXME: Workaround for BackendException: Backend Internal error: Exception during IR lowering
 
         screen.Content()
     }

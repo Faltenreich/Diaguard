8b6e1226443e88ab13b1fdaeda4a105f6aabd57f
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-20 23:53:05 +0200

    Prepare DSL for DecimalPlacesForm

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/overview/OverviewPreferenceList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/overview/OverviewPreferenceList.kt
@@ -9,12 +9,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.faltenreich.diaguard.preference.color.ColorSchemeForm
+import com.faltenreich.diaguard.preference.decimal.DecimalPlacesForm
 import com.faltenreich.diaguard.preference.list.item.action
 import com.faltenreich.diaguard.preference.list.item.category
 import com.faltenreich.diaguard.preference.list.preferences
 import com.faltenreich.diaguard.shared.di.viewModel
 import diaguard.shared.generated.resources.Res
 import diaguard.shared.generated.resources.color_scheme
+import diaguard.shared.generated.resources.decimal_places
 import diaguard.shared.generated.resources.ic_user
 import diaguard.shared.generated.resources.therapy
 import org.jetbrains.compose.resources.stringResource
@@ -27,12 +29,18 @@ fun OverviewPreferenceList(
     val state = viewModel.collectState() ?: return
 
     var showColorSchemeForm by remember { mutableStateOf(false) }
+    var showDecimalPlacesForm by remember { mutableStateOf(false) }
 
     preferences(modifier = modifier.fillMaxSize()) {
         action(
             title = { stringResource(Res.string.color_scheme) },
             subtitle = { stringResource(state.colorScheme.labelResource) },
             onClick = { showColorSchemeForm = true },
+        )
+        action(
+            title = { stringResource(Res.string.decimal_places) },
+            subtitle = { state.decimalPlaces.toString() },
+            onClick = { showDecimalPlacesForm = true },
         )
         category(
             title = { stringResource(Res.string.therapy) },
@@ -43,6 +51,12 @@ fun OverviewPreferenceList(
     if (showColorSchemeForm) {
         ModalBottomSheet(onDismissRequest = { showColorSchemeForm = false }) {
             ColorSchemeForm(viewModel = viewModel())
+        }
+    }
+
+    if (showDecimalPlacesForm) {
+        ModalBottomSheet(onDismissRequest = { showDecimalPlacesForm = false }) {
+            DecimalPlacesForm(viewModel = viewModel())
         }
     }
 }

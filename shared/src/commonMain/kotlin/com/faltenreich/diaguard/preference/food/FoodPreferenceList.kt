44c5aed92f1e0564d8939ea4e8174de937c902bc
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-20 22:21:24 +0200

    Revert "Remove PreferenceList"
    This reverts commit f9809470c9497bdce5ccb9fd6f1c93126c5f5b18.

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/food/FoodPreferenceList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/food/FoodPreferenceList.kt
@@ -1,11 +1,10 @@
 package com.faltenreich.diaguard.preference.food
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.AppTheme
+import com.faltenreich.diaguard.preference.list.PreferenceList
 import com.faltenreich.diaguard.shared.view.NoticeBar
 import com.faltenreich.diaguard.shared.view.NoticeBarStyle
 import diaguard.shared.generated.resources.Res
@@ -20,11 +19,10 @@ fun FoodPreferenceList(
     val state = viewModel.collectState() ?: return
 
     Column(modifier = modifier) {
-        LazyColumn(modifier = modifier.fillMaxSize()) {
-            items(state.preferences) { preference ->
-                preference.Content(Modifier)
-            }
-        }
+        PreferenceList(
+            items = state.preferences,
+            modifier = Modifier.weight(AppTheme.dimensions.weight.W_1),
+        )
         NoticeBar(
             text = stringResource(Res.string.food_preferences_none_selected),
             isVisible = !state.showAnyFood,
--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/overview/OverviewPreferenceList.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/overview/OverviewPreferenceList.kt
@@ -1,10 +1,8 @@
 package com.faltenreich.diaguard.preference.overview
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.faltenreich.diaguard.preference.list.PreferenceList
 
 @Composable
 fun OverviewPreferenceList(
@@ -12,9 +10,8 @@ fun OverviewPreferenceList(
     modifier: Modifier = Modifier,
 ) {
     val state = viewModel.collectState()
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(state?.items ?: emptyList()) { preference ->
-            preference.Content(Modifier)
-        }
-    }
+    PreferenceList(
+        items = state?.items ?: emptyList(),
+        modifier = modifier,
+    )
 }

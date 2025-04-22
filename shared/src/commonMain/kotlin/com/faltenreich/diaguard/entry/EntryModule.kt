a8472b6e34cb85380bea07fb06257976afe558dc
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-22 22:25:06 +0200

    Refactor dependency injection

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/EntryModule.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/entry/EntryModule.kt
@@ -18,37 +18,37 @@ import com.faltenreich.diaguard.entry.search.EntrySearchViewModel
 import com.faltenreich.diaguard.entry.search.SearchEntriesUseCase
 import com.faltenreich.diaguard.entry.tag.EntryTagRepository
 import com.faltenreich.diaguard.entry.tag.StoreEntryTagsUseCase
-import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 fun entryModule() = module {
-    singleOf(::EntryRepository)
-    singleOf(::EntryTagRepository)
+    factoryOf(::EntryRepository)
+    factoryOf(::EntryTagRepository)
 
-    singleOf(::GetEntryByIdUseCase)
-    singleOf(::GetFoodByIdUseCase)
-    singleOf(::GetDateTimeForEntryUseCase)
-    singleOf(::GetMeasurementCategoryInputStateUseCase)
-    singleOf(::GetFoodEatenInputStateUseCase)
-    singleOf(::GetTagsByQueryUseCase)
-    singleOf(::GetTagsOfEntry)
-    singleOf(::MapEntryListItemStateUseCase)
+    factoryOf(::GetEntryByIdUseCase)
+    factoryOf(::GetFoodByIdUseCase)
+    factoryOf(::GetDateTimeForEntryUseCase)
+    factoryOf(::GetMeasurementCategoryInputStateUseCase)
+    factoryOf(::GetFoodEatenInputStateUseCase)
+    factoryOf(::GetTagsByQueryUseCase)
+    factoryOf(::GetTagsOfEntry)
+    factoryOf(::MapEntryListItemStateUseCase)
 
-    single {
+    factory {
         ValidateEntryFormInputUseCase(
             dispatcher = get(),
             ruleForEntryFormInput = ValidEntryFormInputRule(),
             rulesForProperties = listOf(RealisticMeasurementValueRule()),
         )
     }
-    singleOf(::StoreEntryUseCase)
-    singleOf(::StoreEntryTagsUseCase)
-    singleOf(::DeleteEntryUseCase)
-    singleOf(::SearchEntriesUseCase)
+    factoryOf(::StoreEntryUseCase)
+    factoryOf(::StoreEntryTagsUseCase)
+    factoryOf(::DeleteEntryUseCase)
+    factoryOf(::SearchEntriesUseCase)
 
     viewModel { (entryId: Long?, dateTimeIsoString: String?, foodId: Long?) ->
         EntryFormViewModel(entryId, dateTimeIsoString, foodId)
     }
-    factory { (query: String) -> EntrySearchViewModel(query) }
+    viewModel { (query: String) -> EntrySearchViewModel(query) }
 }

0cfe31483221f647a9806c2106ce5bf1c9c0e61b
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-22 22:23:51 +0200

    Refactor dependency injection

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/datetime/DateTimeModule.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/datetime/DateTimeModule.kt
@@ -2,25 +2,20 @@
 
 package com.faltenreich.diaguard.datetime
 
-import androidx.compose.ui.input.key.Key.Companion.T
 import com.faltenreich.diaguard.datetime.factory.DateTimeFactory
 import com.faltenreich.diaguard.datetime.factory.GetTodayUseCase
 import com.faltenreich.diaguard.datetime.format.DateTimeFormatter
 import com.faltenreich.diaguard.datetime.format.FormatDateTimeUseCase
 import com.faltenreich.diaguard.datetime.kotlinx.KotlinxDateTimeFactory
-import org.koin.core.module.dsl.singleOf
-import org.koin.core.parameter.ParametersHolder
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun dateTimeModule() = module {
-    single<DateTimeFactory> { KotlinxDateTimeFactory() }
+    factoryOf(::KotlinxDateTimeFactory) bind DateTimeFactory::class
 
-    singleOf(::DateTimeFormatter)
+    factoryOf(::DateTimeFormatter)
 
-    singleOf(::FormatDateTimeUseCase)
-    singleOf(::GetTodayUseCase)
+    factoryOf(::FormatDateTimeUseCase)
+    factoryOf(::GetTodayUseCase)
 }
-
-inline operator fun <reified T> ParametersHolder.component6(): T = elementAt(5, T::class)
-inline operator fun <reified T> ParametersHolder.component7(): T = elementAt(6, T::class)
-inline operator fun <reified T> ParametersHolder.component8(): T = elementAt(7, T::class)

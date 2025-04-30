daf95ebc429ecc97b7315937e0da0d44632ffa49
Faltenreich <philipp.fahlteich@gmail.com>
2025-04-30 19:16:26 +0200

    Refactoring

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/datetime/kotlinx/KotlinxTime.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/datetime/kotlinx/KotlinxTime.kt
@@ -8,35 +8,28 @@ import com.faltenreich.diaguard.shared.serialization.ObjectInputStream
 import com.faltenreich.diaguard.shared.serialization.ObjectOutputStream
 import kotlinx.datetime.LocalTime
 
-class KotlinxTime(
-    hourOfDay: Int,
-    minuteOfHour: Int,
-    secondOfMinute: Int = 0,
-    millisOfSecond: Int = 0,
-    nanosOfMilli: Int = 0,
-) : Time {
+class KotlinxTime(private var delegate: LocalTime) : Time {
 
-    private var delegate: LocalTime = LocalTime(
-        hour = hourOfDay,
-        minute = minuteOfHour,
-        second = secondOfMinute,
-        nanosecond = millisOfSecond * DateTimeConstants.NANOS_PER_SECOND + nanosOfMilli,
+    override val hourOfDay: Int get() = delegate.hour
+    override val minuteOfHour: Int get() = delegate.minute
+    override val secondOfMinute: Int get() = delegate.second
+    override val millisOfSecond: Int get() = delegate.nanosecond / DateTimeConstants.NANOS_PER_SECOND
+    override val nanosOfMilli: Int get() = delegate.nanosecond.mod(DateTimeConstants.NANOS_PER_SECOND)
+
+    constructor(
+        hourOfDay: Int,
+        minuteOfHour: Int,
+        secondOfMinute: Int = 0,
+        millisOfSecond: Int = 0,
+        nanosOfMilli: Int = 0,
+    ) : this(
+        LocalTime(
+            hour = hourOfDay,
+            minute = minuteOfHour,
+            second = secondOfMinute,
+            nanosecond = millisOfSecond * DateTimeConstants.NANOS_PER_SECOND + nanosOfMilli,
+        )
     )
-
-    override val hourOfDay: Int
-        get() = delegate.hour
-
-    override val minuteOfHour: Int
-        get() = delegate.minute
-
-    override val secondOfMinute: Int
-        get() = delegate.second
-
-    override val millisOfSecond: Int
-        get() = delegate.nanosecond / DateTimeConstants.NANOS_PER_SECOND
-
-    override val nanosOfMilli: Int
-        get() = delegate.nanosecond.mod(DateTimeConstants.NANOS_PER_SECOND)
 
     override fun atDate(date: Date): DateTime {
         return KotlinxDateTime(
@@ -56,7 +49,7 @@ class KotlinxTime(
         minuteOfHour: Int,
         secondOfMinute: Int,
         millisOfSecond: Int,
-        nanosOfMilli: Int
+        nanosOfMilli: Int,
     ): Time {
         return KotlinxTime(hourOfDay, minuteOfHour, secondOfMinute, millisOfSecond, nanosOfMilli)
     }

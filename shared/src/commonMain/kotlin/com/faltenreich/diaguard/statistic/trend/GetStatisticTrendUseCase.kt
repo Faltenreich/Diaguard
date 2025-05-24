d9796a68813e3d57dfbd5fd25867d893a8185f5a
Faltenreich <philipp.fahlteich@gmail.com>
2025-05-24 14:10:40 +0200

    Refactoring

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/statistic/trend/GetStatisticTrendUseCase.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/statistic/trend/GetStatisticTrendUseCase.kt
@@ -2,8 +2,10 @@ package com.faltenreich.diaguard.statistic.trend
 
 import com.faltenreich.diaguard.datetime.Date
 import com.faltenreich.diaguard.datetime.DateProgression
+import com.faltenreich.diaguard.datetime.DateRange
 import com.faltenreich.diaguard.datetime.format.DateTimeFormatter
 import com.faltenreich.diaguard.measurement.category.MeasurementCategory
+import com.faltenreich.diaguard.measurement.property.MeasurementProperty
 import com.faltenreich.diaguard.measurement.property.MeasurementPropertyRepository
 import com.faltenreich.diaguard.measurement.property.range.MeasurementValueRange
 import com.faltenreich.diaguard.measurement.value.MeasurementValue
@@ -12,6 +14,7 @@ import com.faltenreich.diaguard.measurement.value.tint.GetMeasurementValueTintUs
 import com.faltenreich.diaguard.shared.database.DatabaseKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlin.math.max
@@ -25,47 +28,55 @@ class GetStatisticTrendUseCase(
 
     operator fun invoke(
         category: MeasurementCategory.Local,
-        dateRange: ClosedRange<Date>,
+        dateRange: DateRange,
     ): Flow<StatisticTrendState> {
         val propertyKey = DatabaseKey.MeasurementProperty.BLOOD_SUGAR
-
         return propertyRepository.observeByKey(propertyKey).flatMapLatest { property ->
-            combine(
-                DateProgression(dateRange.start, dateRange.endInclusive).map { date ->
-                    valueRepository.observeAverageByPropertyKey(
-                        propertyKey = propertyKey,
-                        minDateTime = date.atStartOfDay(),
-                        maxDateTime = date.atEndOfDay(),
-                    ).map { date to it }
-                }
-            ) { averagesByDate ->
-                averagesByDate.map { (date, average) ->
-                    StatisticTrendState.Day(
-                        date = dateTimeFormatter.formatDayOfWeek(date, abbreviated = true),
-                        average = if (average != null && property != null) {
-                            val value = MeasurementValue.Average(
-                                value = average,
-                                property = property,
-                            )
-                            StatisticTrendState.Value(
-                                value = average,
-                                tint = getValueTint(value),
-                            )
-                        } else {
-                            null
-                        },
+            if (property != null) {
+                combine(
+                    DateProgression(dateRange.start, dateRange.endInclusive).map { date ->
+                        observeAverage(property, date).map { date to it }
+                    }
+                ) { averagesByDate ->
+                    averagesByDate.map { (date, average) ->
+                        StatisticTrendState.Day(
+                            date = dateTimeFormatter.formatDayOfWeek(date, abbreviated = true),
+                            average = average?.let {
+                                val value = MeasurementValue.Average(
+                                    value = average,
+                                    property = property,
+                                )
+                                StatisticTrendState.Value(
+                                    value = average,
+                                    tint = getValueTint(value),
+                                )
+                            },
+                        )
+                    }
+                }.map { days ->
+                    val targetValue = property.range.target ?: MeasurementValueRange.BLOOD_SUGAR_TARGET_DEFAULT
+                    val maximumValue = days.mapNotNull { it.average?.value }.maxOrNull()
+                    val maximumValueDefault = targetValue * 2
+                    StatisticTrendState(
+                        days = days,
+                        targetValue = targetValue,
+                        maximumValue = max(maximumValue ?: maximumValueDefault, maximumValueDefault),
                     )
                 }
-            }.map { days ->
-                val targetValue = property?.range?.target ?: MeasurementValueRange.BLOOD_SUGAR_TARGET_DEFAULT
-                val maximumValue = days.mapNotNull { it.average?.value }.maxOrNull()
-                val maximumValueDefault = targetValue * 2
-                StatisticTrendState(
-                    days = days,
-                    targetValue = targetValue,
-                    maximumValue = max(maximumValue ?: maximumValueDefault, maximumValueDefault),
-                )
+            } else {
+                emptyFlow()
             }
         }
+    }
+
+    private fun observeAverage(
+        property: MeasurementProperty.Local,
+        date: Date,
+    ): Flow<Double?> {
+        return valueRepository.observeAverageByPropertyId(
+            propertyId = property.id,
+            minDateTime = date.atStartOfDay(),
+            maxDateTime = date.atEndOfDay(),
+        )
     }
 }

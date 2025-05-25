49d0ff97a0b04957c1ce7c41b39743bafd04c8e4
Faltenreich <philipp.fahlteich@gmail.com>
2025-05-25 22:33:02 +0200

    Design radio group for statistic property

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/statistic/property/StatisticProperty.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/statistic/property/StatisticProperty.kt
@@ -1,24 +1,17 @@
 package com.faltenreich.diaguard.statistic.property
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 import com.faltenreich.diaguard.AppTheme
-import com.faltenreich.diaguard.shared.view.DropdownTextMenu
 import com.faltenreich.diaguard.shared.view.FormRow
 import com.faltenreich.diaguard.statistic.StatisticIntent
-import diaguard.shared.generated.resources.Res
-import diaguard.shared.generated.resources.date_range_picker_open
-import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun StatisticProperty(
@@ -26,26 +19,25 @@ fun StatisticProperty(
     onIntent: (StatisticIntent) -> Unit,
     modifier: Modifier = Modifier,
 ) = with(state) {
-    var expandDropDown by remember { mutableStateOf(false) }
-
-    FormRow(
-        icon = { Box(modifier = Modifier.width(AppTheme.dimensions.size.ImageMedium)) },
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable(
-                onClickLabel = stringResource(Res.string.date_range_picker_open),
-                role = Role.Button,
-                onClick = { expandDropDown = true },
-            ),
-    ) {
-        Text(selection.name)
-
-        DropdownTextMenu(
-            expanded = expandDropDown,
-            onDismissRequest = { expandDropDown = false },
-            items = properties.map { property ->
-                property.name to { onIntent(StatisticIntent.SetProperty(property)) }
+    Column(modifier = modifier) {
+        state.properties.forEach { property ->
+            val isSelected = property == selection
+            FormRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = isSelected,
+                        role = Role.RadioButton,
+                        onClick = { onIntent(StatisticIntent.SetProperty(property)) },
+                    ),
+            ) {
+                RadioButton(
+                    selected = isSelected,
+                    onClick = null,
+                    modifier = Modifier.padding(horizontal = AppTheme.dimensions.padding.P_0_5),
+                )
+                Text(property.name)
             }
-        )
+        }
     }
 }

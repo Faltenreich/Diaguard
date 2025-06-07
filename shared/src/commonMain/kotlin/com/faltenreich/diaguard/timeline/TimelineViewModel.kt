0c9c96464d01c59c0eab1fc32a16eafd3afb4028
Faltenreich <philipp.fahlteich@gmail.com>
2025-06-07 21:07:28 +0200

    Refactoring TimelineViewModel

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/TimelineViewModel.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/TimelineViewModel.kt
@@ -18,6 +18,7 @@ import com.faltenreich.diaguard.timeline.date.TimelineDateState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 
@@ -27,13 +28,13 @@ class TimelineViewModel(
     getPreference: GetPreferenceUseCase,
     getCategories: GetActiveMeasurementCategoriesWithPropertiesUseCase,
     private val getValues: GetMeasurementValuesInDateRangeUseCase,
-    private val getChartData: GetTimelineChartDataUseCase,
-    private val getTableData: GetTimelineTableDataUseCase,
+    private val getChart: GetTimelineChartDataUseCase,
+    private val getTable: GetTimelineTableDataUseCase,
     private val pushScreen: PushScreenUseCase,
 ) : ViewModel<TimelineState, TimelineIntent, TimelineEvent>() {
 
-    private val initialDate = MutableStateFlow(getToday())
-    private val currentDate = MutableStateFlow(initialDate.value)
+    private val initialDate = getToday()
+    private val currentDate = MutableStateFlow(initialDate)
     private val dateRange = currentDate.map { date ->
         date.minus(2, DateUnit.DAY) .. date.plus(2, DateUnit.DAY)
     }
@@ -42,11 +43,11 @@ class TimelineViewModel(
     private val properties = categoriesWithProperties.map { it.values.flatten() }
     private val values = dateRange.flatMapLatest(getValues::invoke)
     private val decimalPlaces = getPreference(DecimalPlacesPreference)
-    private val chartData = combine(values, properties, getChartData::invoke)
-    private val tableData = combine(values, properties, categories, decimalPlaces, getTableData::invoke)
+    private val chart = combine(values, properties, getChart::invoke)
+    private val table = combine(values, properties, categories, decimalPlaces, getTable::invoke)
     private val dateDialog = MutableStateFlow<TimelineState.DateDialog?>(null)
     private val date = combine(
-        initialDate,
+        flowOf(initialDate),
         currentDate,
         currentDate.map(formatDate::invoke),
         dateDialog,
@@ -54,8 +55,8 @@ class TimelineViewModel(
     )
 
     override val state = combine(
-        chartData,
-        tableData,
+        chart,
+        table,
         date,
         getPreference(ColorSchemePreference),
         ::TimelineState,

16f7ed34570b9a4bec703d801a743ea35c23946a
Faltenreich <philipp.fahlteich@gmail.com>
2025-06-10 22:09:12 +0200

    Prepare color stops in TimelineChartState

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/canvas/chart/TimelineChartState.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/canvas/chart/TimelineChartState.kt
@@ -1,7 +1,9 @@
 package com.faltenreich.diaguard.timeline.canvas.chart
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 
 data class TimelineChartState(
     val values: List<Offset>,
+    val colorStops: List<Pair<Float, Color>>,
 )

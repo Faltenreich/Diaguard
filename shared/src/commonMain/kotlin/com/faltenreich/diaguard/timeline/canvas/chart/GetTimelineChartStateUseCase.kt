31006193e86709ed9d19e21864b905f824c579e7
Faltenreich <philipp.fahlteich@gmail.com>
2025-06-13 20:01:12 +0200

    Prepare dynamic scaling timeline chart

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/canvas/chart/GetTimelineChartStateUseCase.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/canvas/chart/GetTimelineChartStateUseCase.kt
@@ -23,28 +23,38 @@ class GetTimelineChartStateUseCase {
         val rectangle = dimensions.chart
         val stepCount = time.hourProgression.last / time.hourProgression.step
 
-        val valueStep = Y_AXIS_STEP
-        val valueMin = Y_AXIS_MIN
-        val valueMax = max(Y_AXIS_MAX_MIN, values.maxOfOrNull { it.value } ?: 0.0)
-        val valueMaxPadded = valueMax + valueStep
-        val valueAxis = valueMin .. valueMaxPadded step valueStep
-
-        val coordinates = values.map { value ->
+        val valuesWithXCoordinate = values.map { value ->
             val dateTime = value.entry.dateTime
-
             val xAxisStep = DateTimeConstants.HOURS_PER_DAY / stepCount
             val widthPerDay = dimensions.chart.size.width
             val widthPerHour = widthPerDay / stepCount
             val widthPerMinute = widthPerHour / DateTimeConstants.MINUTES_PER_HOUR
             val offsetInMinutes = time.initialDateTime.minutesUntil(dateTime)
             val offsetOfDateTime = (offsetInMinutes / xAxisStep) * widthPerMinute
-            val x = dimensions.chart.topLeft.x + dimensions.scroll + offsetOfDateTime
+            value to dimensions.chart.topLeft.x + dimensions.scroll + offsetOfDateTime
+        }
 
+        val valueStep = Y_AXIS_STEP
+        val valueMin = Y_AXIS_MIN
+        // FIXME: Visible value should have precedence
+        val valueMaxValueWithX = valuesWithXCoordinate.maxByOrNull { (value, _) -> value.value }
+        val valueMaxValue = valueMaxValueWithX?.first?.value ?: 0.0
+        val valueMaxX = valueMaxValueWithX?.second
+        val valueMaxDistance =
+            if (valueMaxX == null) 0f
+            else if (valueMaxX < rectangle.left) rectangle.left - valueMaxX
+            else if (valueMaxX > rectangle.right) valueMaxX - rectangle.right
+            else 0f
+        // TODO: Find right distance to remove from valueMaxValue
+        val valueMax = max(Y_AXIS_MAX_MIN, valueMaxValue - valueMaxDistance)
+        val valueMaxPadded = valueMax + valueStep
+        val valueAxis = valueMin .. valueMaxPadded step valueStep
+
+        val coordinates = valuesWithXCoordinate.map { (value, x) ->
             val percentage = (value.value - valueAxis.first()) / (valueAxis.last() - valueAxis.first())
             val y = dimensions.chart.topLeft.y +
                 dimensions.chart.size.height -
                 (percentage.toFloat() * dimensions.chart.size.height)
-
             Offset(x, y)
         }
 

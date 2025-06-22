03f415cb0eeb7b0eb1071b7e92d486203a4cd69c
Faltenreich <philipp.fahlteich@gmail.com>
2025-06-22 13:25:08 +0200

    Simplify TimelineTime via translate

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/canvas/time/TimelineTime.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/timeline/canvas/time/TimelineTime.kt
@@ -5,10 +5,10 @@ package com.faltenreich.diaguard.timeline.canvas.time
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.unit.center
-import androidx.compose.ui.unit.toSize
 import com.faltenreich.diaguard.timeline.TimelineConfig
 
 @Suppress("FunctionName")
@@ -25,45 +25,40 @@ fun DrawScope.TimelineTime(
     )
 
     state.hours.forEach { hour ->
-        if (hour.hour == 0) {
+        translate(left = hour.x) {
+            if (hour.hour == 0) {
+                drawLine(
+                    color = config.gridShadowColor,
+                    start = Offset.Zero,
+                    end = Offset(
+                        x = 0f,
+                        y = dimensions.time.top,
+                    ),
+                    strokeWidth = config.gridStrokeWidth * 20,
+                )
+            }
+
             drawLine(
-                color = config.gridShadowColor,
-                start = Offset(
-                    x = hour.x,
-                    y = 0f,
-                ),
+                color = config.gridStrokeColor,
+                start = Offset.Zero,
                 end = Offset(
-                    x = hour.x,
+                    x = 0f,
                     y = dimensions.time.top,
                 ),
-                strokeWidth = config.gridStrokeWidth * 20,
+                strokeWidth = config.gridStrokeWidth,
+            )
+
+            val text = hour.hour.toString()
+            val textSize = textMeasurer.measure(text, config.textStyle)
+            drawText(
+                textMeasurer = textMeasurer,
+                text = text,
+                topLeft = Offset(
+                    x = 0f - textSize.size.center.x,
+                    y = dimensions.time.center.y - textSize.size.center.y,
+                ),
+                style = config.textStyle,
             )
         }
-
-        drawLine(
-            color = config.gridStrokeColor,
-            start = Offset(
-                x = hour.x,
-                y = 0f,
-            ),
-            end = Offset(
-                x = hour.x,
-                y = dimensions.time.top,
-            ),
-            strokeWidth = config.gridStrokeWidth,
-        )
-
-        val text = hour.hour.toString()
-        val textSize = textMeasurer.measure(text, config.textStyle)
-        drawText(
-            textMeasurer = textMeasurer,
-            text = text,
-            topLeft = Offset(
-                x = hour.x - textSize.size.center.x,
-                y = dimensions.time.center.y - textSize.size.center.y,
-            ),
-            size = textSize.size.toSize(),
-            style = config.textStyle,
-        )
     }
 }

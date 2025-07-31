c8efd13443160c2bb0cc51b1bc4c96b3834db8ee
Faltenreich <philipp.fahlteich@gmail.com>
2025-07-31 19:38:06 +0200

    Change bottom loading indicator of FoodList

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/list/FoodListLoadingIndicator.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/food/search/list/FoodListLoadingIndicator.kt
@@ -1,28 +1,15 @@
 package com.faltenreich.diaguard.food.search.list
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.faltenreich.diaguard.AppTheme
 import com.faltenreich.diaguard.shared.view.preview.AppPreview
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun FoodListLoadingIndicator(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-            .fillMaxWidth()
-            .defaultMinSize(minHeight = AppTheme.dimensions.size.TouchSizeLarge)
-            .padding(AppTheme.dimensions.padding.P_3),
-        contentAlignment = Alignment.Center,
-    ) {
-        CircularProgressIndicator()
-    }
+    LinearProgressIndicator(modifier.fillMaxWidth())
 }
 
 @Preview

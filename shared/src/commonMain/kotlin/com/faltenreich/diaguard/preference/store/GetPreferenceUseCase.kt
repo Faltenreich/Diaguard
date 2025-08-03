90bf588ee766bd3a5f40fa55a38c50a356c82de3
Faltenreich <philipp.fahlteich@gmail.com>
2025-08-03 17:45:49 +0200

    Avoid collecting redundant preference

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/store/GetPreferenceUseCase.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/preference/store/GetPreferenceUseCase.kt
@@ -2,6 +2,7 @@ package com.faltenreich.diaguard.preference.store
 
 import com.faltenreich.diaguard.preference.Preference
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 
 class GetPreferenceUseCase(
@@ -9,9 +10,11 @@ class GetPreferenceUseCase(
 ) {
 
     inline operator fun <reified Store, Domain> invoke(preference: Preference<Store, Domain>): Flow<Domain> {
-        return preferenceStore.read(preference).map { store ->
-            // FIXME: Collected again on changing other preference
-            store?.let { preference.onRead(store) } ?: preference.default
-        }
+        return preferenceStore.read(preference)
+            .map { store ->
+                store?.let { preference.onRead(store) }
+                    ?: preference.default
+            }
+            .distinctUntilChanged()
     }
 }

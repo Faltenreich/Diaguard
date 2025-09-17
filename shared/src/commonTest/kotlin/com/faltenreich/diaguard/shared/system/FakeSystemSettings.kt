284287a8d8af62215903277e3540fb930a80aff1
Faltenreich <philipp.fahlteich@gmail.com>
2025-09-17 15:14:20 +0200

    Adjust FakeSystemSettings to changed SystemSettings

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/shared/system/FakeSystemSettings.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/shared/system/FakeSystemSettings.kt
@@ -3,6 +3,4 @@ package com.faltenreich.diaguard.shared.system
 class FakeSystemSettings : SystemSettings {
 
     override fun openNotificationSettings() = Unit
-
-    override fun openPermissionSettings() = Unit
 }

efaafd6ae733ebc1edb0ed9dee936985a083a7a1
Faltenreich <philipp.fahlteich@gmail.com>
2025-10-14 21:26:28 +0200

    Fix dependency injection for Locale

--- shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/localization/LocalizationModule.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/diaguard/shared/localization/LocalizationModule.kt
@@ -1,11 +1,13 @@
 package com.faltenreich.diaguard.shared.localization
 
+import androidx.compose.ui.text.intl.Locale
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun localizationModule() = module {
+    factory { Locale.current }
     singleOf(::ComposeLocalization) bind Localization::class
     factoryOf(::NumberFormatter)
 }

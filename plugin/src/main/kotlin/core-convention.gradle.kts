c6dd98985cf5b03501d73471d83286fc4bf37a60
Faltenreich <philipp.fahlteich@gmail.com>
2025-11-19 18:44:33 +0100

    Split up convention plugins

---
+++ plugin/src/main/kotlin/core-convention.gradle.kts
@@ -0,0 +1,39 @@
+val libs = extensions.getByType<org.gradle.accessors.dm.LibrariesForLibs>()
+
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project.dependencies.platform(libs.koin.bom))
+                implementation(libs.koin.core)
+                implementation(libs.koin.compose)
+                implementation(libs.koin.compose.viewmodel)
+                implementation(libs.koin.viewmodel)
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(project.dependencies.platform(libs.koin.bom))
+                implementation(libs.koin.test)
+                implementation(libs.kotlinx.coroutines.test)
+                implementation(libs.turbine)
+            }
+        }
+    }
+
+    targets.configureEach {
+        compilations.configureEach {
+            compileTaskProvider.get().compilerOptions {
+                freeCompilerArgs.add("-Xexpect-actual-classes")
+            }
+        }
+    }
+
+    val javaVersion: Int by rootProject.extra
+    jvmToolchain(javaVersion)
+}
--- plugin/src/main/kotlin/multiplatform-convention.gradle.kts
+++ plugin/src/main/kotlin/multiplatform-convention.gradle.kts
@@ -1,55 +1,20 @@
 val libs = extensions.getByType<org.gradle.accessors.dm.LibrariesForLibs>()
 
 plugins {
-    kotlin("multiplatform")
+    id("core-convention")
     id("com.android.library")
 }
 
-val appNamespace: String by rootProject.extra
-val appCompileSdk: Int by rootProject.extra
-val javaVersion: Int by rootProject.extra
-
 kotlin {
     androidTarget()
-
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-
-    sourceSets {
-        commonMain {
-            dependencies {
-                implementation(project.dependencies.platform(libs.koin.bom))
-                implementation(libs.koin.core)
-                implementation(libs.koin.compose)
-                implementation(libs.koin.compose.viewmodel)
-                implementation(libs.koin.viewmodel)
-            }
-        }
-
-        commonTest {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(project.dependencies.platform(libs.koin.bom))
-                implementation(libs.koin.test)
-                implementation(libs.kotlinx.coroutines.test)
-                implementation(libs.turbine)
-            }
-        }
-    }
-
-    targets.configureEach {
-        compilations.configureEach {
-            compileTaskProvider.get().compilerOptions {
-                freeCompilerArgs.add("-Xexpect-actual-classes")
-            }
-        }
-    }
-
-    jvmToolchain(javaVersion)
 }
 
 android {
+    val appNamespace: String by rootProject.extra
+    val appCompileSdk: Int by rootProject.extra
     namespace = appNamespace
     compileSdk = appCompileSdk
 }

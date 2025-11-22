6ce0ba97765f3f625e13bc18068ae5cef23e4428
Faltenreich <philipp.fahlteich@gmail.com>
2025-11-22 17:35:25 +0100

    Import core modules into shared tests

--- shared/src/commonTest/kotlin/com/faltenreich/diaguard/shared/view/FakeWindowController.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/diaguard/shared/view/FakeWindowController.kt
@@ -1,5 +1,7 @@
 package com.faltenreich.diaguard.shared.view
 
+import com.faltenreich.diaguard.view.window.WindowController
+
 class FakeWindowController : WindowController {
 
     override suspend fun setIsAppearanceLightStatusBars(isAppearanceLightStatusBars: Boolean) = Unit

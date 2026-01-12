7c90205ada03c89619a57293ab3061c097004f52
Faltenreich <philipp.fahlteich@gmail.com>
2026-01-12 19:42:47 +0100

    Reduce dependencies

--- app/common/build.gradle.kts
+++ app/common/build.gradle.kts
@@ -6,22 +6,9 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                // TODO: Remove unnecessary dependencies
-                implementation(project(":core:architecture"))
-                implementation(project(":core:config"))
-                implementation(project(":core:datetime"))
-                implementation(project(":core:injection"))
-                implementation(project(":core:localization"))
-                implementation(project(":core:logging"))
-                implementation(project(":core:network"))
-                implementation(project(":core:persistence"))
-                implementation(project(":core:serialization"))
                 implementation(project(":core:system"))
-                implementation(project(":core:view"))
-                implementation(project(":data"))
                 implementation(project(":feature:backup"))
                 implementation(project(":feature:dashboard"))
-                implementation(project(":feature:datetime"))
                 implementation(project(":feature:entry"))
                 implementation(project(":feature:export"))
                 implementation(project(":feature:food"))
@@ -33,8 +20,6 @@ kotlin {
                 implementation(project(":feature:statistic"))
                 implementation(project(":feature:tag"))
                 implementation(project(":feature:timeline"))
-                implementation(project.dependencies.platform(libs.koin.bom))
-                implementation(libs.koin.test)
             }
         }
         androidInstrumentedTest {

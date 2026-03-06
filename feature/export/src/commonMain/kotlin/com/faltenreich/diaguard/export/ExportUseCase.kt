f8e2228ce6f287395b7159a21dd3d4e284944d36
Faltenreich <philipp.fahlteich@gmail.com>
2026-03-06 17:37:33 +0100

    Reduce export use cases

--- feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/ExportUseCase.kt
+++ feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/ExportUseCase.kt
@@ -2,14 +2,14 @@ package com.faltenreich.diaguard.export
 
 import com.faltenreich.diaguard.data.export.ExportSettings
 import com.faltenreich.diaguard.data.export.ExportType
-import com.faltenreich.diaguard.export.pdf.ExportPdfUseCase
+import com.faltenreich.diaguard.export.pdf.PdfExport
 import com.faltenreich.diaguard.persistence.file.File
 
-class ExportUseCase(private val exportPdf: ExportPdfUseCase) {
+class ExportUseCase(private val pdfExport: PdfExport) {
 
     suspend operator fun invoke(settings: ExportSettings): File? {
         return when (settings.exportType) {
-            ExportType.PDF -> exportPdf(settings)
+            ExportType.PDF -> pdfExport.export(settings)
             ExportType.CSV -> TODO()
         }
     }
--- feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/pdf/PdfModule.kt
+++ feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/pdf/PdfModule.kt
@@ -1,13 +1,10 @@
 package com.faltenreich.diaguard.export.pdf
 
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 fun pdfModule() = module {
     includes(pdfPlatformModule())
-
-    factoryOf(::ExportPdfUseCase)
 }
 
 expect fun pdfPlatformModule(): Module

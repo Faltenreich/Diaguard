e38d3f1cecb83d9ca1b6f33ce4583f1371ae415f
Faltenreich <philipp.fahlteich@gmail.com>
2026-09-12 20:04:11 +0200

    Add FIXME

--- core/serialization/src/commonMain/kotlin/com/faltenreich/diaguard/serialization/Serializable.kt
+++ core/serialization/src/commonMain/kotlin/com/faltenreich/diaguard/serialization/Serializable.kt
@@ -1,3 +1,7 @@
 package com.faltenreich.diaguard.serialization
 
+// FIXME: Remove and find way to support types can be stored inside the Bundle.
+//  Cannot access 'com.faltenreich.diaguard.serialization.Serializable'
+//  which is a supertype of 'DateRange' or one of its arguments. This may be forbidden soon.
+//  Check the module classpath for missing or conflicting dependencies.
 expect interface Serializable

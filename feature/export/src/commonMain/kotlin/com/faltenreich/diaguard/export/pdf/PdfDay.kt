92080abfc39d27cf37ab093c9ca86895d8fbece6
Faltenreich <philipp.fahlteich@gmail.com>
2026-09-15 20:21:28 +0200

    Introduce PdfDay

---
+++ feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/pdf/PdfDay.kt
@@ -0,0 +1,20 @@
+package com.faltenreich.diaguard.export.pdf
+
+internal class PdfDay(text: String) : PdfDrawable {
+
+    private val text = PdfText(text, PdfPaint.bold)
+    private val padding = PdfSpacing.CELL_PADDING.points
+
+    override fun getSize(page: PdfPage): PdfSize {
+        val size = text.getSize(page)
+        return size.copy(
+            width = page.viewport.width,
+            height = size.height + padding * 2,
+        )
+    }
+
+    override fun drawOn(page: PdfPage, position: PdfPosition) {
+        text.drawOn(page, position.copy(x = position.x + padding, y = position.y + padding))
+        // TODO: Outsource in component, add padding and hours
+    }
+}

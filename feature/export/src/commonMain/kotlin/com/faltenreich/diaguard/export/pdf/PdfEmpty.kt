e963170ca1675e4ef5f20f554b9ebc8bea2f3edb
Faltenreich <philipp.fahlteich@gmail.com>
2026-09-25 17:18:39 +0200

    Simplify PdfEmpty

--- feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/pdf/PdfEmpty.kt
+++ feature/export/src/commonMain/kotlin/com/faltenreich/diaguard/export/pdf/PdfEmpty.kt
@@ -17,39 +17,28 @@ internal class PdfEmpty(
 ) : PdfDrawable {
 
     private val date = PdfDate(date, dateTimeFormatter)
-    private val label = PdfText(label, PdfPaint.label)
-    private val padding = PdfSpacing.CELL_PADDING.points
+    private val label = PdfCell(PdfText(label, PdfPaint.label))
 
     override fun getSize(): PdfSize {
         return PdfSize(
             width = width,
-            height = date.getSize().height + label.getSize().height + (padding * 4),
+            height = date.getSize().height + label.getSize().height,
         )
     }
 
     override fun drawOn(page: PdfPage, position: PdfPosition) {
-        val dayPosition = position.copy(
-            x = position.x + padding,
-            y = position.y + padding,
-        )
-        date.drawOn(page, dayPosition)
+        date.drawOn(page, position)
 
-        val backgroundPosition = position.copy(
-            y = position.y + date.getSize().height + padding * 2,
-        )
+        val backgroundPosition = position.copy(y = position.y + date.getSize().height)
         val background = let {
             val size = PdfSize(
                 width = page.viewport.width,
-                height = label.getSize().height + padding * 2,
+                height = label.getSize().height,
             )
             PdfBackground(size, PdfPaint.background)
         }
         background.drawOn(page, backgroundPosition)
 
-        val labelPosition = backgroundPosition.copy(
-            x = position.x + padding,
-            y = backgroundPosition.y + padding,
-        )
-        label.drawOn(page, labelPosition)
+        label.drawOn(page, backgroundPosition)
     }
 }
